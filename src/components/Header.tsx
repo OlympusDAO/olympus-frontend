c05b8c7c51de8acb2beeda85605e4b715edb4021
Dmitri <unbanksyeth@gmail.com>
2021-05-28 15:51:43 -0700

    jsx -> tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import { PageHeader } from "antd";
+import React from "react";
+
+// displays a page header
+
+export default function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light justify-content-end d-md-none">
+      <button
+        className="navbar-toggler"
+        type="button"
+        onClick={() => {}}
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+    </nav>
+
+  );
+}
