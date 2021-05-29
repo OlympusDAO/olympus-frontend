af8b9190d9594f6e9e169bcd4cdaa7badc045776
Dmitri <unbanksyeth@gmail.com>
2021-05-29 08:09:02 -0700

    Add ability to stake and unstake.

---
+++ src/actions/Stake.actions.js
@@ -0,0 +1,64 @@
+import { ethers } from "ethers";
+import { addresses, Actions } from "../constants";
+import { abi as ierc20Abi } from '../abi/IERC20.json';
+import { abi as OHMPreSale } from '../abi/OHMPreSale.json';
+import { abi as OlympusStaking } from '../abi/OlympusStaking.json';
+import { abi as MigrateToOHM } from '../abi/MigrateToOHM.json';
+import { abi as sOHM } from '../abi/sOHM.json';
+import { abi as LPStaking } from '../abi/LPStaking.json';
+import { abi as DistributorContract } from '../abi/DistributorContract.json';
+import { abi as BondContract } from '../abi/BondContract.json';
+import { abi as DaiBondContract } from '../abi/DaiBondContract.json';
+
+export const fetchStakeSuccess = payload => ({
+  type: Actions.FETCH_STAKE_SUCCESS,
+  payload
+});
+
+export const changeStake = ({ action, value, provider, address, networkID }) => async dispatch => {
+  if (!provider) {
+    alert('Please connect your wallet!');
+    return;
+  }
+
+  const signer = provider.getSigner();
+  const staking = await new ethers.Contract(
+    addresses[networkID].STAKING_ADDRESS,
+    OlympusStaking,
+    signer
+  );
+
+  let stakeTx;
+
+  try {
+    if (action === 'stake') {
+      stakeTx = await staking.stakeOHM(ethers.utils.parseUnits(value, 'gwei'));
+      await stakeTx.wait();
+    } else {
+      stakeTx = await staking.unstakeOHM(ethers.utils.parseUnits(value, 'gwei'));
+      await stakeTx.wait();
+    }
+  } catch (error) {
+    if (error.code === -32603 && error.message.indexOf('ds-math-sub-underflow') >= 0) {
+      alert(
+        'You may be trying to stake more than your balance! Error code: 32603. Message: ds-math-sub-underflow'
+      );
+    } else {
+      alert(error.message);
+    }
+    return;
+  }
+
+
+  const ohmContract  = new ethers.Contract(addresses[networkID].OHM_ADDRESS, ierc20Abi, provider);
+  const ohmBalance   = await ohmContract.balanceOf(address);
+  const sohmContract = new ethers.Contract(addresses[networkID].SOHM_ADDRESS, ierc20Abi, provider);
+  const sohmBalance  = await sohmContract.balanceOf(address);
+
+
+  return dispatch(fetchStakeSuccess({
+    ohm: ethers.utils.formatUnits(ohmBalance, 'gwei'),
+    sohm: ethers.utils.formatUnits(sohmBalance, 'gwei')
+  }))
+
+};
--- src/constants.js
+++ src/constants.js
@@ -46,6 +46,7 @@ export const addresses = {
 export const Actions = {
   FETCH_ACCOUNT_SUCCESS: 'account/FETCH_ACCOUNT_SUCCESS',
   FETCH_APP_SUCCESS: 'app/FETCH_APP_SUCCESS',
+  FETCH_STAKE_SUCCESS: 'stake/FETCH_STAKE_SUCCESS'
 };
 
 
