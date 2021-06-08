ad6f12fe1b5b2b6c74ea6477d64a8e3c7a5ea78e
Siferbel <info@siferbel.com>
2021-06-08 02:42:11 +0200

    Added different type of fonts, added light theme color config.

--- src/theme.js
+++ src/theme.js
@@ -6,8 +6,8 @@ export const darkTheme = {
 }
   
 export const lightTheme = {
-    color: "#EFEFEF",
-    backgroundColor: "#4158D0",
-    background: "linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)",
-    cardBg: "#4242426A"
+    color: "#2E2E2E",
+    backgroundColor: "#EFE9E0",
+    background: "radial-gradient(circle at 25% 0%, rgba(227,255,240,.5), rgba(227,255,240,0) 50%), radial-gradient(circle at 80% 80%, rgba(131,165,203,.5), rgba(131,165,203,0) 50%)",
+    cardBg: "rgba(252,252,252, 0.6) !important"
 }
