5185c0b2aafa2f66bb243de38494db62ce0d497f
LolChocotaco <zayenx@gmail.com>
2021-06-10 17:43:33 -0700

    added migrate view and stubbed out some basic changes

---
+++ src/actions/Migrate.actions.js
@@ -0,0 +1,103 @@
+import { ethers } from "ethers";
+import { addresses, Actions } from "../constants";
+import { abi as ierc20Abi } from "../abi/IERC20.json";
+import { abi as OlympusStaking } from "../abi/OlympusStaking.json";
+
+export const TYPES = { STAKE: "STAKE", UNSTAKE: "UNSTAKE" };
+
+export const fetchMigrateSuccess = payload => ({
+  type: Actions.FETCH_MIGRATE_SUCCESS,
+  payload,
+});
+
+export const changeApproval =
+  ({ token, provider, address, networkID }) =>
+  async dispatch => {
+    if (!provider) {
+      alert("Please connect your wallet!");
+      return;
+    }
+
+    const signer = provider.getSigner();
+    // TODO: need the new wsOhm contract
+    const ohmContract = await new ethers.Contract(addresses[networkID].OHM_ADDRESS, ierc20Abi, signer);
+    const wsohmContract = await new ethers.Contract(addresses[networkID].WSOHM_ADDRESS, ierc20Abi, signer);
+    const sohmContract = await new ethers.Contract(addresses[networkID].SOHM_ADDRESS, ierc20Abi, signer);
+
+    let approveTx;
+    try {
+      if (token === "wsohm") {
+        approveTx = await wsohmContract.approve(
+          addresses[networkID].STAKING_ADDRESS,
+          ethers.utils.parseUnits("1000000000", "gwei").toString(),
+        );
+      } else if (token === "ohm") {
+        approveTx = await ohmContract.approve(
+          addresses[networkID].STAKING_ADDRESS,
+          ethers.utils.parseUnits("1000000000", "gwei").toString(),
+        );
+      }
+
+      await approveTx.wait();
+    } catch (error) {
+      alert(error.message);
+      return;
+    }
+
+    const stakeAllowance = await ohmContract.allowance(address, addresses[networkID].STAKING_ADDRESS);
+    const unstakeAllowance = await sohmContract.allowance(address, addresses[networkID].STAKING_ADDRESS);
+
+    return dispatch(
+      fetchMigrateSuccess({
+        migrate: {
+          ohm: stakeAllowance,
+          sohm: unstakeAllowance,
+        },
+      }),
+    );
+  };
+
+export const changeStake =
+  ({ action, value, provider, address, networkID }) =>
+  async dispatch => {
+    if (!provider) {
+      alert("Please connect your wallet!");
+      return;
+    }
+
+    const signer = provider.getSigner();
+    // keep the STAKING_ADDRESS as the CURRENT version. Only reference the old address here.
+    const oldStaking = await new ethers.Contract(addresses[networkID].OLD_STAKING_ADDRESS, OlympusStaking, signer);
+    const staking = await new ethers.Contract(addresses[networkID].STAKING_ADDRESS, "INSERT_ABI_HERE", signer);
+
+    let stakeTx;
+
+    try {
+      if (action === "stake") {
+        stakeTx = await staking.stakeOHM(ethers.utils.parseUnits(value, "gwei"));
+        await stakeTx.wait();
+      } else {
+        stakeTx = await oldStaking.unstakeOHM(ethers.utils.parseUnits(value, "gwei"));
+        await stakeTx.wait();
+      }
+    } catch (error) {
+      if (error.code === -32603 && error.message.indexOf("ds-math-sub-underflow") >= 0) {
+        alert("You may be trying to stake more than your balance! Error code: 32603. Message: ds-math-sub-underflow");
+        return;
+      }
+      alert(error.message);
+      return;
+    }
+
+    const ohmContract = new ethers.Contract(addresses[networkID].OHM_ADDRESS, ierc20Abi, provider);
+    const ohmBalance = await ohmContract.balanceOf(address);
+    const sohmContract = new ethers.Contract(addresses[networkID].SOHM_ADDRESS, ierc20Abi, provider);
+    const sohmBalance = await sohmContract.balanceOf(address);
+
+    return dispatch(
+      fetchStakeSuccess({
+        ohm: ethers.utils.formatUnits(ohmBalance, "gwei"),
+        sohm: ethers.utils.formatUnits(sohmBalance, "gwei"),
+      }),
+    );
+  };
