1809c9eb5c19b4ef088d5552ee4b879bb9e53b1d
girth brooks <gb@dwj.com>
2021-06-16 11:46:48 -0400

    added new actions , contracts, migrate from master. modified sizing on cards, added currency in bond table

--- src/reducers.js
+++ src/reducers.js
@@ -3,6 +3,7 @@ import { Actions } from "./constants";
 export function app(state = {}, action) {
   switch (action.type) {
     case Actions.FETCH_APP_SUCCESS:
+    case Actions.FETCH_MIGRATE_SUCCESS:
     case Actions.FETCH_ACCOUNT_SUCCESS:
       return { ...state, ...action.payload };
     default:
