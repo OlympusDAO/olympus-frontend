19bda5e6d5e90bfb9e887dcd9b4a820d9fea79f4
girth brooks <gb@dwj.com>
2021-06-16 15:19:56 -0400

    styled 404

--- src/views/404/NotFound.jsx
+++ src/views/404/NotFound.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import OlympusLogo from '../../assets/logo.svg';
+import "./notfound.scss";
 
 export default function NotFound() {
 	return (
-		<div>
+		<div id="not-found">
 			<div className="not-found-header">
 				<a href="https://olympusdao.finance" target="_blank">
 					<img className="branding-header-icon" src={OlympusLogo} alt="OlympusDAO" />
