0bb3be818f0d2f1aafbce32a95d2d4ecf915360f
girth brooks <gb@dwj.com>
2021-06-18 17:50:47 -0400

    cleanup

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,8 +1,4 @@
-import React, { useState, useCallback, } from 'react';
-import { BrowserRouter, NavLink, Route, Switch } from "react-router-dom";
-import Address from "./Address";
-import { StaticJsonRpcProvider, Web3Provider } from "@ethersproject/providers";
-import Web3Modal from "web3modal";
+import React from 'react';
 import OlympusLogo from '../assets/logo.svg';
 import GitHubImg from '../assets/github.svg';
 import MediumImg from '../assets/medium.svg';
@@ -10,32 +6,20 @@ import TwitterImg from '../assets/twitter.svg';
 import DiscordImg from '../assets/discord.svg';
 
 
-
-
 function Social() {
-  // isBondPage and isDashboard arent DRY, this can be optimized
-  const isBondPage = useCallback((match, location) => {
-    if (!match) {
-      return false;
-    }
-
-    return match.url.indexOf('bonds') >= 0 || match.url.indexOf('choose_bond') >= 0
-  }, []);
-
-
   return (
     <div className="social-row">
       <a href="https://github.com/OlympusDAO/olympus-frontend"
-        ><img src={GitHubImg} alt="" className="social-icon-small"
+        ><img src={GitHubImg} alt="github" target="_blank" className="social-icon-small"
       /></a>
       <a href="https://olympusdao.medium.com/"
-        ><img src={MediumImg} alt="" className="social-icon-small"
+        ><img src={MediumImg} alt="medium" target="_blank" className="social-icon-small"
       /></a>
       <a href="https://twitter.com/OlympusDAO"
-        ><img src={TwitterImg} alt="" className="social-icon-small"
+        ><img src={TwitterImg} alt="twitter" target="_blank" className="social-icon-small"
       /></a>
       <a href="https://discord.gg/6QjjtUcfM4"
-        ><img src={DiscordImg} alt="" className="social-icon-small"
+        ><img src={DiscordImg} alt="discord" target="_blank" className="social-icon-small"
       /></a>
     </div>
   );
