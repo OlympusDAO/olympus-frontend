cc6fb982372b84032d6c878b5e0cd2e792fd0f91
Emm <tslaallin@protonmail.com>
2021-06-29 17:37:00 +1200

    Implement new menu design and only show add token when eth api is available

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,122 +1,136 @@
-import React, { Fragment } from "react";
-// import { StaticJsonRpcProvider, Web3Provider } from "@ethersproject/providers";
-import { useSelector, useDispatch } from 'react-redux';
-// import Web3Modal from "web3modal";
-import { shorten } from '../../helpers';
-import ThemeSwitcher from "../ThemeSwitch/ThemeSwitch";
+import React, { useState } from "react";
 import { Flex } from "rimble-ui";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
+import InfoIcon from "@material-ui/icons/Info";
+import ThemeSwitcher from "../ThemeSwitch/ThemeSwitch";
 import "./topbar.scss";
-import { ReactComponent as OhmIcon } from '../../assets/icons/ohm-mini-logo.svg';
-import { ReactComponent as SohmIcon } from '../../assets/icons/sohm-gold-logo.svg';
-import ToggleButton from "@material-ui/lab/ToggleButton";
 
-const SOHM_ADDRESS = '0x04f2694c8fcee23e8fd0dfea1d4f5bb8c352111f';
-const OHM_ADDRESS = '0x383518188c0c6d7730d91b2c03a03c837814a899';
+const SOHM_ADDRESS = "0x04f2694c8fcee23e8fd0dfea1d4f5bb8c352111f";
+const OHM_ADDRESS = "0x383518188c0c6d7730d91b2c03a03c837814a899";
 const TOKEN_DECIMALS = 9;
-const TOKEN_IMAGE = 'https://app.olympusdao.finance/static/media/logo.b949cd68.svg';
-
+const TOKEN_IMAGE = "https://app.olympusdao.finance/static/media/logo.b949cd68.svg";
 
 const addOhmToWallet = (tokenSymbol, tokenAddress) => async () => {
-  try {
-    const wasAdded = await window.ethereum.request({
-      method: 'wallet_watchAsset',
-      params: {
-        type: 'ERC20',
-        options: {
-          address: tokenAddress,
-          symbol: tokenSymbol,
-          decimals: TOKEN_DECIMALS,
-          image: TOKEN_IMAGE,
+  if (window.ethereum) {
+    try {
+      const wasAdded = await window.ethereum.request({
+        method: "wallet_watchAsset",
+        params: {
+          type: "ERC20",
+          options: {
+            address: tokenAddress,
+            symbol: tokenSymbol,
+            decimals: TOKEN_DECIMALS,
+            image: TOKEN_IMAGE,
+          },
         },
-      },
-    });
+      });
 
-    if (wasAdded) {
-      console.log('Welcome aboard Ohmie!');
-    } else {
-      console.log('Woopsie!');
+      if (wasAdded) {
+        console.log("Welcome aboard Ohmie!");
+      } else {
+        console.log("Woopsie!");
+      }
+    } catch (error) {
+      console.log(error);
     }
-  } catch (error) {
-    console.log(error);
   }
-}
+};
 
+const ohmMenu = (isShown = false, theme = "light") => {
+  const isEthereumAPIAvailable = window.ethereum;
+  return isShown ? (
+    <div className="ohm-card add-ohm-wallet">
+      <div className="ohm-menu-container">
+        <div style={{ marginBottom: "5px" }}>
+          <a
+            style={{ fontFamily: "Square", color: theme === "light" ? "black" : "white" }}
+            href="https://app.sushi.com/swap?inputCurrency=0x6b175474e89094c44da98b954eedeac495271d0f&outputCurrency=0x383518188c0c6d7730d91b2c03a03c837814a899"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Buy on Sushiswap{" "}
+          </a>
+          <i className="fa fa-external-link-alt" />
+        </div>
+        <div>
+          <a
+            style={{ fontFamily: "Square", color: theme === "light" ? "black" : "white" }}
+            href="https://app.uniswap.org/swap?inputCurrency=0x6b175474e89094c44da98b954eedeac495271d0f&outputCurrency=0x383518188c0c6d7730d91b2c03a03c837814a899"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Buy on Uniswap{" "}
+          </a>
+          <i className="fa fa-external-link-alt" />
+        </div>
+      </div>
 
+      {isEthereumAPIAvailable ? (
+        <>
+          <hr style={{ color: "grey", margin: "0px" }} />
+          <div className="ohm-menu-container">
+            <p className="add-token-title">ADD TOKEN TO WALLET</p>
+            <div onClick={addOhmToWallet("sOHM", SOHM_ADDRESS)}>
+              <p className="ohm-token-name">sOHM</p>
+            </div>
+            <div onClick={addOhmToWallet("OHM", OHM_ADDRESS)}>
+              <p className="ohm-token-name">OHM</p>
+            </div>
+          </div>
+        </>
+      ) : null}
+    </div>
+  ) : null;
+};
 
-function TopBar({ web3Modal, loadWeb3Modal, logoutOfWeb3Modal, address, mainnetProvider, theme, toggleTheme}) {
-	const isVerySmallScreen = useMediaQuery("(max-width: 649px)");
-	const isUltraSmallScreen = useMediaQuery("(max-width: 495px)");
+function TopBar({ web3Modal, loadWeb3Modal, logoutOfWeb3Modal, theme, toggleTheme }) {
+  const [showOhmMenu, setShowOhmMenu] = useState(false);
 
   const modalButtons = [];
   if (web3Modal) {
     if (web3Modal.cachedProvider) {
       modalButtons.push(
-        	<button type="button" className={`btn top-bar-button btn-overwrite-primer m-2`} onClick={logoutOfWeb3Modal} key={1}>
-						Disconnect
-				</button>,
+        <button
+          type="button"
+          className="btn top-bar-button btn-overwrite-primer m-2"
+          onClick={logoutOfWeb3Modal}
+          key={1}
+        >
+          Disconnect
+        </button>,
       );
     } else {
       modalButtons.push(
-        <button type="button" className={`btn top-bar-button btn-overwrite-primer m-2`} onClick={loadWeb3Modal} key={2}>Connect Wallet</button>,
+        <button type="button" className="btn top-bar-button btn-overwrite-primer m-2" onClick={loadWeb3Modal} key={2}>
+          Connect Wallet
+        </button>,
       );
     }
   }
 
   return (
-    <div className={`dapp-topbar`}>
-			<Flex className="dapp-topbar-items">
-				{!(address && isUltraSmallScreen) &&
-					<ThemeSwitcher
-						theme={theme}
-						toggleTheme={toggleTheme}
-					/>
-				}
+    <div className="dapp-topbar">
+      {ohmMenu(showOhmMenu, theme)}
+      <Flex className="dapp-topbar-items">
+        <>
+          <button
+            id="get-ohm"
+            className="get-ohm-button btn btn-overwrite-primer m-2 top-bar-button"
+            title="Get OHM"
+            onClick={() => setShowOhmMenu(!showOhmMenu)}
+          >
+            <div style={{ paddingTop: "5px" }}>
+              <InfoIcon style={{ marginBottom: "5px" }} fontSize="small" /> <span>Ohm</span>
+            </div>
+          </button>
+        </>
 
-				{!isVerySmallScreen &&
-          <Fragment>
-            <ToggleButton
-              className="toggle-button btn top-bar-button m-2 .add-ohm-wallet"
-              type="button"
-              title="Add sOHM to Wallet"
-              value="check"
-              onClick={addOhmToWallet('sOHM', SOHM_ADDRESS)}
-            >
-              <SohmIcon className="ohm-mm-icon" />
-            </ToggleButton>
+        <div className="wallet-menu" id="wallet-menu">
+          {modalButtons}
+        </div>
 
-            <ToggleButton
-              className="toggle-button btn top-bar-button m-2 .add-ohm-wallet"
-              type="button"
-              title="Add OHM to Wallet"
-              value="check"
-              onClick={addOhmToWallet('OHM', OHM_ADDRESS)}
-            >
-              <OhmIcon className="ohm-mm-icon" />
-            </ToggleButton>
-            <button
-              id="get-ohm"
-              className="get-ohm-button btn btn-overwrite-primer m-2 top-bar-button"
-              title="Get OHM"
-            >
-              <a href="https://app.sushi.com/swap?inputCurrency=0x6b175474e89094c44da98b954eedeac495271d0f&outputCurrency=0x383518188c0c6d7730d91b2c03a03c837814a899" target="_blank">
-                Get OHM
-              </a>
-            </button>
-          </Fragment>
-
-				}
-
-				<div className="wallet-menu" id="wallet-menu">
-					{modalButtons}
-					{address && <button type="button" className={`btn top-bar-button btn-overwrite-primer m-2`}>
-						<a href={`https://etherscan.io/address/${address}`} target="_blank" className="ml-2">
-							{shorten(address)}
-						</a>
-					</button>
-					}
-				</div>
-			</Flex>
+        <ThemeSwitcher theme={theme} toggleTheme={toggleTheme} />
+      </Flex>
     </div>
   );
 }
