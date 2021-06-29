b30daf34009b5a8628b73f2cf977347caf9eca1a
girth brooks <gb@dwj.com>
2021-06-29 12:57:46 -0400

    deleted comment block from app actions, removed console logs, renamed graph id in constants

--- src/lib/apolloClient.js
+++ src/lib/apolloClient.js
@@ -1,11 +1,14 @@
 import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
-import { THEGRAPH_ID } from "../constants";
-const APIRUL = "https://api.thegraph.com/subgraphs/id/"+THEGRAPH_ID;
+import { THE_GRAPH_ID } from "../constants";
+
+
+const APIRUL = "https://api.thegraph.com/subgraphs/id/"+THE_GRAPH_ID;
 
 const client = new ApolloClient({
 	uri: APIRUL,
 	cache: new InMemoryCache()
 });
+
 
 export default client;
 
