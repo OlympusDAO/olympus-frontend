b0f44e8cf90f3e2d19cccbade1c7f4b38d1402c3
Drondin <drondin596@gmail.com>
2021-06-30 07:49:45 +0000

    Fix unstake

--- src/views/Stake/Migrate.jsx
+++ src/views/Stake/Migrate.jsx
@@ -31,7 +31,7 @@ export default function Migrate({ address, provider, web3Modal, loadWeb3Modal })
     return state.app.staking && state.app.staking.ohmStake;
   });
   const unstakeAllowance = useSelector(state => {
-    return state.app.migrate && state.app.migrate.unstakeAllowance;
+    return state.app.staking && state.app.staking.ohmUnstake;
   });
   const newStakingAPY = useSelector(state => {
     return (state.app && state.app.stakingAPY) || 0;
