553963ef12697be494c47b27932f9b836ad68110
girth brooks <gb@dwj.com>
2021-07-01 11:44:47 -0400

    sidebar spacing, mobile bond views, typography. anext up link colors and table format

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -18,19 +18,15 @@ function Sidebar({ isExpanded, theme, currentIndex }) {
   const checkPage = useCallback((match, location, page) => {
     const currentPath = location.pathname.replace("/", "");
     const currentURL = window.location.href;
-
     if (currentPath.indexOf("dashboard") >= 0 && page === "dashboard") {
       return true;
     }
-
     if (currentPath.indexOf("stake") >= 0 && page === "stake") {
       return true;
     }
-
     if ((currentPath.indexOf("bonds") >= 0 || currentPath.indexOf("choose_bond") >= 0) && page === "bonds") {
       return true;
     }
-
     return false;
   }, []);
 
@@ -40,61 +36,59 @@ function Sidebar({ isExpanded, theme, currentIndex }) {
       className={`${isExpanded ? 'show' : '' } d-lg-block sidebar collapse`}
       id="sidebarContent"
     >
-    <Drawer variant="permanent" anchor="left">
-      <Paper className="dapp-sidebar">
-        <div className="dapp-menu-top">
-          <div className="branding-header">
-            <a href="https://olympusdao.finance" target="_blank">
-              <img className="branding-header-icon" src={OlympusLogo} alt="OlympusDAO" />
-              <h3>Olympus</h3>
-            </a>
-          </div>
-        
+      <Drawer variant="permanent" anchor="left">
+        <Paper className="dapp-sidebar">
+          <div className="dapp-menu-top">
+            <div className="branding-header">
+              <a href="https://olympusdao.finance" target="_blank">
+                <img className="branding-header-icon" src={OlympusLogo} alt="OlympusDAO" />
+                <h3>Olympus</h3>
+              </a>
+            </div>
+          
+            <div className="dapp-menu-links">
+              <div className="dapp-nav" id="navbarNav">
+                <NavLink id="dash-nav" to="/dashboard" isActive={(match, location) => { return checkPage(match, location, "dashboard") }} className={`button button-dapp-menu ${isActive ? "active" : ""}`}>
+                  <DashboardIcon className="me-3" />
+                  <span>Dashboard</span>
+                </NavLink>
 
-        <div className="dapp-menu-links">
-          <div className="dapp-nav" id="navbarNav">
-            <NavLink id="dash-nav" to="/dashboard" isActive={(match, location) => { return checkPage(match, location, "dashboard") }} className={`button button-dapp-menu ${isActive ? "active" : ""}`}>
-              <DashboardIcon className="me-3" />
-              <span>Dashboard</span>
-            </NavLink>
+                <NavLink id="stake-nav" to="/" isActive={(match, location) => { return checkPage(match, location, "stake") }}  className={`button button-dapp-menu ${isActive ? "active" : ""}`} >
+                  <StakeIcon className="me-3" />
+                  <span>Stake</span>
+                </NavLink>
 
-            <NavLink id="stake-nav" to="/" isActive={(match, location) => { return checkPage(match, location, "stake") }}  className={`button button-dapp-menu ${isActive ? "active" : ""}`} >
-              <StakeIcon className="me-3" />
-              <span>Stake</span>
-            </NavLink>
+                <NavLink id="bond-nav" to="/bonds" isActive={(match, location) => { return checkPage(match, location, "bonds") }} className={`button button-dapp-menu ${isActive ? "active" : ""}`}>
+                  <BondIcon className="me-3" />
+                  <span>Bond</span>
+                </NavLink>
 
-            <NavLink id="bond-nav" to="/bonds" isActive={(match, location) => { return checkPage(match, location, "bonds") }} className={`button button-dapp-menu ${isActive ? "active" : ""}`}>
-              <BondIcon className="me-3" />
-              <span>Bond</span>
-            </NavLink>
-
-            {/* needs functions for discount calc */}
-            <div className="dapp-menu-data discounts">
-              <div className="bond-discounts">
-                <p>Bond discounts</p>
-                {bonds.map((bond, i) => (
-                  <Link to={`/bonds/${bond.value}`} key={i} className={"bond"}>{bond.name}<span>{bond.discount ? trim(bond.discount * 100, 2) : ''}%</span></Link>
-                ))}
+                <div className="dapp-menu-data discounts">
+                  <div className="bond-discounts">
+                    <p>Bond discounts</p>
+                    {bonds.map((bond, i) => (
+                      <Link to={`/bonds/${bond.value}`} key={i} className={"bond"}>{bond.name}<span>{bond.discount ? trim(bond.discount * 100, 2) : ''}%</span></Link>
+                    ))}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        </div>
 
-        <hr />
+          <hr />
 
-        <div className="dapp-menu-data bottom">
-          <div className="dapp-menu-external-links">
-            { Object.keys(externalUrls).map((link, i) => {
-              return <a key={i} href={`${externalUrls[link].url}`} target="_blank" className="button button-dapp-menu">
-                {externalUrls[link].icon}
-                <span>{externalUrls[link].title}</span>
-              </a>
-              }
-            )}
+          <div className="dapp-menu-data bottom">
+            <div className="dapp-menu-external-links">
+              { Object.keys(externalUrls).map((link, i) => {
+                return <a key={i} href={`${externalUrls[link].url}`} target="_blank" className="button button-dapp-menu">
+                  {externalUrls[link].icon}
+                  <span>{externalUrls[link].title}</span>
+                </a>
+                }
+              )}
+            </div>
           </div>
 
-        
           {theme === "girth" &&
             <div className="data-ohm-index">
               <p>Current Index </p>
@@ -104,11 +98,8 @@ function Sidebar({ isExpanded, theme, currentIndex }) {
           <div className="dapp-menu-social">
             <Social />
           </div>
-        </div>
-
-        
-
-      </Paper>
+          
+        </Paper>
       </Drawer>
     </div>
   );
