132dd08c66126fd13b3132908d49ce185aa1cc5c
girth brooks <gb@dwj.com>
2021-07-01 15:56:06 -0400

    top bar buttons basic theme applied

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -6,8 +6,13 @@ import { shorten } from '../../helpers';
 import ThemeSwitcher from "../ThemeSwitch/ThemeSwitch";
 >>>>>>> fixed links in bond discounts
 import { Flex } from "rimble-ui";
+<<<<<<< HEAD
 import InfoIcon from "@material-ui/icons/Info";
 import ThemeSwitcher from "../ThemeSwitch/ThemeSwitch";
+=======
+import { Button } from "@material-ui/core";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+>>>>>>> top bar buttons basic theme applied
 import "./topbar.scss";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { addresses } from "../../constants";
@@ -104,6 +109,7 @@ function TopBar({ web3Modal, loadWeb3Modal, logoutOfWeb3Modal, address, theme, t
   if (web3Modal) {
     if (web3Modal.cachedProvider) {
       modalButtons.push(
+<<<<<<< HEAD
         <button
           type="button"
           className="btn top-bar-button btn-overwrite-primer m-2"
@@ -118,11 +124,27 @@ function TopBar({ web3Modal, loadWeb3Modal, logoutOfWeb3Modal, address, theme, t
         <button type="button" className="btn top-bar-button btn-overwrite-primer m-2" onClick={loadWeb3Modal} key={2}>
           Connect Wallet
         </button>,
+=======
+        	<Button 
+						type="button" 
+						variant="contained"
+						color="primary"
+						className={`btn top-bar-button btn-overwrite-primer m-2`} 
+						onClick={logoutOfWeb3Modal} 
+						key={1}>
+						Disconnect
+				</Button>,
+      );
+    } else {
+      modalButtons.push(
+        <Button variant="contained" color="secondary" type="button" className={`btn top-bar-button btn-overwrite-primer m-2`} onClick={loadWeb3Modal} key={2}>Connect Wallet</Button>,
+>>>>>>> top bar buttons basic theme applied
       );
     }
   }
 
   return (
+<<<<<<< HEAD
     <div className="dapp-topbar">
       {ohmMenu(showOhmMenu, theme, isBigScreen, networkID)}
       <Flex className="dapp-topbar-items">
@@ -145,6 +167,42 @@ function TopBar({ web3Modal, loadWeb3Modal, logoutOfWeb3Modal, address, theme, t
 
         <ThemeSwitcher theme={theme} toggleTheme={toggleTheme} />
       </Flex>
+=======
+    <div className={`dapp-topbar`}>
+			<Flex className="dapp-topbar-items">		
+				{!(address && isUltraSmallScreen) &&  
+				
+						<ThemeSwitcher 
+							theme={theme}
+							toggleTheme={toggleTheme} 
+						/>
+				}
+				
+				{!isVerySmallScreen && 
+					<Button
+						id="get-ohm"
+						className="get-ohm-button m-2 top-bar-button"
+						variant="contained"
+						color="secondary"
+						title="Get OHM"
+					>		
+						<a href="https://app.sushi.com/swap?inputCurrency=0x6b175474e89094c44da98b954eedeac495271d0f&outputCurrency=0x383518188c0c6d7730d91b2c03a03c837814a899" target="_blank">
+							Get OHM
+						</a>
+					</Button>
+				}
+				
+				<div className="wallet-menu" id="wallet-menu">
+					{modalButtons}
+					{address && <button type="button" className={`btn top-bar-button btn-overwrite-primer m-2`}>
+						<a href={`https://etherscan.io/address/${address}`} target="_blank" className="ml-2">
+							{shorten(address)}
+						</a>
+					</button>
+					}
+				</div>
+			</Flex>
+>>>>>>> top bar buttons basic theme applied
     </div>
   );
 }
