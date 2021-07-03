882216cc8154de863b4f32f496c2552fb8988e6e
Drondin <drondin596@gmail.com>
2021-07-03 16:14:53 +0000

    Connect subgraph by name

--- src/lib/apolloClient.js
+++ src/lib/apolloClient.js
@@ -1,9 +1,16 @@
 import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 import { THE_GRAPH_URL } from "../constants";
 
+<<<<<<< HEAD
 const client = new ApolloClient({
   uri: THE_GRAPH_URL,
   cache: new InMemoryCache(),
+=======
+
+const client = new ApolloClient({
+	uri: THE_GRAPH_URL,
+	cache: new InMemoryCache()
+>>>>>>> Connect subgraph by name
 });
 
 const apollo = queryString => {
