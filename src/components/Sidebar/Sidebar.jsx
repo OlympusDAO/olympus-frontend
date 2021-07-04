60c3c09749030a81ed95ab0b82e8e79a3918bf3a
Emm <tslaallin@protonmail.com>
2021-07-04 22:25:11 +1200

    Make sidebar discount numbers bold

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -91,7 +91,7 @@ function Sidebar({ address }) {
               <div className="bond-discounts">
                 <p>Bond discounts</p>
                 {bonds.map((bond, i) => (
-                  <Link to={`/bonds/${bond.value}`} key={i} className={"bond"}>{bond.name}<span>{bond.discount ? trim(bond.discount * 100, 2) : ''}%</span></Link>
+                  <Link to={`/bonds/${bond.value}`} key={i} className={"bond"}>{bond.name}<span style={{fontWeight: 'bold'}}>{bond.discount ? trim(bond.discount * 100, 2) : ''} %</span></Link>
                 ))}
               </div>
             </div>
