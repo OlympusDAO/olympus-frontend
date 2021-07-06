e7a93b4ab7fa2f73bc1f8a31e8cc452bb98c0d20
girth brooks <gb@dwj.com>
2021-07-05 23:31:51 -0400

    fixed topbar, stake mobile buttons, bond view, bond modal

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useSelector } from "react-redux";
 import { Paper, Grid, Typography } from "@material-ui/core";
 import { trim } from "../../helpers";
@@ -19,46 +18,50 @@ function Dashboard() {
   });
 
   return (
-    <div className="dashboard-view">
-      <Grid container spacing={2} className="top-row-data">
-        <Grid item lg={4} md={5} sm={5} xs={5} className="olympus-card">
-          <Paper>
-              <Typography variant="h6">Price</Typography>
-              <Typography variant="h5">${trim(marketPrice, 2)}</Typography>
-          </Paper> 
+    <div id="dashboard-view">
+      <Grid container spacing={1} className="top-row-data">
+        <Grid item lg={4} md={4} sm={5} xs={6} className="olympus-card">
+          <Paper className="ohm-card">
+            <Typography variant="h6">Price</Typography>
+            <Typography variant="h5">${trim(marketPrice, 2)}</Typography>
+          </Paper>
         </Grid>
 
-        <Grid item lg={4} md={7} sm={7} xs={7}>
-          <Paper>
+        <Grid item lg={4} md={4} sm={7} xs={6}>
+          <Paper className="ohm-card">
             <Typography variant="h6">Market Cap</Typography>
             <Typography variant="h5">
-                {marketCap && new Intl.NumberFormat("en-US", {
+              {marketCap &&
+                new Intl.NumberFormat("en-US", {
                   style: "currency",
                   currency: "USD",
                   maximumFractionDigits: 0,
-                  minimumFractionDigits: 0
+                  minimumFractionDigits: 0,
                 }).format(marketCap)}
-             </Typography>
-          </Paper> 
+            </Typography>
+          </Paper>
         </Grid>
 
-        <Grid item lg={4} md={12} sm={12} xs={12}>
-          <Paper>
+        <Grid item lg={4} md={4} sm={12} xs={12}>
+          <Paper className="ohm-card">
             <Typography variant="h6">Supply (circulating/total)</Typography>
-              <Typography variant="h5">
-                  {circSupply && new Intl.NumberFormat("en-US", { 
-                    maximumFractionDigits: 0,
-                    minimumFractionDigits: 0
-                  }).format(circSupply)}/ 
-                  {totalSupply && new Intl.NumberFormat("en-US", { 
-                    maximumFractionDigits: 0,
-                    minimumFractionDigits: 0
-                  }).format(totalSupply)}
-              </Typography>
-            </Paper>
+            <Typography variant="h5">
+              {circSupply &&
+                new Intl.NumberFormat("en-US", {
+                  maximumFractionDigits: 0,
+                  minimumFractionDigits: 0,
+                }).format(circSupply)}
+              /
+              {totalSupply &&
+                new Intl.NumberFormat("en-US", {
+                  maximumFractionDigits: 0,
+                  minimumFractionDigits: 0,
+                }).format(totalSupply)}
+            </Typography>
+          </Paper>
         </Grid>
       </Grid>
-      
+
       <Grid container spacing={2} className="main-data-area">
         <Grid item lg={4} sm={12}>
           <div className="dune-card">
@@ -70,7 +73,7 @@ function Dashboard() {
             />
           </div>
         </Grid>
-        
+
         <Grid item lg={4} sm={12}>
           <div className="dune-card">
             <iframe
@@ -148,7 +151,6 @@ function Dashboard() {
           </div>
         </Grid>
       </Grid>
-      
     </div>
   );
 }
