37564c5b16a11dd1897d818552bf0d0b62180061
LolChocotaco <zayenx@gmail.com>
2021-07-09 11:31:42 -0700

    updated to ohmCirculatingSupply

--- src/actions/App.actions.js
+++ src/actions/App.actions.js
@@ -4,23 +4,20 @@ import { abi as OlympusStaking } from "../abi/OlympusStaking.json";
 import { abi as OlympusStakingv2 } from "../abi/OlympusStakingv2.json";
 import { abi as sOHM } from "../abi/sOHM.json";
 import { abi as sOHMv2 } from "../abi/sOhmv2.json";
-import axios from 'axios';
+import axios from "axios";
 import { contractForReserve, addressForAsset } from "../helpers";
 import { BONDS } from "../constants";
 import { abi as BondOhmDaiCalcContract } from "../abi/bonds/OhmDaiCalcContract.json";
 import apollo from "../lib/apolloClient.js";
-
 
 export const fetchAppSuccess = payload => ({
   type: Actions.FETCH_APP_SUCCESS,
   payload,
 });
 
-
 export const loadAppDetails =
   ({ networkID, provider }) =>
   async dispatch => {
-
     const protocolMetricsQuery = `
       query {
         _meta {
@@ -30,7 +27,7 @@ export const loadAppDetails =
         }
         protocolMetrics(first: 1, orderBy: timestamp, orderDirection: desc) {
           timestamp
-          circulatingSupply
+          ohmCirculatingSupply
           totalSupply
           ohmPrice
           marketCap
@@ -42,19 +39,28 @@ export const loadAppDetails =
 
     console.log(graphData);
 
+    if (!graphData || graphData == null) {
+      console.error("Returned a null response when querying TheGraph");
+      return;
+    }
+
     const stakingTVL = parseFloat(graphData.data.protocolMetrics[0].totalValueLocked);
     const marketPrice = parseFloat(graphData.data.protocolMetrics[0].ohmPrice);
     const marketCap = parseFloat(graphData.data.protocolMetrics[0].marketCap);
-    const circSupply = parseFloat(graphData.data.protocolMetrics[0].circulatingSupply);
+    const circSupply = parseFloat(graphData.data.protocolMetrics[0].ohmCirculatingSupply);
     const totalSupply = parseFloat(graphData.data.protocolMetrics[0].totalSupply);
     // const currentBlock = parseFloat(graphData.data._meta.block.number);
 
-    const currentBlock = await provider.getBlockNumber(); 
+    const currentBlock = await provider.getBlockNumber();
     const stakingContract = new ethers.Contract(addresses[networkID].STAKING_ADDRESS, OlympusStakingv2, provider);
     const oldStakingContract = new ethers.Contract(addresses[networkID].OLD_STAKING_ADDRESS, OlympusStaking, provider);
     const sohmMainContract = new ethers.Contract(addresses[networkID].SOHM_ADDRESS, sOHMv2, provider);
     const sohmOldContract = new ethers.Contract(addresses[networkID].OLD_SOHM_ADDRESS, sOHM, provider);
-    const bondCalculator = new ethers.Contract(addresses[networkID].BONDS.OHM_DAI_CALC, BondOhmDaiCalcContract, provider);
+    const bondCalculator = new ethers.Contract(
+      addresses[networkID].BONDS.OHM_DAI_CALC,
+      BondOhmDaiCalcContract,
+      provider,
+    );
 
     // Calculate Treasury Balance
     let token = contractForReserve({ bond: BONDS.dai, networkID, provider });
@@ -65,22 +71,22 @@ export const loadAppDetails =
 
     token = contractForReserve({ bond: BONDS.ohm_dai, networkID, provider });
     let ohmDaiAmount = await token.balanceOf(addresses[networkID].TREASURY_ADDRESS);
-    let valuation    = await bondCalculator.valuation(addressForAsset({bond: BONDS.ohm_dai, networkID}), ohmDaiAmount);
-    let markdown     = await bondCalculator.markdown(addressForAsset({bond: BONDS.ohm_dai, networkID}));
-    let ohmDaiUSD    = (valuation / Math.pow(10, 9)) * (markdown / Math.pow(10, 18))
+    let valuation = await bondCalculator.valuation(addressForAsset({ bond: BONDS.ohm_dai, networkID }), ohmDaiAmount);
+    let markdown = await bondCalculator.markdown(addressForAsset({ bond: BONDS.ohm_dai, networkID }));
+    let ohmDaiUSD = (valuation / Math.pow(10, 9)) * (markdown / Math.pow(10, 18));
 
     token = contractForReserve({ bond: BONDS.ohm_frax, networkID, provider });
     let ohmFraxAmount = await token.balanceOf(addresses[networkID].TREASURY_ADDRESS);
-    valuation    = await bondCalculator.valuation(addressForAsset({bond: BONDS.ohm_frax, networkID}), ohmFraxAmount);
-    markdown     = await bondCalculator.markdown(addressForAsset({bond: BONDS.ohm_frax, networkID}));
-    let ohmFraxUSD   = (valuation / Math.pow(10, 9)) * (markdown / Math.pow(10, 18))
+    valuation = await bondCalculator.valuation(addressForAsset({ bond: BONDS.ohm_frax, networkID }), ohmFraxAmount);
+    markdown = await bondCalculator.markdown(addressForAsset({ bond: BONDS.ohm_frax, networkID }));
+    let ohmFraxUSD = (valuation / Math.pow(10, 9)) * (markdown / Math.pow(10, 18));
 
-    const treasuryBalance  = daiAmount / Math.pow(10, 18) + fraxAmount / Math.pow(10,18) + ohmDaiUSD + ohmFraxUSD;
+    const treasuryBalance = daiAmount / Math.pow(10, 18) + fraxAmount / Math.pow(10, 18) + ohmDaiUSD + ohmFraxUSD;
 
     // Calculating staking
     const epoch = await stakingContract.epoch();
     const stakingReward = epoch.distribute;
-    const circ =  await sohmMainContract.circulatingSupply();
+    const circ = await sohmMainContract.circulatingSupply();
     const stakingRebase = stakingReward / circ;
     const fiveDayRate = Math.pow(1 + stakingRebase, 5 * 3) - 1;
     const stakingAPY = Math.pow(1 + stakingRebase, 365 * 3) - 1;
@@ -113,11 +119,10 @@ export const loadAppDetails =
     );
   };
 
-export const getFraxData = () =>
-  async dispatch => {
-    const resp = await axios.get('https://api.frax.finance/combineddata/');
-    return dispatch({
-      type: Actions.FETCH_FRAX_SUCCESS,
-      payload: resp.data && resp.data.liq_staking && resp.data.liq_staking["Uniswap FRAX/OHM"]
-    })
-  };
+export const getFraxData = () => async dispatch => {
+  const resp = await axios.get("https://api.frax.finance/combineddata/");
+  return dispatch({
+    type: Actions.FETCH_FRAX_SUCCESS,
+    payload: resp.data && resp.data.liq_staking && resp.data.liq_staking["Uniswap FRAX/OHM"],
+  });
+};
