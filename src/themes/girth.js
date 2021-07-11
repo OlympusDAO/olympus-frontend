c06da90f719205b7663713c450070a9e3a033df8
Girth Brooks <girth.eth@gmail.com>
2021-07-11 03:58:48 -0400

    girth mode fixes

--- src/themes/girth.js
+++ src/themes/girth.js
@@ -1,7 +1,8 @@
 import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
 import fonts from "./fonts";
+import commonSettings from "./global.js";
 
-export const gTheme = {
+export const girthTheme = {
   color: "#EFEFEF",
   gold: "#F8CC82",
   textHighlightColor: "#F4D092",
@@ -9,165 +10,147 @@ export const gTheme = {
   background: "linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)",
   paperBg: "#4242426A",
   modalBg: "#F9F9F9AA",
-  primaryBtnBg: "#759AAE",
-  primaryBtnBgHover: "#93AEBC",
+  primaryButtonBG: "#759AAE",
+  primaryButtonBGHover: "#93AEBC",
   largeTextColor: "#759AAE",
   activeLinkColor: "#222222",
   CTAButtonBorder: "2px solid #FFF",
-  CTAButtonBorderColor: "linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)",
+  secondaryButtonBG: "linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)",
+  secondaryyButtonBGHover: "#93AEBC",
 };
 
 export const girth = responsiveFontSizes(
-  createMuiTheme({
-    typography: {
-      fontSize: 16,
-      fontFamily: "Square",
+  createMuiTheme(
+    {
+      primary: {
+        main: girthTheme.color,
+      },
+      palette: {
+        type: "dark",
+        background: {
+          default: girthTheme.backgroundColor,
+          paper: girthTheme.paperBg,
+        },
+        contrastText: girthTheme.color,
+        primary: {
+          main: girthTheme.color,
+        },
+        neutral: {
+          main: girthTheme.color,
+          secondary: girthTheme.gray,
+        },
+        text: {
+          primary: girthTheme.color,
+          secondary: girthTheme.gray,
+        },
+      },
+      typography: {
+        fontFamily: "Square",
+      },
       props: {
-        gutterBottom: false,
+        MuiSvgIcon: {
+          htmlColor: girthTheme.color,
+        },
       },
-      h3: {
-        padding: "10px",
-      },
-      h4: {
-        margin: "10px",
-      },
-      h5: {
-        fontWeight: "bold",
-      },
-    },
-    palette: {
-      type: "light",
-      background: {
-        default: gTheme.backgroundColor,
-        paper: gTheme.paperBg,
-      },
-      contrastText: "#FAFAFA",
-    },
-    overrides: {
-      MuiCssBaseline: {
-        "@global": {
-          "@font-face": fonts,
-          body: {
-            background: gTheme.background,
-            backgroundRepeat: "no-repeat",
-            backgroundAttachment: "fixed",
+      overrides: {
+        MuiCssBaseline: {
+          "@global": {
+            "@font-face": fonts,
+            body: {
+              background: girthTheme.background,
+            },
           },
         },
-      },
-      MuiContainer: {
-        root: {
-          backgroundColor: "transparent",
-          flexGrow: 1,
-        },
-      },
-      MuiPaper: {
-        root: {
-          padding: "20px 20px 20px 20px", // global
-          backdropFilter: "blur(60px)", // global
-          borderRadius: "5px", // global
-          maxWidth: "869px",
-        },
-      },
-      MuiDrawer: {
-        root: {
-          width: "280px",
-          flexShrink: 0,
-        },
-        paper: {
-          width: "inherit",
-          backgroundColor: "#00000000",
-          padding: 0,
-          square: true,
-          rounded: false,
-        },
-      },
-      MuiLink: {
-        root: {
-          color: gTheme.color,
-          "&:hover": {
-            borderColor: gTheme.color,
-            cursor: "pointer",
-            color: gTheme.color,
-          },
-          ".active": {
-            color: gTheme.textHighlightColor,
+        MuiPaper: {
+          "&.ohm-modal": {
+            backgroundColor: girthTheme.modalBg,
           },
         },
-      },
-      MuiTableRow: {
-        head: {
-          color: "#999999",
-        },
-      },
-      MuiTableCell: {
-        root: {
-          borderBottom: 0,
-          color: gTheme.color,
-        },
-      },
-      MuiToggleButton: {
-        root: {
-          backgroundColor: gTheme.paperBg,
-        },
-      },
-      MuiButton: {
-        root: {
-          borderRadius: "5px",
-        },
-        containedPrimary: {
-          color: "#FAFAFA",
-          backgroundColor: gTheme.primaryBtnBg,
-          "&:hover": {
-            backgroundColor: gTheme.primaryBtnBgHover,
+        MuiLink: {
+          root: {
+            color: girthTheme.color,
+            "&:hover": {
+              color: girthTheme.textHighlightColor,
+              textDecoration: "none",
+              "&.active": {
+                color: girthTheme.color,
+              },
+            },
+            "&.active": {
+              color: girthTheme.color,
+              textDecoration: "underline",
+            },
           },
         },
-        containedSecondary: {
-          backgroundColor: gTheme.paperBg,
-          color: gTheme.color,
-        },
-        outlinedPrimary: {
-          color: gTheme.gold,
-          borderColor: gTheme.gold,
-          textTransform: "none",
-          textDecoration: "none",
-        },
-        outlinedSecondary: {
-          color: gTheme.color,
-          borderColor: gTheme.color,
-          textTransform: "none",
-          textDecoration: "none",
-        },
-        textPrimary: {
-          color: "#A3A3A3",
-          "&:hover": {
-            color: gTheme.gold,
-            backgroundColor: "#00000000",
-          },
-          "&:active": {
-            color: gTheme.gold,
-            borderBottom: "#F8CC82",
+        MuiTableCell: {
+          root: {
+            color: girthTheme.color,
           },
         },
-        textSecondary: {
-          color: gTheme.color,
-          textTransform: "none",
-          padding: "2px 2px",
-          "&:hover": {
-            color: gTheme.textHighlightColor,
-            backgroundColor: "#00000000",
+        MuiToggleButton: {
+          root: {
+            backgroundColor: girthTheme.paperBg,
+            "&:hover": {
+              backgroundColor: girthTheme.secondaryButtonHoverBG,
+            },
+            selected: {
+              backgroundColor: girthTheme.secondaryButtonHoverBG,
+            },
+          },
+        },
+        MuiButton: {
+          containedPrimary: {
+            color: girthTheme.color,
+            backgroundColor: girthTheme.primaryButtonBG,
+            "&:hover": {
+              backgroundColor: girthTheme.primaryButtonHoverBG,
+              color: girthTheme.primaryButtonHoverColor,
+            },
+          },
+          containedSecondary: {
+            backgroundColor: girthTheme.paperBg,
+            color: girthTheme.color,
+            "&:hover": {
+              backgroundColor: girthTheme.secondaryButtonHoverBG,
+            },
+          },
+          outlinedPrimary: {
+            color: girthTheme.gold,
+            borderColor: girthTheme.gold,
+            "&:hover": {
+              color: girthTheme.outlinedPrimaryButtonHoverColor,
+              backgroundColor: girthTheme.primaryButtonHoverBG,
+            },
+          },
+          outlinedSecondary: {
+            color: girthTheme.color,
+            borderColor: girthTheme.color,
+            "&:hover": {
+              color: girthTheme.outlinedSecondaryButtonHoverColor,
+              backgroundColor: girthTheme.outlinedSecondaryButtonHoverBG,
+              borderColor: "#333333",
+            },
+          },
+          textPrimary: {
+            color: "#A3A3A3",
+            "&:hover": {
+              color: girthTheme.gold,
+              backgroundColor: "#00000000",
+            },
+            "&:active": {
+              color: girthTheme.gold,
+              borderBottom: "#F8CC82",
+            },
+          },
+          textSecondary: {
+            color: girthTheme.color,
+            "&:hover": {
+              color: girthTheme.textHighlightColor,
+            },
           },
         },
       },
     },
-    props: {
-      MuiButton: {
-        disableElevation: true,
-        disableFocusRipple: true,
-        disableRipple: true,
-      },
-      MuiPaper: {
-        elevation: 0,
-      },
-    },
-  }),
+    commonSettings,
+  ),
 );
