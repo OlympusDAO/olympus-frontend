1487110a68c003622c693e15d5ad0e41d21d6104
appleseed-iii <appleseed-iii@protonmail.com>
2021-08-19 00:32:07 -0500

    update Migrate sOHM per Issue OlympusDAO#336

--- src/views/Stake/Stake.jsx
+++ src/views/Stake/Stake.jsx
@@ -164,26 +164,17 @@ function Stake() {
                 <RebaseTimer />
 
                 {address && oldSohmBalance > 0.01 && (
-                  <Link
+                  <a
                     className="migrate-sohm-button"
                     component={NavLink}
-                    to="/stake/migrate"
+                    style={{ textDecoration: "none" }}
+                    href="https://docs.olympusdao.finance/using-the-website/migrate"
                     aria-label="migrate-sohm"
+                    target="_blank"
                   >
                     <NewReleases viewBox="0 0 24 24" />
-                    <Typography>Migrate sOHM</Typography>
-                  </Link>
-                )}
-                {address && oldSohmBalance < 0.01 && (
-                  <Link
-                    component={NavLink}
-                    to="/stake/migrate"
-                    className="migrate-sohm-button complete"
-                    aria-label="migrate-sohm-complete"
-                  >
-                    <CheckCircleIcon viewBox="0 0 24 24" />
-                    <Typography>sOHM Migrated</Typography>
-                  </Link>
+                    <Typography>Migrate sOHM!</Typography>
+                  </a>
                 )}
               </div>
             </Grid>
