a6e6f0771a5cad90d7936f439f4159fe216aa59e
appleseed-iii <80423742+appleseed-iii@users.noreply.github.com>
2021-08-27 10:07:07 -0500

    Key Update for PR 382 & 381 (#390)
    * Key Update for PR 382
    
    - Add fresh API key to monitor request volume changes as a result of PR 382
    
    * update comment

--- src/hooks/web3Context.tsx
+++ src/hooks/web3Context.tsx
@@ -32,7 +32,8 @@ const defaultApiKey = "https://eth-mainnet.alchemyapi.io/v2/_gg7wSSi0KMBsdKnGVfH
 
 const TEMP_ALCHEMY_IDS = [
   // "rZD4Q_qiIlewksdYFDfM3Y0mzZy-8Naf", // appleseed-temp1
-  "9GOp6SIgE0en92i3r0JSvxccZ0N2idmO", // appleseed-temp2
+  // "9GOp6SIgE0en92i3r0JSvxccZ0N2idmO", // appleseed-temp2
+  "j0QUyceqxu31tQrAQSotL2YMqmuzoGPh", // appleseed-temp3
 ];
 function getAlchemyAPI(chainID: Number) {
   const randomIndex = Math.floor(Math.random() * ALCHEMY_ID_LIST.length);
