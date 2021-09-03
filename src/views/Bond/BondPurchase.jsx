990bfa0a85db10f24d800d1e9b8a352c08ee1c07
spaceturtleship <spaceturtleship@protonmail.com>
2021-09-03 16:22:15 +0100

    Add timer to update bond details at a set interval (currently 60 seconds)

--- src/views/Bond/BondPurchase.jsx
+++ src/views/Bond/BondPurchase.jsx
@@ -18,11 +18,13 @@ import { isPendingTxn, txnButtonText } from "src/slices/PendingTxnsSlice";
 import { Skeleton } from "@material-ui/lab";
 
 function BondPurchase({ bond, slippage }) {
+  const SECONDS_TO_REFRESH = 60;
   const dispatch = useDispatch();
   const { provider, address, chainID } = useWeb3Context();
 
   const [recipientAddress, setRecipientAddress] = useState(address);
   const [quantity, setQuantity] = useState("");
+  const [secondsToRefresh, setSecondsToRefresh] = useState(SECONDS_TO_REFRESH);
 
   const currentBlock = useSelector(state => {
     return state.app.currentBlock;
@@ -90,6 +92,20 @@ function BondPurchase({ bond, slippage }) {
   useEffect(() => {
     if (address) setRecipientAddress(address);
   }, [provider, quantity, address]);
+
+  useEffect(() => {
+    let interval = null;
+    if (secondsToRefresh > 0) {
+      interval = setInterval(() => {
+        setSecondsToRefresh(secondsToRefresh => secondsToRefresh - 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+      dispatch(calcBondDetails({ bond, value: quantity, provider, networkID: chainID }));
+      setSecondsToRefresh(SECONDS_TO_REFRESH);
+    }
+    return () => clearInterval(interval);
+  }, [secondsToRefresh, quantity]);
 
   const onSeekApproval = async () => {
     await dispatch(changeApproval({ address, bond, provider, networkID: chainID }));
