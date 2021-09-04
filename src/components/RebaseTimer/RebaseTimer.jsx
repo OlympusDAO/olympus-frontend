caac146de5a1321dbd4bd985eb674006571697b5
spaceturtleship <spaceturtleship@protonmail.com>
2021-09-04 10:24:17 +0100

    Use state.app.currentBlock to initialize time to next rebase display, use an interval to decrement the value every 60 seconds to keep display up to date

--- src/components/RebaseTimer/RebaseTimer.jsx
+++ src/components/RebaseTimer/RebaseTimer.jsx
@@ -3,28 +3,50 @@ import { getRebaseBlock, secondsUntilBlock, prettifySeconds } from "../../helper
 import { Box, Typography } from "@material-ui/core";
 import "./rebasetimer.scss";
 import { Skeleton } from "@material-ui/lab";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 function RebaseTimer() {
+  const SECONDS_TO_REFRESH = 60;
+  const [secondsToRebase, setSecondsToRebase] = useState(0);
+  const [secondsToRefresh, setSecondsToRefresh] = useState(SECONDS_TO_REFRESH);
+
   const currentBlock = useSelector(state => {
     return state.app.currentBlock;
   });
 
-  const timeUntilRebase = useMemo(() => {
+  // This initializes secondsToRebase as soon as currentBlock becomes available
+  useMemo(() => {
     if (currentBlock) {
       const rebaseBlock = getRebaseBlock(currentBlock);
       const seconds = secondsUntilBlock(currentBlock, rebaseBlock);
-      return prettifySeconds(seconds);
+      setSecondsToRebase(seconds);
+      debugger;
     }
   }, [currentBlock]);
+
+  // After every period SECONDS_TO_REFRESH, decrement secondsToRebase by SECONDS_TO_REFRESH,
+  // keeping the display up to date without requiring an on chain request to update currentBlock.
+  useEffect(() => {
+    let interval = null;
+    if (secondsToRefresh > 0) {
+      interval = setInterval(() => {
+        setSecondsToRefresh(secondsToRefresh => secondsToRefresh - 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+      setSecondsToRebase(secondsToRebase => secondsToRebase - SECONDS_TO_REFRESH);
+      setSecondsToRefresh(SECONDS_TO_REFRESH);
+    }
+    return () => clearInterval(interval);
+  }, [secondsToRebase, secondsToRefresh]);
 
   return (
     <Box className="rebase-timer">
       <Typography variant="body2">
         {currentBlock ? (
-          timeUntilRebase ? (
+          setSecondsToRebase ? (
             <>
-              <strong>{timeUntilRebase}</strong> to next rebase
+              <strong>{prettifySeconds(secondsToRebase)}</strong> to next rebase
             </>
           ) : (
             <strong>rebasing</strong>
