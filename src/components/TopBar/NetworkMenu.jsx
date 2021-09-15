8d1ff8a5dca23559a668d3295c86c26a3529ac7a
spaceturtleship <spaceturtleship@protonmail.com>
2021-09-15 16:10:47 +0100

    Add logos to NetworkMenu button

--- src/components/TopBar/NetworkMenu.jsx
+++ src/components/TopBar/NetworkMenu.jsx
@@ -2,18 +2,27 @@ import { Button, Link, Typography } from "@material-ui/core";
 import { NavLink } from "react-router-dom";
 import Box from "@material-ui/core/Box";
 import { useWeb3Context } from "../../hooks/web3Context";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import "./networkmenu.scss";
+import ethereum from "../../assets/ethereum.jpg";
+import arbitrum from "../../assets/arbitrum.jpg";
 
 function NetworkMenu() {
   const { chainName } = useWeb3Context();
   const [name, setName] = useState("");
+  const [image, setImage] = useState();
 
   useEffect(() => {
     setName(chainName);
+    if (chainName === "Ethereum") {
+      setImage(ethereum);
+    } else if (chainName === "Arbitrum") {
+      setImage(arbitrum);
+    }
   }, [chainName]);
 
   return (
-    <Box component="div">
+    <Box component="div" className="network-menu-container">
       <Button
         aria-label="change-network"
         size="large"
@@ -22,8 +31,10 @@ function NetworkMenu() {
         title="Change Network"
         component={NavLink}
         to="/network"
+        className="network-menu-button"
       >
-        <Typography>{name}</Typography>
+        <img src={image} alt={chainName} height="40px" width="40px" />
+        <Typography className="network-menu-button-content">{name}</Typography>
       </Button>
     </Box>
   );
