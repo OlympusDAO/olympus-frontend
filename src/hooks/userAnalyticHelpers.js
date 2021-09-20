bd285babe1276ca36adfbf8725b95bb14148b0e8
Erick Delgado <taken404@gmail.com>
2021-09-20 13:53:20 -0400

    removing analytic push to google
    discussing service to use today sep20

--- src/hooks/userAnalyticHelpers.js
+++ src/hooks/userAnalyticHelpers.js
@@ -3,8 +3,4 @@ import ReactGA from "react-ga4";
 // Pushing data to google analytics
 export function ua(data) {
   const stringifiedData = JSON.stringify(data);
-  ReactGA.event({
-    category: data.txType,
-    action: stringifiedData,
-  });
 }
