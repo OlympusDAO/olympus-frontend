2a18e7ae46df5994300eab912230bc83905854eb
appleseed-iii <appleseed-iii@protonmail.com>
2021-09-21 10:18:53 -0500

    fix BondRedeem button on ClaimRow
    1. fix BondRedeem button by correcting bond.name lookup
    2. remove stale BondRedeemV1.jsx, was no longer used anywhere
    3. remove `accountBonds` useEffect from ChooseBond, wasn't doing anything except console.logs
    4. add skeleton to vestingPeriod

--- src/views/ChooseBond/ClaimRow.jsx
+++ src/views/ChooseBond/ClaimRow.jsx
@@ -16,8 +16,10 @@ export function ClaimBondTableData({ userBond }) {
   const { bonds } = useBonds();
   const { address, chainID, provider } = useWeb3Context();
 
-  const bondName = userBond[0];
   const bond = userBond[1];
+  const bondName = bond.bond;
+
+  const isAppLoading = useSelector(state => state.app.loading ?? true);
 
   const currentBlock = useSelector(state => {
     return state.app.currentBlock;
@@ -33,7 +35,6 @@ export function ClaimBondTableData({ userBond }) {
 
   async function onRedeem({ autostake }) {
     let currentBond = bonds.find(bnd => bnd.name === bondName);
-    console.log(currentBond);
     await dispatch(redeemBond({ address, bond: currentBond, networkID: chainID, provider, autostake }));
   }
 
@@ -52,7 +53,7 @@ export function ClaimBondTableData({ userBond }) {
       </TableCell>
       <TableCell align="center">{bond.interestDue ? trim(bond.interestDue, 4) : <Skeleton width={100} />}</TableCell>
       <TableCell align="right" style={{ whiteSpace: "nowrap" }}>
-        {vestingPeriod()}
+        {isAppLoading ? <Skeleton /> : vestingPeriod()}
       </TableCell>
       <TableCell align="right">
         <Button
