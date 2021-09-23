11c61db52dbff6a05640a00a4f556e29ea2d983f
appleseed-iii <80423742+appleseed-iii@users.noreply.github.com>
2021-09-23 14:54:40 -0500

    FIX: overlooked indexOf === 0 for websocket detection (#465)
    * FIX: overlooked indexOf === 0 for websocket detection
    
    * remove console log

--- src/hooks/web3Context.tsx
+++ src/hooks/web3Context.tsx
@@ -73,7 +73,7 @@ export const Web3ContextProvider: React.FC<{ children: ReactElement }> = ({ chil
 
   // if websocket we need to change providerType
   const providerType = () => {
-    if (uri.indexOf("ws://") > 0 || uri.indexOf("wss://") > 0) {
+    if (uri.indexOf("ws://") === 0 || uri.indexOf("wss://") === 0) {
       return new WebSocketProvider(uri);
     } else {
       return new StaticJsonRpcProvider(uri);
