92078bad0bce3808dab9b28211d5ececdd6b80a9
appleseed-iii <appleseed-iii@protonmail.com>
2021-09-28 09:56:01 -0500

    Bug Fix: Handle no Segment Analytics blocking action completion

--- src/helpers/userAnalyticHelpers.js
+++ src/helpers/userAnalyticHelpers.js
@@ -1,8 +1,11 @@
 // Pushing data to segment analytics
 export function segmentUA(data) {
   const stringifiedData = JSON.stringify(data);
-  var analytics = (window.analytics = window.analytics || []);
-  analytics.track(data.type, {
-    data,
-  });
+  var analytics = (window.analytics = window.analytics);
+  // NOTE (appleseed): the analytics object may not exist (if there is no SEGMENT_API_KEY)
+  if (analytics) {
+    analytics.track(data.type, {
+      data,
+    });
+  }
 }
