4ce125490bf48c69685bad5418e2b3c356a9225d
Bruno Ilponse <bruno.ilponse@gmail.com>
2021-10-03 10:56:18 +0200

    Fixed connectbutton compilation issue

--- src/components/ConnectButton.jsx
+++ src/components/ConnectButton.jsx
@@ -1,5 +1,6 @@
 import { Button } from "@material-ui/core";
 import { useWeb3Context } from "src/hooks/web3Context";
+import { Trans } from "@lingui/macro";
 
 const ConnectButton = () => {
   const { connect } = useWeb3Context();
