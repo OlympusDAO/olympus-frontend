80e7606e89f472e264e1d4883e9975f378188d03
0xJem <0x0xJem@gmail.com>
2021-10-06 00:44:25 +0500

    Proper implementation of async/await for country lookup

--- src/helpers/userAnalyticHelpers.js
+++ src/helpers/userAnalyticHelpers.js
@@ -5,33 +5,24 @@ import { retrieveUTMQueryParameters } from "./QueryParameterHelper";
  * Obtain country from IP address
  * @returns the country name or an empty string
  */
-export function countryLookup() {
+async function countryLookup() {
   // Determine the country the user is from, based on IP
   // Geoapify offers 3000 lookups/day, so we should be fine
   var apiKey = EnvHelper.getGeoapifyAPIKey();
 
   if (!apiKey) return "";
 
-  var country = fetch("https://api.geoapify.com/v1/ipinfo?apiKey=" + apiKey, {
+  var response = await fetch("https://api.geoapify.com/v1/ipinfo?apiKey=" + apiKey, {
     method: "GET",
-  })
-    .then(function (response) {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw "Unable to determine country from IP lookup: " + response.body;
-      }
-    })
-    .then(function (json) {
-      return json.country.name;
-    })
-    .catch(function (error) {
-      console.error(error);
-      // Set the country to a default value
-      return "";
-    });
+  });
 
-  return country;
+  if (!response.ok) {
+    console.error("Unable to determine country from IP lookup: " + response.body);
+    return "";
+  }
+
+  var json = await response.json();
+  return json.country.name;
 }
 
 // Pushing data to segment analytics
@@ -40,8 +31,7 @@ export function segmentUA(data) {
   var analytics = (window.analytics = window.analytics);
 
   // We don't record the IP for privacy reasons, so we capture the country instead
-  var country = countryLookup();
-  data.country = country;
+  countryLookup().then(country => (data.country = country));
 
   // Ensure that any UTM query parameters are sent along to Segment
   // var queryParameters = retrieveUTMQueryParameters();
