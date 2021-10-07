c8eb446dc53edbe529749054c6e8c0754baba1b1
appleseed-iii <appleseed-iii@protonmail.com>
2021-10-07 15:23:16 -0500

    ohm_lusd going live

--- src/helpers/AllBonds.ts
+++ src/helpers/AllBonds.ts
@@ -171,7 +171,7 @@ export const ohm_lusd = new LPBond({
 // Is it a stableCoin bond? use `new StableBond`
 // Is it an LP Bond? use `new LPBond`
 // Add new bonds to this array!!
-export const allBonds = [dai, frax, eth, ohm_dai, ohm_frax, lusd];
+export const allBonds = [dai, frax, eth, ohm_dai, ohm_frax, lusd, ohm_lusd];
 export const allBondsMap = allBonds.reduce((prevVal, bond) => {
   return { ...prevVal, [bond.name]: bond };
 }, {});
