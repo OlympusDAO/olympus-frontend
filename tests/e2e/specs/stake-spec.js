1a9182c8e6a717f6466a4199b53adbd1baade52c
joey <joeyr84@gmail.com>
2021-10-07 12:14:44 -0500

    wip

--- tests/e2e/specs/stake-spec.js
+++ tests/e2e/specs/stake-spec.js
@@ -20,15 +20,15 @@ describe("Stake tests", () => {
     cy.confirmMetamaskPermissionToSpend();
     // Stake and check that balance changed accordingly
     cy.get("#user-balance").then($p => {
+      cy.log($p.text());
       const balance_before_txt = $p.text();
+      cy.log(balance_before_txt);
       const target_balance = ohmRound(ohmRound(balance_before_txt) - STAKE_AMOUNT);
       // Stake
-      // can/should we chain this contains + click?
       cy.get(".stake-button").contains("Stake OHM");
       cy.get(".stake-button").click();
-      // check wallet button says "in progress"
-      cy.get("#wallet-button").contains("In progress");
       cy.confirmMetamaskTransaction();
+      cy.get("#wallet-button").contains("In progress");
       cy.get(".stake-button").contains("Pending...");
       // Wait for balance to change
       cy.get("#user-balance")
@@ -39,20 +39,37 @@ describe("Stake tests", () => {
           expect(balance_after).to.eq(target_balance);
         });
     });
-    // TODO
-    it(`Unstakes ${STAKE_AMOUNT} sOHM`, () => {
-      cy.get("#wallet-button span").contains("Connect Wallet");
-      cy.get("#wallet-button").click();
-      cy.get(".web3modal-provider-name").contains("MetaMask").click();
-      cy.acceptMetamaskAccess();
-      cy.get(".stake-button").contains("Approve");
-      cy.get("#amount-input").type(STAKE_AMOUNT);
+    assert(true);
+  });
+  it(`Unstakes ${STAKE_AMOUNT} sOHM`, () => {
+    cy.get("#wallet-button span").contains("Connect Wallet");
+    cy.get("#wallet-button").click();
+    cy.get(".web3modal-provider-name").contains("MetaMask").click();
+    // cy.acceptMetamaskAccess();
+    cy.get("#simple-tab-1").click();
+    cy.get(".stake-button").contains("Approve");
+    cy.get("#amount-input").type(STAKE_AMOUNT);
+    cy.get(".stake-button").click();
+    cy.confirmMetamaskPermissionToSpend();
+    // Unstake
+    cy.get("#user-balance").then($p => {
+      cy.log($p.text());
+      const balance_before_txt = $p.text();
+      const target_balance = ohmRound(ohmRound(balance_before_txt) + STAKE_AMOUNT);
+      // Wait for balance to change
+      cy.get(".stake-button").contains("Unstake OHM");
+      cy.get(".stake-button").click();
+      cy.confirmMetamaskTransaction();
+      cy.get("#wallet-button").contains("In progress");
+      cy.get(".stake-button").contains("Pending...");
+      cy.get("#user-balance")
+        .not(`:contains(${balance_before_txt})`)
+        .then($p => {
+          // Check new balance
+          const balance_after = ohmRound($p.text());
+          expect(balance_after).to.eq(target_balance);
+        });
     });
-    // TODO
-    // it(`disconnects wallet from app`, () => {
-
-    // })
-
     assert(true);
   });
 });
