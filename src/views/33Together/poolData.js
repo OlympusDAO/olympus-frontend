9cb7b0d26586539b9c4075d75e311612b467491f
appleseed-iii <appleseed-iii@protonmail.com>
2021-10-08 14:10:18 -0500

    change to mainnet address

--- src/views/33Together/poolData.js
+++ src/views/33Together/poolData.js
@@ -1,9 +1,13 @@
 // import apollo from "../../lib/apolloClient";
 
 // TODO: add paramaterization for testnet/mainnet pool ids
+// PT_PRIZE_POOL_ADDRESS
+// "0x60bc094cb0c966e60ed3be0549e92f3bc572e9f8"
+// 0xf9081132864ed5e4980cfae83bdb122d86619281 -> rinkeby
+// 0xeab695a8f5a44f583003a8bc97d677880d528248 -> mainnet
 export const poolDataQuery = `
 query {
-  prizePool(id: "0x60bc094cb0c966e60ed3be0549e92f3bc572e9f8") {
+  prizePool(id: "0xeab695a8f5a44f583003a8bc97d677880d528248") {
     id
     owner
     deactivated
