ded12e126b93d09746c2c54f84d5b4210c917135
0xJem <91760036+0xJem@users.noreply.github.com>
2021-10-14 21:00:37 +0500

    Convert the user's IP address into a country and send to Segment (#503)
    * Convert the user's IP address into a country and send to Segment
    * Remove redundant export
    * Refactor process.env call into EnvHelper
    * If there is no API key, don't throw an exception
    * Fix some stupid mistakes with return values
    * Proper implementation of async/await for country lookup
    Co-authored-by: Jeremy Higgs <jeremy@ecoenergy.global>

--- src/helpers/Environment.ts
+++ src/helpers/Environment.ts
@@ -105,4 +105,14 @@ export class EnvHelper {
     if (ALL_URIs.length === 0) console.error("API keys must be set in the .env");
     return ALL_URIs;
   }
+
+  static getGeoapifyAPIKey() {
+    var apiKey = EnvHelper.env.REACT_APP_GEOAPIFY_API_KEY;
+    if (!apiKey) {
+      console.warn("Missing REACT_APP_GEOAPIFY_API_KEY environment variable");
+      return null;
+    }
+
+    return apiKey;
+  }
 }
