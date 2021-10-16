b87881d5ce799aa919f89a6e75abebe6304ded71
Bruno Ilponse <bruno.ilponse@gmail.com>
2021-10-16 08:37:53 +0200

    More refactoring

--- src/views/Bond/bond.test.js
+++ src/views/Bond/bond.test.js
@@ -1,34 +1,11 @@
-import { render, screen, waitFor } from "../../../tests/unit/utils";
-import handlers from "../../../tests/unit/handlers";
+import { renderRoute, setup, screen, waitFor } from "../../../tests/unit/utils";
 import "@testing-library/jest-dom";
-import { setupServer } from "msw/node";
 import { getByTestId } from "@testing-library/dom";
-import { Router } from "react-router-dom";
-import { createMemoryHistory } from "history";
-import App from "../../App";
 
-const server = setupServer(...handlers);
+setup();
 
-// Enable API mocking before tests.
-beforeAll(() => server.listen());
-
-// Reset any runtime request handlers we may add during the tests.
-afterEach(() => server.resetHandlers());
-
-// Disable API mocking after the tests are done.
-afterAll(() => server.close());
-
-jest.setTimeout(10000);
 test("the bond treasury balance and OHM price", async () => {
-  // we should probably just be rendering the <Bond /> component
-  const history = createMemoryHistory();
-  const route = "/bonds";
-  history.push(route);
-  render(
-    <Router history={history}>
-      <App />
-    </Router>,
-  );
+  renderRoute("/bonds");
   expect(screen.getByTestId("treasury-balance")).toBeInTheDocument();
   /*
   await waitFor(
--- src/views/Stake/stake.test.js
+++ src/views/Stake/stake.test.js
@@ -1,60 +1,10 @@
-import { render, screen, waitFor } from "../../../test-utils";
+import { renderRoute, screen, waitFor, setup } from "../../../tests/unit/utils";
 import "@testing-library/jest-dom";
-import { rest } from "msw";
-import { setupServer } from "msw/node";
 import { getByTestId } from "@testing-library/dom";
-import Stake from "./Stake.jsx";
-import App from "../../App";
 
-// We use msw to intercept the network request during the test
-export const handlers = [
-  rest.post("https://api.thegraph.com/subgraphs/name/drondin/olympus-graph", (req, res, ctx) => {
-    return res(
-      ctx.json({
-        data: {
-          _meta: {
-            __typename: "_Meta_",
-            block: {
-              __typename: "_Block_",
-              number: 13404804,
-            },
-          },
-          protocolMetrics: [
-            {
-              __typename: "ProtocolMetric",
-              marketCap: "2850404741.408880328568714963827889",
-              nextDistributedOhm: "9536.348673867",
-              nextEpochRebase: "0.3984399885022833379898967937151319",
-              ohmCirculatingSupply: "2606164.580244948",
-              ohmPrice: "1093.716322835212771836103791185812",
-              sOhmCirculatingSupply: "2393421.581431541",
-              timestamp: "1633996882",
-              totalSupply: "3217915.76256185",
-              totalValueLocked: "2617724251.037744790511990363550964",
-            },
-          ],
-        },
-      }),
-      // ctx.delay(150),
-    );
-  }),
-];
-
-const server = setupServer(...handlers);
-
-// Enable API mocking before tests.
-beforeAll(() => server.listen());
-
-// Reset any runtime request handlers we may add during the tests.
-afterEach(() => server.resetHandlers());
-
-// Disable API mocking after the tests are done.
-afterAll(() => server.close());
-
-jest.setTimeout(60000);
+setup();
 test("the stake page APY, TVL, and Index", async () => {
-  // we should probably just be rendering the <Stake /> component
-  render(<App />);
+  renderRoute("/stake");
   expect(screen.getByTestId("apy-loading")).toBeInTheDocument();
   expect(screen.getByTestId("tvl-loading")).toBeInTheDocument();
   expect(screen.getByTestId("index-loading")).toBeInTheDocument();
