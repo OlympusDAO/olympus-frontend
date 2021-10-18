3dbaa8d210600b36bdee094ef21f83cbf2bc7767
casuwu <taken404@gmail.com>
2021-10-18 14:13:56 -0400

    Revert "Location tracking"
    This reverts commit 6796269d033aa1d061e68810af8edddc25e47e03.

--- src/helpers/userAnalyticHelpers.js
+++ src/helpers/userAnalyticHelpers.js
@@ -6,31 +6,37 @@ import { retrieveUTMQueryParameters } from "./QueryParameterHelper";
  * @returns the country name or an empty string
  */
 async function countryLookup() {
-  return new Promise(async (resolve, reject) => {
-    const apiKey = EnvHelper.getGeoapifyAPIKey();
-    if (!apiKey) return "";
-    const response = await fetch(`https://api.geoapify.com/v1/ipinfo?apiKey=${apiKey}`, {
-      method: "GET",
-    });
+  // Determine the country the user is from, based on IP
+  // Geoapify offers 3000 lookups/day, so we should be fine
+  var apiKey = EnvHelper.getGeoapifyAPIKey();
 
-    if (!response.ok) {
-      console.error("Unable to determine country from IP lookup: " + response.body);
-      return resolve("Undefined");
-    }
-    console.log(response);
-    const json = await response.json();
-    resolve(json.country.name);
+  if (!apiKey) return "";
+
+  var response = await fetch("https://api.geoapify.com/v1/ipinfo?apiKey=" + apiKey, {
+    method: "GET",
   });
+
+  if (!response.ok) {
+    console.error("Unable to determine country from IP lookup: " + response.body);
+    return "";
+  }
+
+  var json = await response.json();
+  return json.country.name;
 }
 
 // Pushing data to segment analytics
-export async function segmentUA(data) {
-  let analytics = (window.analytics = window.analytics);
-  const country = await countryLookup();
-  data.country = country;
-  const queryParameters = retrieveUTMQueryParameters();
-  const combinedData = Object.assign({}, data, queryParameters);
+export function segmentUA(data) {
+  var analytics = (window.analytics = window.analytics);
 
+  // We don't record the IP for privacy reasons, so we capture the country instead
+  countryLookup().then(country => (data.country = country));
+
+  // Ensure that any UTM query parameters are sent along to Segment
+  var queryParameters = retrieveUTMQueryParameters();
+  var combinedData = Object.assign({}, data, queryParameters);
+
+  // NOTE (appleseed): the analytics object may not exist (if there is no SEGMENT_API_KEY)
   try {
     analytics.track(
       data.type,
