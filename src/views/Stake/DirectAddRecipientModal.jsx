c03b6e09015efae3a10acd8b50b478b2d02bd449
0xJem <0x0xJem@gmail.com>
2021-10-20 01:09:26 +0500

    Improve styling of modal window. Add field for wallet address.

--- src/views/Stake/DirectAddRecipientModal.jsx
+++ src/views/Stake/DirectAddRecipientModal.jsx
@@ -1,4 +1,4 @@
-import { Modal, Paper, Typography, SvgIcon, Link } from "@material-ui/core";
+import { Modal, Paper, Typography, SvgIcon, Link, Button } from "@material-ui/core";
 import { FormControl } from "@material-ui/core";
 import { InputLabel } from "@material-ui/core";
 import { OutlinedInput } from "@material-ui/core";
@@ -13,12 +13,20 @@ export function DirectAddRecipientModal({ isModalHidden, setIsModalHidden }) {
   // e.g. 5% = 5
   const [percentage, setPercentage] = useState(0);
 
+  const [walletAddress, setWalletAddress] = useState("");
+
   const handleSetPercentage = value => {
+    // TODO finalise this
     console.log(value);
     setPercentage(value);
   };
 
-  // TODO styling
+  const handleSetWallet = value => {
+    // TODO finalise this
+    console.log(value);
+    setWalletAddress(value);
+  };
+
   // TODO add amount
   // TODO add wallet
   // TODO add validation
@@ -30,10 +38,10 @@ export function DirectAddRecipientModal({ isModalHidden, setIsModalHidden }) {
           <Link onClick={() => setIsModalHidden(true)}>
             <SvgIcon color="primary" component={XIcon} />
           </Link>
-          <Typography variant="h5">Add Yield Recipient</Typography>
+          <Typography variant="h4">Add Yield Recipient</Typography>
         </div>
-        % of Staked Position
-        <FormControl className="ohm-input" variant="outlined" color="primary">
+        <Typography variant="h5">% of Staked Position</Typography>
+        <FormControl className="modal-input" variant="outlined" color="primary">
           <InputLabel htmlFor="percentage-input"></InputLabel>
           <OutlinedInput
             id="percentage-input"
@@ -45,8 +53,26 @@ export function DirectAddRecipientModal({ isModalHidden, setIsModalHidden }) {
             labelWidth={0}
             endAdornment={<InputAdornment position="end">%</InputAdornment>}
           />
+          <Typography variant="body2">Staked position does not include redirected yield positions</Typography>
         </FormControl>
-        Staked position does not include redirected yield positions Recipient Address
+        <Typography variant="h5">Recipient Address</Typography>
+        <FormControl className="modal-input" variant="outlined" color="primary">
+          <InputLabel htmlFor="wallet-input"></InputLabel>
+          <OutlinedInput
+            id="wallet-input"
+            type="text"
+            placeholder="Enter a wallet address in the form of 0x ..."
+            className="stake-input"
+            value={walletAddress}
+            onChange={e => handleSetWallet(e.target.value)}
+            labelWidth={0}
+          />
+        </FormControl>
+        <FormControl className="ohm-modal-submit">
+          <Button variant="contained" color="primary">
+            Add Recipient
+          </Button>
+        </FormControl>
       </Paper>
     </Modal>
   );
