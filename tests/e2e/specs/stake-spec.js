bc61d813ae90e7527d857a3b485de38a7010d209
wKich <w@kich.dev>
2021-10-21 21:12:50 +0200

    interactors PoC

--- tests/e2e/specs/stake-spec.js
+++ tests/e2e/specs/stake-spec.js
@@ -1,3 +1,5 @@
+import { Button, HTML, TextField } from "@interactors/material-ui";
+
 var STAKE_AMOUNT = 0.1;
 // Sometimes we need to round float values because bigint type does not exist (yet) in javascript
 function ohmRound(val) {
@@ -9,35 +11,31 @@ describe("Stake tests", () => {
     cy.visit("/#/");
   });
   it(`Stake ${STAKE_AMOUNT} OHM`, () => {
-    cy.get("#wallet-button span").contains("Connect Wallet");
-    cy.get("#wallet-button").click();
-    cy.get(".web3modal-provider-name").contains("MetaMask").click();
+    cy.do(Button("Connect Wallet", { id: "wallet-button" }).click());
+    cy.do(HTML("MetaMask").click());
+
     cy.acceptMetamaskAccess();
-    cy.get(".stake-button").contains("Approve");
-    cy.get("#amount-input").type(STAKE_AMOUNT);
-    // Approve spend limit
-    cy.get(".stake-button").click();
+
+    cy.do(TextField("Enter an amount").fillIn(STAKE_AMOUNT));
+    cy.do(Button("Approve").click());
+
     cy.confirmMetamaskPermissionToSpend();
+
     // Stake and check that balance changed accordingly
     cy.get("#user-balance").then($p => {
-      cy.log($p.text());
       const balance_before_txt = $p.text();
       cy.log(balance_before_txt);
       const target_balance = ohmRound(ohmRound(balance_before_txt) - STAKE_AMOUNT);
+
       // Stake
-      cy.get(".stake-button").contains("Stake OHM");
-      cy.get(".stake-button").click();
+      cy.do(Button("Stake OHM").click());
+
       cy.confirmMetamaskTransaction();
-      cy.get("#wallet-button").contains("In progress");
-      cy.get(".stake-button").contains("Pending...");
+
+      cy.expect([Button("In progress").exists(), Button("Pending...", { disabled: true }).exists()]);
+
       // Wait for balance to change
-      cy.get("#user-balance")
-        .not(`:contains(${balance_before_txt})`)
-        .then($p => {
-          // Check new balance
-          const balance_after = ohmRound($p.text());
-          expect(balance_after).to.eq(target_balance);
-        });
+      cy.expect(HTML(`${target_balance} OHM`, { id: "user-balance" }).exists());
     });
     assert(true);
   });
