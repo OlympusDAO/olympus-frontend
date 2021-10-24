b917e5fbb55505c5508e87fccae0986384da1eb4
Sam Potter <sam.potter@hotmail.com>
2021-10-24 17:45:22 +1100

    Abstracted metrics into individual components

--- src/views/TreasuryDashboard/TreasuryDashboard.jsx
+++ src/views/TreasuryDashboard/TreasuryDashboard.jsx
@@ -17,100 +17,112 @@ import {
   tooltipInfoMessages,
   itemType,
 } from "./treasuryData.js";
+import { memo } from "react";
 
-const TreasuryDashboard = () => {
-  const smallerScreen = useMediaQuery("(max-width: 650px)");
-  const verySmallScreen = useMediaQuery("(max-width: 379px)");
+const Metric = props => <Box className={`metric ${props.className}`}>{props.children}</Box>;
+Metric.Value = props => <Typography variant="h5">{props.children || <Skeleton type="text" />}</Typography>;
+Metric.Title = props => (
+  <Typography variant="h6" text="colorSecondary">
+    {props.children}
+  </Typography>
+);
 
+const MarketCap = () => {
   const marketCap = useSelector(state => state.app.marketCap);
-  const circSupply = useSelector(state => state.app.circSupply);
+
+  return (
+    <Metric className="market">
+      <Metric.Title>Market Cap</Metric.Title>
+      <Metric.Value>{marketCap && formatCurrency(marketCap, 0)}</Metric.Value>
+    </Metric>
+  );
+};
+
+const OHMPrice = () => {
   const marketPrice = useSelector(state => state.app.marketPrice);
+
+  return (
+    <Metric className="price">
+      <Metric.Title>OHM Price</Metric.Title>
+      <Metric.Value>{marketPrice && formatCurrency(marketPrice, 2)}</Metric.Value>
+    </Metric>
+  );
+};
+
+const CircSupply = () => {
+  const circSupply = useSelector(state => state.app.circSupply);
   const totalSupply = useSelector(state => state.app.totalSupply);
-  const currentIndex = useSelector(state => state.app.currentIndex);
-  const wsOhmPrice = useSelector(state => state.app.marketPrice * state.app.currentIndex);
+
+  const isDataLoaded = circSupply && totalSupply;
+
+  return (
+    <Metric className="circ">
+      <Metric.Title>Circulating Supply (total)</Metric.Title>
+      <Metric.Value>{isDataLoaded && parseInt(circSupply) + " / " + parseInt(totalSupply)}</Metric.Value>
+    </Metric>
+  );
+};
+
+const BackingPerOHM = () => {
   const backingPerOhm = useSelector(state => state.app.treasuryMarketValue / state.app.circSupply);
 
   return (
-    <div id="treasury-dashboard-view" className={`${smallerScreen && "smaller"} ${verySmallScreen && "very-small"}`}>
+    <Metric className="bpo">
+      <Metric.Title>Backing per OHM</Metric.Title>
+      <Metric.Value>{!isNaN(backingPerOhm) && formatCurrency(backingPerOhm, 2)}</Metric.Value>
+    </Metric>
+  );
+};
+
+const CurrentIndex = () => {
+  const currentIndex = useSelector(state => state.app.currentIndex);
+
+  return (
+    <Metric className="index">
+      <Metric.Title>
+        Current Index
+        <InfoTooltip message="The current index tracks the amount of sOHM accumulated since the beginning of staking. Basically, how much sOHM one would have if they staked and held a single OHM from day 1." />
+      </Metric.Title>
+      <Metric.Value>{currentIndex && trim(currentIndex, 2) + " sOHM"}</Metric.Value>
+    </Metric>
+  );
+};
+
+const WSOHMPrice = () => {
+  const wsOhmPrice = useSelector(state => state.app.marketPrice * state.app.currentIndex);
+
+  return (
+    <Metric className="wsoprice">
+      <Metric.Title>
+        wsOHM Price
+        <InfoTooltip message="wsOHM = sOHM * index\n\nThe price of wsOHM is equal to the price of OHM multiplied by the current index" />
+      </Metric.Title>
+      <Metric.Value>{wsOhmPrice && formatCurrency(wsOhmPrice, 2)}</Metric.Value>
+    </Metric>
+  );
+};
+
+const TreasuryDashboard = memo(() => {
+  const isSmallScreen = useMediaQuery("(max-width: 650px)");
+  const isVerySmallScreen = useMediaQuery("(max-width: 379px)");
+
+  return (
+    <div id="treasury-dashboard-view" className={`${isSmallScreen && "smaller"} ${isVerySmallScreen && "very-small"}`}>
       <Container
         style={{
-          paddingLeft: smallerScreen || verySmallScreen ? "0" : "3.3rem",
-          paddingRight: smallerScreen || verySmallScreen ? "0" : "3.3rem",
+          paddingLeft: isSmallScreen || isVerySmallScreen ? "0" : "3.3rem",
+          paddingRight: isSmallScreen || isVerySmallScreen ? "0" : "3.3rem",
         }}
       >
-        <Box className={`hero-metrics`}>
+        <Box className="hero-metrics">
           <Paper className="ohm-card">
             <Box display="flex" flexWrap="wrap" justifyContent="space-between" alignItems="center">
-              <Box className="metric market">
-                <Typography variant="h6" color="textSecondary">
-                  Market Cap
-                </Typography>
-                <Typography variant="h5">
-                  {marketCap && formatCurrency(marketCap, 0)}
-                  {!marketCap && <Skeleton type="text" />}
-                </Typography>
-              </Box>
-
-              <Box className="metric price">
-                <Typography variant="h6" color="textSecondary">
-                  OHM Price
-                </Typography>
-                <Typography variant="h5">
-                  {/* appleseed-fix */}
-                  {marketPrice ? formatCurrency(marketPrice, 2) : <Skeleton type="text" />}
-                </Typography>
-              </Box>
-
-              <Box className="metric wsoprice">
-                <Typography variant="h6" color="textSecondary">
-                  wsOHM Price
-                  <InfoTooltip
-                    message={
-                      "wsOHM = sOHM * index\n\nThe price of wsOHM is equal to the price of OHM multiplied by the current index"
-                    }
-                  />
-                </Typography>
-
-                <Typography variant="h5">
-                  {wsOhmPrice ? formatCurrency(wsOhmPrice, 2) : <Skeleton type="text" />}
-                </Typography>
-              </Box>
-
-              <Box className="metric circ">
-                <Typography variant="h6" color="textSecondary">
-                  Circulating Supply (total)
-                </Typography>
-                <Typography variant="h5">
-                  {circSupply && totalSupply ? (
-                    parseInt(circSupply) + " / " + parseInt(totalSupply)
-                  ) : (
-                    <Skeleton type="text" />
-                  )}
-                </Typography>
-              </Box>
-
-              <Box className="metric bpo">
-                <Typography variant="h6" color="textSecondary">
-                  Backing per OHM
-                </Typography>
-                <Typography variant="h5">
-                  {backingPerOhm ? formatCurrency(backingPerOhm, 2) : <Skeleton type="text" />}
-                </Typography>
-              </Box>
-
-              <Box className="metric index">
-                <Typography variant="h6" color="textSecondary">
-                  Current Index
-                  <InfoTooltip
-                    message={
-                      "The current index tracks the amount of sOHM accumulated since the beginning of staking. Basically, how much sOHM one would have if they staked and held a single OHM from day 1."
-                    }
-                  />
-                </Typography>
-                <Typography variant="h5">
-                  {currentIndex ? trim(currentIndex, 2) + " sOHM" : <Skeleton type="text" />}
-                </Typography>
-              </Box>
+              <MarketCap />
+              <OHMPrice />
+              <WSOHMPrice />
+              <CircSupply />
+              <BackingPerOHM />
+              <CurrentIndex />
             </Box>
           </Paper>
         </Box>
@@ -182,11 +194,13 @@ const TreasuryDashboard = () => {
       </Container>
     </div>
   );
-};
+});
 
 const useTreasuryMetrics = () => {
   return useQuery("treasury_metrics", async () => {
     const response = await apollo(treasuryDataQuery);
+
+    console.log("metrics");
 
     // Transform string values to floats
     return response.data.protocolMetrics.map(metric =>
@@ -198,6 +212,8 @@ const useTreasuryMetrics = () => {
 const useTreasuryRebases = () => {
   return useQuery("treasury_rebases", async () => {
     const response = await apollo(rebasesDataQuery);
+
+    console.log("rebases");
 
     // Transform string values to floats
     return response.data.rebases;
