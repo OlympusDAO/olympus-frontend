9e99e7824658d492612f2bc739a761d7f1f3d459
Sam Potter <sam.potter@hotmail.com>
2021-10-24 15:23:37 +1100

    Abstracted graphs into individual components

--- src/views/TreasuryDashboard/TreasuryDashboard.jsx
+++ src/views/TreasuryDashboard/TreasuryDashboard.jsx
@@ -1,9 +1,14 @@
-import { useEffect, useState } from "react";
-import { Paper, Grid, Typography, Box, Zoom, Container, useMediaQuery } from "@material-ui/core";
-import { Skeleton } from "@material-ui/lab";
+import "./treasury-dashboard.scss";
 import { useSelector } from "react-redux";
+import { Skeleton } from "@material-ui/lab";
+import apollo from "../../lib/apolloClient";
+import { useTheme } from "@material-ui/core/styles";
 import Chart from "../../components/Chart/Chart.jsx";
 import { trim, formatCurrency } from "../../helpers";
+import InfoTooltip from "src/components/InfoTooltip/InfoTooltip.jsx";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { Paper, Grid, Typography, Box, Zoom, Container, useMediaQuery } from "@material-ui/core";
+
 import {
   treasuryDataQuery,
   rebasesDataQuery,
@@ -12,75 +17,18 @@ import {
   tooltipInfoMessages,
   itemType,
 } from "./treasuryData.js";
-import { useTheme } from "@material-ui/core/styles";
-import "./treasury-dashboard.scss";
-import apollo from "../../lib/apolloClient";
-import InfoTooltip from "src/components/InfoTooltip/InfoTooltip.jsx";
 
-function TreasuryDashboard() {
-  const [data, setData] = useState(null);
-  const [apy, setApy] = useState(null);
-  const [runway, setRunway] = useState(null);
-  const [staked, setStaked] = useState(null);
-  const theme = useTheme();
+const TreasuryDashboard = () => {
   const smallerScreen = useMediaQuery("(max-width: 650px)");
   const verySmallScreen = useMediaQuery("(max-width: 379px)");
 
-  const marketPrice = useSelector(state => {
-    return state.app.marketPrice;
-  });
-  const circSupply = useSelector(state => {
-    return state.app.circSupply;
-  });
-  const totalSupply = useSelector(state => {
-    return state.app.totalSupply;
-  });
-  const marketCap = useSelector(state => {
-    return state.app.marketCap;
-  });
-
-  const currentIndex = useSelector(state => {
-    return state.app.currentIndex;
-  });
-
-  const backingPerOhm = useSelector(state => {
-    return state.app.treasuryMarketValue / state.app.circSupply;
-  });
-
-  const wsOhmPrice = useSelector(state => {
-    return state.app.marketPrice * state.app.currentIndex;
-  });
-
-  useEffect(() => {
-    apollo(treasuryDataQuery).then(r => {
-      let metrics = r.data.protocolMetrics.map(entry =>
-        Object.entries(entry).reduce((obj, [key, value]) => ((obj[key] = parseFloat(value)), obj), {}),
-      );
-      metrics = metrics.filter(pm => pm.treasuryMarketValue > 0);
-      setData(metrics);
-
-      let staked = r.data.protocolMetrics.map(entry => ({
-        staked: (parseFloat(entry.sOhmCirculatingSupply) / parseFloat(entry.ohmCirculatingSupply)) * 100,
-        timestamp: entry.timestamp,
-      }));
-      staked = staked.filter(pm => pm.staked < 100);
-      setStaked(staked);
-
-      let runway = metrics.filter(pm => pm.runway10k > 5);
-      setRunway(runway);
-    });
-
-    apollo(rebasesDataQuery).then(r => {
-      let apy = r.data.rebases.map(entry => ({
-        apy: Math.pow(parseFloat(entry.percentage) + 1, 365 * 3) * 100,
-        timestamp: entry.timestamp,
-      }));
-
-      apy = apy.filter(pm => pm.apy < 300000);
-
-      setApy(apy);
-    });
-  }, []);
+  const marketCap = useSelector(state => state.app.marketCap);
+  const circSupply = useSelector(state => state.app.circSupply);
+  const marketPrice = useSelector(state => state.app.marketPrice);
+  const totalSupply = useSelector(state => state.app.totalSupply);
+  const currentIndex = useSelector(state => state.app.currentIndex);
+  const wsOhmPrice = useSelector(state => state.app.marketPrice * state.app.currentIndex);
+  const backingPerOhm = useSelector(state => state.app.treasuryMarketValue / state.app.circSupply);
 
   return (
     <div id="treasury-dashboard-view" className={`${smallerScreen && "smaller"} ${verySmallScreen && "very-small"}`}>
@@ -171,93 +119,28 @@ function TreasuryDashboard() {
           <Grid container spacing={2} className="data-grid">
             <Grid item lg={6} md={6} sm={12} xs={12}>
               <Paper className="ohm-card ohm-chart-card">
-                <Chart
-                  type="area"
-                  data={data}
-                  dataKey={["totalValueLocked"]}
-                  stopColor={[["#768299", "#98B3E9"]]}
-                  headerText="Total Value Deposited"
-                  headerSubText={`${data && formatCurrency(data[0].totalValueLocked)}`}
-                  bulletpointColors={bulletpoints.tvl}
-                  itemNames={tooltipItems.tvl}
-                  itemType={itemType.dollar}
-                  infoTooltipMessage={tooltipInfoMessages.tvl}
-                  expandedGraphStrokeColor={theme.palette.graphStrokeColor}
-                />
+                <TotalValueDepositedGraph />
               </Paper>
             </Grid>
 
             <Grid item lg={6} md={6} sm={12} xs={12}>
               <Paper className="ohm-card ohm-chart-card">
-                <Chart
-                  type="stack"
-                  data={data}
-                  dataKey={[
-                    "treasuryDaiMarketValue",
-                    "treasuryFraxMarketValue",
-                    "treasuryWETHMarketValue",
-                    "treasuryXsushiMarketValue",
-                  ]}
-                  stopColor={[
-                    ["#F5AC37", "#EA9276"],
-                    ["#768299", "#98B3E9"],
-                    ["#DC30EB", "#EA98F1"],
-                    ["#8BFF4D", "#4C8C2A"],
-                  ]}
-                  headerText="Market Value of Treasury Assets"
-                  headerSubText={`${data && formatCurrency(data[0].treasuryMarketValue)}`}
-                  bulletpointColors={bulletpoints.coin}
-                  itemNames={tooltipItems.coin}
-                  itemType={itemType.dollar}
-                  infoTooltipMessage={tooltipInfoMessages.mvt}
-                  expandedGraphStrokeColor={theme.palette.graphStrokeColor}
-                />
+                <MarketValueGraph />
               </Paper>
             </Grid>
 
             <Grid item lg={6} md={6} sm={12} xs={12}>
               <Paper className="ohm-card ohm-chart-card">
-                <Chart
-                  type="stack"
-                  data={data}
-                  format="currency"
-                  dataKey={["treasuryDaiRiskFreeValue", "treasuryFraxRiskFreeValue"]}
-                  stopColor={[
-                    ["#F5AC37", "#EA9276"],
-                    ["#768299", "#98B3E9"],
-                    ["#000", "#fff"],
-                    ["#000", "#fff"],
-                  ]}
-                  headerText="Risk Free Value of Treasury Assets"
-                  headerSubText={`${data && formatCurrency(data[0].treasuryRiskFreeValue)}`}
-                  bulletpointColors={bulletpoints.coin}
-                  itemNames={tooltipItems.coin}
-                  itemType={itemType.dollar}
-                  infoTooltipMessage={tooltipInfoMessages.rfv}
-                  expandedGraphStrokeColor={theme.palette.graphStrokeColor}
-                />
+                <RiskFreeValueGraph />
               </Paper>
             </Grid>
 
             <Grid item lg={6} md={6} sm={12} xs={12}>
               <Paper className="ohm-card">
-                <Chart
-                  type="area"
-                  data={data}
-                  dataKey={["treasuryOhmDaiPOL"]}
-                  stopColor={[["rgba(128, 204, 131, 1)", "rgba(128, 204, 131, 0)"]]}
-                  headerText="Protocol Owned Liquidity OHM-DAI"
-                  headerSubText={`${data && trim(data[0].treasuryOhmDaiPOL, 2)}% `}
-                  dataFormat="percent"
-                  bulletpointColors={bulletpoints.pol}
-                  itemNames={tooltipItems.pol}
-                  itemType={itemType.percentage}
-                  infoTooltipMessage={tooltipInfoMessages.pol}
-                  expandedGraphStrokeColor={theme.palette.graphStrokeColor}
-                  isPOL={true}
-                />
+                <ProtocolOwnedLiquidityGraph />
               </Paper>
             </Grid>
+
             {/*  Temporarily removed until correct data is in the graph */}
             {/* <Grid item lg={6} md={12} sm={12} xs={12}>
               <Paper className="ohm-card">
@@ -279,60 +162,19 @@ function TreasuryDashboard() {
 
             <Grid item lg={6} md={6} sm={12} xs={12}>
               <Paper className="ohm-card">
-                <Chart
-                  type="area"
-                  data={staked}
-                  dataKey={["staked"]}
-                  stopColor={[["#55EBC7", "#47ACEB"]]}
-                  headerText="OHM Staked"
-                  dataFormat="percent"
-                  headerSubText={`${staked && trim(staked[0].staked, 2)}% `}
-                  isStaked={true}
-                  bulletpointColors={bulletpoints.staked}
-                  infoTooltipMessage={tooltipInfoMessages.staked}
-                  expandedGraphStrokeColor={theme.palette.graphStrokeColor}
-                />
+                <OHMStakedGraph />
               </Paper>
             </Grid>
 
             <Grid item lg={6} md={6} sm={12} xs={12}>
               <Paper className="ohm-card">
-                <Chart
-                  type="line"
-                  scale="log"
-                  data={apy}
-                  dataKey={["apy"]}
-                  color={theme.palette.text.primary}
-                  stroke={[theme.palette.text.primary]}
-                  headerText="APY over time"
-                  dataFormat="percent"
-                  headerSubText={`${apy && trim(apy[0].apy, 2)}%`}
-                  bulletpointColors={bulletpoints.apy}
-                  itemNames={tooltipItems.apy}
-                  itemType={itemType.percentage}
-                  infoTooltipMessage={tooltipInfoMessages.apy}
-                  expandedGraphStrokeColor={theme.palette.graphStrokeColor}
-                />
+                <APYOverTimeGraph />
               </Paper>
             </Grid>
 
             <Grid item lg={6} md={6} sm={12} xs={12}>
               <Paper className="ohm-card">
-                <Chart
-                  type="line"
-                  data={runway}
-                  dataKey={["runwayCurrent"]}
-                  color={theme.palette.text.primary}
-                  stroke={[theme.palette.text.primary]}
-                  headerText="Runway Available"
-                  headerSubText={`${data && trim(data[0].runwayCurrent, 1)} Days`}
-                  dataFormat="days"
-                  bulletpointColors={bulletpoints.runway}
-                  itemNames={tooltipItems.runway}
-                  itemType={""}
-                  infoTooltipMessage={tooltipInfoMessages.runway}
-                  expandedGraphStrokeColor={theme.palette.graphStrokeColor}
-                />
+                <RunwayAvailableGraph />
               </Paper>
             </Grid>
           </Grid>
@@ -340,6 +182,224 @@ function TreasuryDashboard() {
       </Container>
     </div>
   );
-}
+};
 
-export default TreasuryDashboard;
+const useTreasuryMetrics = () => {
+  return useQuery("treasury_metrics", async () => {
+    const response = await apollo(treasuryDataQuery);
+
+    // Transform string values to floats
+    return response.data.protocolMetrics.map(metric =>
+      Object.entries(metric).reduce((obj, [key, value]) => ((obj[key] = parseFloat(value)), obj), {}),
+    );
+  });
+};
+
+const useTreasuryRebases = () => {
+  return useQuery("treasury_rebases", async () => {
+    const response = await apollo(rebasesDataQuery);
+
+    // Transform string values to floats
+    return response.data.rebases;
+  });
+};
+
+const TotalValueDepositedGraph = () => {
+  const theme = useTheme();
+  const { data } = useTreasuryMetrics();
+
+  return (
+    <Chart
+      type="area"
+      data={data}
+      itemType={itemType.dollar}
+      itemNames={tooltipItems.tvl}
+      dataKey={["totalValueLocked"]}
+      headerText="Total Value Deposited"
+      stopColor={[["#768299", "#98B3E9"]]}
+      bulletpointColors={bulletpoints.tvl}
+      infoTooltipMessage={tooltipInfoMessages.tvl}
+      expandedGraphStrokeColor={theme.palette.graphStrokeColor}
+      headerSubText={`${data && formatCurrency(data[0].totalValueLocked)}`}
+    />
+  );
+};
+
+const MarketValueGraph = () => {
+  const theme = useTheme();
+  const { data } = useTreasuryMetrics();
+
+  return (
+    <Chart
+      stopColor={[
+        ["#F5AC37", "#EA9276"],
+        ["#768299", "#98B3E9"],
+        ["#DC30EB", "#EA98F1"],
+        ["#8BFF4D", "#4C8C2A"],
+      ]}
+      dataKey={[
+        "treasuryDaiMarketValue",
+        "treasuryFraxMarketValue",
+        "treasuryWETHMarketValue",
+        "treasuryXsushiMarketValue",
+      ]}
+      data={data}
+      type="stack"
+      itemType={itemType.dollar}
+      itemNames={tooltipItems.coin}
+      bulletpointColors={bulletpoints.coin}
+      headerText="Market Value of Treasury Assets"
+      infoTooltipMessage={tooltipInfoMessages.mvt}
+      expandedGraphStrokeColor={theme.palette.graphStrokeColor}
+      headerSubText={`${data && formatCurrency(data[0].treasuryMarketValue)}`}
+    />
+  );
+};
+
+const RiskFreeValueGraph = () => {
+  const theme = useTheme();
+  const { data } = useTreasuryMetrics();
+
+  return (
+    <Chart
+      stopColor={[
+        ["#F5AC37", "#EA9276"],
+        ["#768299", "#98B3E9"],
+        ["#000", "#fff"],
+        ["#000", "#fff"],
+      ]}
+      data={data}
+      type="stack"
+      format="currency"
+      itemType={itemType.dollar}
+      itemNames={tooltipItems.coin}
+      bulletpointColors={bulletpoints.coin}
+      infoTooltipMessage={tooltipInfoMessages.rfv}
+      headerText="Risk Free Value of Treasury Assets"
+      expandedGraphStrokeColor={theme.palette.graphStrokeColor}
+      dataKey={["treasuryDaiRiskFreeValue", "treasuryFraxRiskFreeValue"]}
+      headerSubText={`${data && formatCurrency(data[0].treasuryRiskFreeValue)}`}
+    />
+  );
+};
+
+const ProtocolOwnedLiquidityGraph = () => {
+  const theme = useTheme();
+  const { data } = useTreasuryMetrics();
+
+  return (
+    <Chart
+      isPOL
+      type="area"
+      data={data}
+      dataFormat="percent"
+      itemNames={tooltipItems.pol}
+      itemType={itemType.percentage}
+      dataKey={["treasuryOhmDaiPOL"]}
+      bulletpointColors={bulletpoints.pol}
+      infoTooltipMessage={tooltipInfoMessages.pol}
+      headerText="Protocol Owned Liquidity OHM-DAI"
+      expandedGraphStrokeColor={theme.palette.graphStrokeColor}
+      headerSubText={`${data && trim(data[0].treasuryOhmDaiPOL, 2)}% `}
+      stopColor={[["rgba(128, 204, 131, 1)", "rgba(128, 204, 131, 0)"]]}
+    />
+  );
+};
+
+const OHMStakedGraph = () => {
+  const theme = useTheme();
+  const { data } = useTreasuryMetrics();
+
+  const staked =
+    data &&
+    data
+      .map(metric => ({
+        staked: (metric.sOhmCirculatingSupply / metric.ohmCirculatingSupply) * 100,
+        timestamp: metric.timestamp,
+      }))
+      .filter(metric => metric.staked < 100);
+
+  return (
+    <Chart
+      isStaked
+      type="area"
+      data={staked}
+      dataKey={["staked"]}
+      dataFormat="percent"
+      headerText="OHM Staked"
+      stopColor={[["#55EBC7", "#47ACEB"]]}
+      bulletpointColors={bulletpoints.staked}
+      infoTooltipMessage={tooltipInfoMessages.staked}
+      expandedGraphStrokeColor={theme.palette.graphStrokeColor}
+      headerSubText={`${staked && trim(staked[0].staked, 2)}% `}
+    />
+  );
+};
+
+const APYOverTimeGraph = () => {
+  const theme = useTheme();
+  const { data } = useTreasuryRebases();
+
+  let apy =
+    data &&
+    data
+      .map(entry => ({
+        timestamp: entry.timestamp,
+        apy: Math.pow(parseFloat(entry.percentage) + 1, 365 * 3) * 100,
+      }))
+      .filter(pm => pm.apy < 300000);
+
+  return (
+    <Chart
+      type="line"
+      scale="log"
+      data={apy}
+      dataKey={["apy"]}
+      dataFormat="percent"
+      headerText="APY over time"
+      itemNames={tooltipItems.apy}
+      itemType={itemType.percentage}
+      color={theme.palette.text.primary}
+      bulletpointColors={bulletpoints.apy}
+      stroke={[theme.palette.text.primary]}
+      infoTooltipMessage={tooltipInfoMessages.apy}
+      headerSubText={`${apy && trim(apy[0].apy, 2)}%`}
+      expandedGraphStrokeColor={theme.palette.graphStrokeColor}
+    />
+  );
+};
+
+const RunwayAvailableGraph = () => {
+  const theme = useTheme();
+  const { data } = useTreasuryMetrics();
+
+  const runway = data && data.filter(metric => metric.runway10k > 5);
+
+  return (
+    <Chart
+      type="line"
+      data={runway}
+      itemType={""}
+      dataFormat="days"
+      dataKey={["runwayCurrent"]}
+      headerText="Runway Available"
+      itemNames={tooltipItems.runway}
+      color={theme.palette.text.primary}
+      stroke={[theme.palette.text.primary]}
+      bulletpointColors={bulletpoints.runway}
+      infoTooltipMessage={tooltipInfoMessages.runway}
+      expandedGraphStrokeColor={theme.palette.graphStrokeColor}
+      headerSubText={`${data && trim(data[0].runwayCurrent, 1)} Days`}
+    />
+  );
+};
+
+const queryClient = new QueryClient();
+
+// Normally this would be done
+// much higher up in our App so that
+export default () => (
+  <QueryClientProvider client={queryClient}>
+    <TreasuryDashboard />
+  </QueryClientProvider>
+);
