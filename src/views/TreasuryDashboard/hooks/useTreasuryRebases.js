9928d9dba4ceb16d1149b82b43ae992efa40ef69
Sam Potter <sam.potter@hotmail.com>
2021-10-24 21:18:48 +1100

    Removed comment

--- src/views/TreasuryDashboard/hooks/useTreasuryRebases.js
+++ src/views/TreasuryDashboard/hooks/useTreasuryRebases.js
@@ -8,7 +8,6 @@ export const useTreasuryRebases = options => {
     async () => {
       const response = await apollo(rebasesDataQuery);
 
-      // Transform string values to floats
       return response.data.rebases;
     },
     options,
