e6b6ade62a1886500749327b3d2fe6d6a76678c3
casuwu <taken404@gmail.com>
2021-10-26 18:06:17 -0400

    capture utm values from marketing campaings

--- src/helpers/QueryParameterHelper.ts
+++ src/helpers/QueryParameterHelper.ts
@@ -2,6 +2,14 @@ import queryString from "query-string";
 
 const sessionStorageKey = "query";
 
+export function getParameterByName(name: string, url: string) {
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 /**
  * Stores the URL query parameters in session storage as a string.
  *
