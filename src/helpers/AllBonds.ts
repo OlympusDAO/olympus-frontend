8ae7c8614496bdad27d285ae864d415bbb97487a
appleseed-iii <appleseed-iii@protonmail.com>
2021-10-27 08:36:44 -0500

    temp turn-off ohm-lusd & lusd bonds until we figure out what contract attribute can be read

--- src/helpers/AllBonds.ts
+++ src/helpers/AllBonds.ts
@@ -230,7 +230,7 @@ export const ohm_weth = new CustomBond({
 // Is it a stableCoin bond? use `new StableBond`
 // Is it an LP Bond? use `new LPBond`
 // Add new bonds to this array!!
-export const allBonds = [dai, frax, eth, ohm_dai, ohm_frax, lusd, ohm_lusd, ohm_weth];
+export const allBonds = [dai, frax, eth, ohm_dai, ohm_frax, ohm_weth];
 export const allBondsMap = allBonds.reduce((prevVal, bond) => {
   return { ...prevVal, [bond.name]: bond };
 }, {});
