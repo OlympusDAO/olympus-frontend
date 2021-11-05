39fc4850903d225a9027f25696bb8994d1e38164
0xJem <0x0xJem@gmail.com>
2021-11-05 15:04:35 +0500

    WIP test of dappeteer

--- tests/e2e/specs/stake-spec.js
+++ tests/e2e/specs/stake-spec.js
@@ -1,4 +1,7 @@
-import { Button, HTML, TextField } from "@interactors/material-ui";
+// import puppeteer from "puppeteer";
+// import dappetter from "@chainsafe/dappeteer";
+const puppeteer = require("puppeteer");
+const dappeteer = require("@chainsafe/dappeteer");
 
 var STAKE_AMOUNT = 0.1;
 // Sometimes we need to round float values because bigint type does not exist (yet) in javascript
@@ -6,68 +9,30 @@ function ohmRound(val) {
   var m = Math.pow(10, 10);
   return Math.round(parseFloat(val) * m) / m;
 }
-describe("Stake tests", () => {
-  beforeEach(() => {
-    cy.visit("/#/");
-  });
-  it(`Stake ${STAKE_AMOUNT} OHM`, () => {
-    cy.do(Button("Connect Wallet", { id: "wallet-button" }).click());
-    cy.do(HTML("MetaMask").click());
 
-    cy.acceptMetamaskAccess();
+async function stake() {
+  const browser = await dappeteer.launch(puppeteer, { metamaskVersion: "v10.1.1" });
 
-    cy.do(TextField("Enter an amount").fillIn(STAKE_AMOUNT));
-    cy.do(Button("Approve").click());
+  const metamask = await dappeteer.setupMetamask(browser, { seed: "agent divorce sleep alien rather maple east barrel stage anchor meadow basket milk abstract rifle tonight early captain total connect echo keep used impulse" });
+  await metamask.switchNetwork("rinkeby");
 
-    cy.confirmMetamaskPermissionToSpend();
+  const page = await browser.newPage();
+  await page.goto("http://localhost:3000/#/stake");
 
-    // Stake and check that balance changed accordingly
-    cy.get("#user-balance").then($p => {
-      const balance_before_txt = $p.text();
-      cy.log(balance_before_txt);
-      const target_balance = ohmRound(ohmRound(balance_before_txt) - STAKE_AMOUNT);
+  await page.click(".connect-button");
 
-      // Stake
-      cy.do(Button("Stake OHM").click());
+  // show metamask window
+  await metamask.page.click(".popover-header__button");
 
-      cy.confirmMetamaskTransaction();
+  await metamask.approve();
 
-      cy.expect([Button("In progress").exists(), Button("Pending...", { disabled: true }).exists()]);
+  // Approve?
 
-      // Wait for balance to change
-      cy.expect(HTML(`${target_balance} OHM`, { id: "user-balance" }).exists());
-    });
-    assert(true);
-  });
-  it(`Unstakes ${STAKE_AMOUNT} sOHM`, () => {
-    cy.get("#wallet-button span").contains("Connect Wallet");
-    cy.get("#wallet-button").click();
-    cy.get(".web3modal-provider-name").contains("MetaMask").click();
-    // cy.acceptMetamaskAccess();
-    cy.get("#simple-tab-1").click();
-    cy.get(".stake-button").contains("Approve");
-    cy.get("#amount-input").type(STAKE_AMOUNT);
-    cy.get(".stake-button").click();
-    cy.confirmMetamaskPermissionToSpend();
-    // Unstake
-    cy.get("#user-balance").then($p => {
-      cy.log($p.text());
-      const balance_before_txt = $p.text();
-      const target_balance = ohmRound(ohmRound(balance_before_txt) + STAKE_AMOUNT);
-      // Wait for balance to change
-      cy.get(".stake-button").contains("Unstake OHM");
-      cy.get(".stake-button").click();
-      cy.confirmMetamaskTransaction();
-      cy.get("#wallet-button").contains("In progress");
-      cy.get(".stake-button").contains("Pending...");
-      cy.get("#user-balance")
-        .not(`:contains(${balance_before_txt})`)
-        .then($p => {
-          // Check new balance
-          const balance_after = ohmRound($p.text());
-          expect(balance_after).to.eq(target_balance);
-        });
-    });
-    assert(true);
-  });
-});
+  await page.waitForSelector("#amount-input");
+  await page.type("#amount-input", "1");
+  await page.click(".stake-button");
+
+  await metamask.confirmMetamaskTransaction();
+}
+
+stake();
