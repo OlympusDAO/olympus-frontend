2e17ad6118f77c03efb3c3492d42b0891413521d
appleseed-iii <80423742+appleseed-iii@users.noreply.github.com>
2021-11-07 22:20:12 -0600

    wsOHM wrapping & add to wallet (#618)
    * add wsOHM token icon
    
    1. remove max-width from `ohmmenu` class so that popper is wide enough for 4 token icons
    2. add wsOHM to `OhmMenu.jsx` so that it can be added to wallet
    3. NOTE: wsOHM will not appear to the user until a Contract address is added
    
    * wip: general ui laid out, need to tie it together
    
    * wrap functions working, figuring out what to do about wsOHM vs sOHM quantities & how to show them to the user
    
    * wrap/unwrap fully functionning, need to double check ui & potentially move to modal
    
    * fix open state
    
    * handling unsupported backdrop-filter transparency on firefox
    
    * fix girth theme opacity on ohm-popper & background colors on bond modal
    
    * remove stale comments, add wsOHM link out to docs

--- src/themes/global.js
+++ src/themes/global.js
@@ -15,6 +15,21 @@ const breakpointValues = {
 //   browser: 1333,
 // };
 
+/**
+ * will remove opacity from rgbaString when backdrop-filter is not supported
+ * @param {String} rgbaString should be the rgba string
+ * @returns modified rgbaString
+ */
+export const handleBackdropFilter = rgbaString => {
+  const supported = CSS.supports("(-webkit-backdrop-filter: none)") || CSS.supports("(backdrop-filter: none)");
+  console.log("supported", supported);
+  if (!supported) {
+    // make the opacity == 0.9;
+    rgbaString = rgbaString.replace(/[\d\.]+\)$/g, "0.9)");
+  }
+  return rgbaString;
+};
+
 const commonSettings = {
   direction: "ltr",
   typography: {
@@ -138,6 +153,7 @@ const commonSettings = {
         // backdropFilter: "blur(33px)",
         backgroundColor: "inherit",
         padding: 0,
+        zIndex: 7,
       },
     },
     MuiBackdrop: {
