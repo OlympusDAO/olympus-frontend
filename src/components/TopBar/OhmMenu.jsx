d94b045ca3742da875c20f25bcab87725a0754b2
casuwu <taken404@gmail.com>
2021-11-09 13:51:34 -0500

    setting up slide logic

--- src/components/TopBar/OhmMenu.jsx
+++ src/components/TopBar/OhmMenu.jsx
@@ -1,4 +1,7 @@
 import { useState, useEffect } from "react";
+import { useTheme } from "@material-ui/core/styles";
+import Switch from "@material-ui/core/Switch";
+
 import { addresses, TOKEN_DECIMALS } from "../../constants";
 import { getTokenImage } from "../../helpers";
 import { useSelector } from "react-redux";
@@ -12,10 +15,16 @@ import { ReactComponent as t33TokenImg } from "../../assets/tokens/token_33T.svg
 import "./ohmmenu.scss";
 import { dai, frax } from "src/helpers/AllBonds";
 import { useWeb3Context } from "../../hooks/web3Context";
+import { trim, formatCurrency } from "../../helpers";
 
 import OhmImg from "src/assets/tokens/token_OHM.svg";
 import SOhmImg from "src/assets/tokens/token_sOHM.svg";
 import token33tImg from "src/assets/tokens/token_33T.svg";
+
+import Chart from "../../components/Chart/Chart.jsx";
+import apollo from "../../lib/apolloClient";
+
+import { rebasesDataQuery, bulletpoints, tooltipItems, tooltipInfoMessages, itemType } from "./treasuryData.js";
 
 const addTokenToWallet = (tokenSymbol, tokenAddress) => async () => {
   if (window.ethereum) {
@@ -58,6 +67,20 @@ const addTokenToWallet = (tokenSymbol, tokenAddress) => async () => {
 
 function OhmMenu() {
   const [anchorEl, setAnchorEl] = useState(null);
+  const [apy, setApy] = useState(null);
+  const [checked, setChecked] = useState(false);
+
+  const theme = useTheme();
+  apollo(rebasesDataQuery).then(r => {
+    let apy = r.data.rebases.map(entry => ({
+      apy: Math.pow(parseFloat(entry.percentage) + 1, 365 * 3) * 100,
+      timestamp: entry.timestamp,
+    }));
+
+    apy = apy.filter(pm => pm.apy < 300000);
+
+    setApy(apy);
+  });
   const isEthereumAPIAvailable = window.ethereum;
   const { chainID } = useWeb3Context();
 
@@ -69,6 +92,15 @@ function OhmMenu() {
 
   const handleClick = event => {
     setAnchorEl(anchorEl ? null : event.currentTarget);
+  };
+  // Handle change boolean inversion,
+  // When that particular button is clicked,
+  //invert the state of that component to present something different
+  // going to need one for wsOHM
+  // sOHM  so only two
+  const handleChange = () => {
+    setChecked(!checked);
+    console.log(checked);
   };
 
   const open = Boolean(anchorEl);
@@ -93,93 +125,155 @@ function OhmMenu() {
             <Fade {...TransitionProps} timeout={100}>
               <Paper className="ohm-menu" elevation={1}>
                 <Box component="div" className="buy-tokens">
-                  <Link
-                    href={`https://app.sushi.com/swap?inputCurrency=${daiAddress}&outputCurrency=${OHM_ADDRESS}`}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <Button size="large" variant="contained" color="secondary" fullWidth>
-                      <Typography align="left">
-                        Buy on Sushiswap <SvgIcon component={ArrowUpIcon} htmlColor="#A3A3A3" />
-                      </Typography>
-                    </Button>
-                  </Link>
-
-                  <Link
-                    href={`https://app.uniswap.org/#/swap?inputCurrency=${fraxAddress}&outputCurrency=${OHM_ADDRESS}`}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <Button size="large" variant="contained" color="secondary" fullWidth>
-                      <Typography align="left">
-                        Buy on Uniswap <SvgIcon component={ArrowUpIcon} htmlColor="#A3A3A3" />
-                      </Typography>
-                    </Button>
-                  </Link>
-
-                  <Link href={`https://abracadabra.money/pool/10`} target="_blank" rel="noreferrer">
-                    <Button size="large" variant="contained" color="secondary" fullWidth>
-                      <Typography align="left">
-                        Wrap sOHM on Abracadabra <SvgIcon component={ArrowUpIcon} htmlColor="#A3A3A3" />
-                      </Typography>
-                    </Button>
-                  </Link>
-                </Box>
-
-                <Box component="div" className="data-links">
-                  <Divider color="secondary" className="less-margin" />
-                  <Link href={`https://dune.xyz/shadow/Olympus-(OHM)`} target="_blank" rel="noreferrer">
-                    <Button size="large" variant="contained" color="secondary" fullWidth>
-                      <Typography align="left">
-                        Shadow's Dune Dashboard <SvgIcon component={ArrowUpIcon} htmlColor="#A3A3A3" />
-                      </Typography>
-                    </Button>
-                  </Link>
-                </Box>
-
-                {isEthereumAPIAvailable ? (
-                  <Box className="add-tokens">
-                    <Divider color="secondary" />
-                    <p>ADD TOKEN TO WALLET</p>
-                    <Box display="flex" flexDirection="row" justifyContent="space-between">
-                      <Button variant="contained" color="secondary" onClick={addTokenToWallet("OHM", OHM_ADDRESS)}>
-                        <SvgIcon
-                          component={ohmTokenImg}
-                          viewBox="0 0 32 32"
-                          style={{ height: "25px", width: "25px" }}
+                  {!checked ? (
+                    <div>
+                      <Paper className="ohm-card">
+                        <Chart
+                          type="line"
+                          scale="log"
+                          data={apy}
+                          dataKey={["apy"]}
+                          color={theme.palette.text.primary}
+                          stroke={[theme.palette.text.primary]}
+                          headerText="APY over time"
+                          dataFormat="percent"
+                          headerSubText={`${apy && trim(apy[0].apy, 2)}%`}
+                          bulletpointColors={bulletpoints.apy}
+                          itemNames={tooltipItems.apy}
+                          itemType={itemType.percentage}
+                          infoTooltipMessage={tooltipInfoMessages.apy}
+                          expandedGraphStrokeColor={theme.palette.graphStrokeColor}
                         />
-                        <Typography variant="body1">OHM</Typography>
-                      </Button>
-                      <Button variant="contained" color="secondary" onClick={addTokenToWallet("sOHM", SOHM_ADDRESS)}>
-                        <SvgIcon
-                          component={sOhmTokenImg}
-                          viewBox="0 0 100 100"
-                          style={{ height: "25px", width: "25px" }}
-                        />
-                        <Typography variant="body1">sOHM</Typography>
-                      </Button>
-                      <Button variant="contained" color="secondary" onClick={addTokenToWallet("33T", PT_TOKEN_ADDRESS)}>
-                        <SvgIcon
-                          component={t33TokenImg}
-                          viewBox="0 0 1000 1000"
-                          style={{ height: "25px", width: "25px" }}
-                        />
-                        <Typography variant="body1">33T</Typography>
-                      </Button>
+                      </Paper>
+                      <Box className="ohm-pairs">
+                        <Button variant="contained" color="secondary" onClick={addTokenToWallet("OHM", OHM_ADDRESS)}>
+                          <Typography align="left">
+                            {" "}
+                            <SvgIcon
+                              component={ohmTokenImg}
+                              viewBox="0 0 32 32"
+                              style={{ height: "25px", width: "25px" }}
+                            />
+                            OHM
+                          </Typography>
+                        </Button>
+                      </Box>
+
+                      <Switch checked={checked} onChange={handleChange} aria-label="Collapse" />
+
+                      <Link
+                        href={`https://app.sushi.com/swap?inputCurrency=${daiAddress}&outputCurrency=${OHM_ADDRESS}`}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <Button size="large" variant="contained" color="secondary" fullWidth>
+                          <Typography>
+                            Buy on Sushiswap <SvgIcon component={ArrowUpIcon} htmlColor="#A3A3A3" />
+                          </Typography>
+                        </Button>
+                      </Link>
+
+                      <Link
+                        href={`https://app.uniswap.org/#/swap?inputCurrency=${fraxAddress}&outputCurrency=${OHM_ADDRESS}`}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <Button size="large" variant="contained" color="secondary" fullWidth>
+                          <Typography align="left">
+                            Buy on Uniswap <SvgIcon component={ArrowUpIcon} htmlColor="#A3A3A3" />
+                          </Typography>
+                        </Button>
+                      </Link>
+
+                      <Link href={`https://abracadabra.money/pool/10`} target="_blank" rel="noreferrer">
+                        <Button size="large" variant="contained" color="secondary" fullWidth>
+                          <Typography align="left">
+                            Wrap sOHM on Abracadabra <SvgIcon component={ArrowUpIcon} htmlColor="#A3A3A3" />
+                          </Typography>
+                        </Button>
+                      </Link>
+                      <Box component="div" className="data-links">
+                        <Divider color="secondary" className="less-margin" />
+                        <Link href={`https://dune.xyz/shadow/Olympus-(OHM)`} target="_blank" rel="noreferrer">
+                          <Button size="large" variant="contained" color="secondary" fullWidth>
+                            <Typography align="left">
+                              Shadow's Dune Dashboard <SvgIcon component={ArrowUpIcon} htmlColor="#A3A3A3" />
+                            </Typography>
+                          </Button>
+                        </Link>
+                        {isEthereumAPIAvailable ? (
+                          <Box className="add-tokens">
+                            <Divider color="secondary" />
+                            <p>ADD TOKEN TO WALLET</p>
+                            <Box display="flex" flexDirection="row" justifyContent="space-between">
+                              <Button
+                                variant="contained"
+                                color="secondary"
+                                onClick={addTokenToWallet("sOHM", SOHM_ADDRESS)}
+                              >
+                                <SvgIcon
+                                  component={sOhmTokenImg}
+                                  viewBox="0 0 100 100"
+                                  style={{ height: "25px", width: "25px" }}
+                                />
+                                <Typography variant="body1">sOHM</Typography>
+                              </Button>
+                              <Button
+                                variant="contained"
+                                color="secondary"
+                                onClick={addTokenToWallet("33T", PT_TOKEN_ADDRESS)}
+                              >
+                                <SvgIcon
+                                  component={t33TokenImg}
+                                  viewBox="0 0 1000 1000"
+                                  style={{ height: "25px", width: "25px" }}
+                                />
+                                <Typography variant="body1">33T</Typography>
+                              </Button>
+                              <Link
+                                href="https://docs.olympusdao.finance/using-the-website/unstaking_lp"
+                                target="_blank"
+                                rel="noreferrer"
+                              >
+                                <Button size="large" variant="contained" color="secondary" fullWidth>
+                                  <Typography align="left">Unstake Legacy LP Token</Typography>
+                                </Button>
+                              </Link>
+                            </Box>
+                          </Box>
+                        ) : null}
+                      </Box>
+                    </div>
+                  ) : null}
+                  {checked ? (
+                    <Box className="ohm-pairs">
+                      {" "}
+                      <Slide direction="left" in={checked} mountOnEnter unmountOnExit>
+                        <Paper className="ohm-card">
+                          <Chart
+                            type="line"
+                            scale="log"
+                            data={apy}
+                            dataKey={["apy"]}
+                            color={theme.palette.text.primary}
+                            stroke={[theme.palette.text.primary]}
+                            headerText="APY over time"
+                            dataFormat="percent"
+                            headerSubText={`${apy && trim(apy[0].apy, 2)}%`}
+                            bulletpointColors={bulletpoints.apy}
+                            itemNames={tooltipItems.apy}
+                            itemType={itemType.percentage}
+                            infoTooltipMessage={tooltipInfoMessages.apy}
+                            expandedGraphStrokeColor={theme.palette.graphStrokeColor}
+                          />
+                        </Paper>
+                      </Slide>
+                      <Switch checked={checked} onChange={handleChange} aria-label="Collapse" />
                     </Box>
-                  </Box>
-                ) : null}
+                  ) : null}
+                </Box>
 
                 <Divider color="secondary" />
-                <Link
-                  href="https://docs.olympusdao.finance/using-the-website/unstaking_lp"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <Button size="large" variant="contained" color="secondary" fullWidth>
-                    <Typography align="left">Unstake Legacy LP Token</Typography>
-                  </Button>
-                </Link>
               </Paper>
             </Fade>
           );
