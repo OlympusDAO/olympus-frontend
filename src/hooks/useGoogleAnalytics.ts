740ce6f9106abc485204f4882159483691a07015
CPTN <51892050+CPTN021@users.noreply.github.com>
2021-11-16 12:04:47 +0100

    added GA Hook

--- src/hooks/useGoogleAnalytics.ts
+++ src/hooks/useGoogleAnalytics.ts
@@ -9,7 +9,7 @@ const useGoogleAnalytics = () => {
   const location = useLocation();
 
   useEffect(() => {
-    if (GA_API_KEY) {
+    if (GA_API_KEY && GA_API_KEY.length > 1) {
       ReactGA.initialize(GA_API_KEY);
       ReactGA.set({ anonymizeIp: true });
       ReactGA.pageview(location.pathname + location.search);
