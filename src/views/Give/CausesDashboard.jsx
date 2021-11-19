126f458f009e75343815f7fa476a15dae31284be
Lienid <0xLienid@protonmail.com>
2021-11-19 15:25:29 -0500

    Framework for causes UI. kind of janky

--- src/views/Give/CausesDashboard.jsx
+++ src/views/Give/CausesDashboard.jsx
@@ -1,6 +1,6 @@
 import { useCallback, useState } from "react";
 import "./give.scss";
-import { Button, Paper, Typography, Zoom } from "@material-ui/core";
+import { Button, Paper, Typography, Zoom, Grid } from "@material-ui/core";
 import { useWeb3Context } from "src/hooks/web3Context";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 
@@ -20,6 +20,64 @@ export default function CausesDashboard() {
               <div className="give-yield-title">
                 <Typography variant="h5">Causes Dashboard</Typography>
               </div>
+            </div>
+            <div className="causes-body">
+              <Grid container spacing={2} className="data-grid">
+                <Grid item xs={12} className="cause-card">
+                  <div className="cause-image">IM Logo</div>
+                  <div className="cause-content">
+                    <div className="cause-title">
+                      <Typography variant="h6">ImpactMarket</Typography>
+                    </div>
+                    <div className="cause-body">
+                      <Typography variant="body2">Lorem ipsum dolor sit amet</Typography>
+                    </div>
+                    <div className="cause-misc-info">
+                      <Typography variant="body2">Time remaining</Typography>
+                      <Typography variant="body2">% complete</Typography>
+                      <Button variant="outlined" color="secondary">
+                        Give
+                      </Button>
+                    </div>
+                  </div>
+                </Grid>
+                <Grid item xs={12} className="cause-card">
+                  <div className="cause-image">Gitcoin Logo</div>
+                  <div className="cause-content">
+                    <div className="cause-title">
+                      <Typography variant="h6">Gitcoin</Typography>
+                    </div>
+                    <div className="cause-body">
+                      <Typography variant="body2">Lorem ipsum dolor sit amet</Typography>
+                    </div>
+                    <div className="cause-misc-info">
+                      <Typography variant="body2">Time remaining</Typography>
+                      <Typography variant="body2">% complete</Typography>
+                      <Button variant="outlined" color="secondary">
+                        Give
+                      </Button>
+                    </div>
+                  </div>
+                </Grid>
+                <Grid item xs={12} className="cause-card">
+                  <div className="cause-image">Angel Logo</div>
+                  <div className="cause-content">
+                    <div className="cause-title">
+                      <Typography variant="h6">Angel Protocol</Typography>
+                    </div>
+                    <div className="cause-body">
+                      <Typography variant="body2">Lorem ipsum dolor sit amet</Typography>
+                    </div>
+                    <div className="cause-misc-info">
+                      <Typography variant="body2">Time remaining</Typography>
+                      <Typography variant="body2">% complete</Typography>
+                      <Button variant="outlined" color="secondary">
+                        Give
+                      </Button>
+                    </div>
+                  </div>
+                </Grid>
+              </Grid>
             </div>
           </Paper>
         </Zoom>
