4252afaaf1a97dabfced1b596d7775d343bf1974
casuwu <taken404@gmail.com>
2021-11-19 20:27:30 -0500

    migrating in gregs changes

--- src/components/TopBar/OhmMenuViews/walletViews/InitialWalletView.jsx
+++ src/components/TopBar/OhmMenuViews/walletViews/InitialWalletView.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
-import { useTheme } from "@material-ui/core/styles";
+import { useTheme, makeStyles } from "@material-ui/core/styles";
 import { trim } from "../../../../helpers";
 import { ReactComponent as ArrowUpIcon } from "../../../../assets/icons/fullscreen.svg";
 import { ReactComponent as sOhmTokenImg } from "../../../../assets/tokens/token_sOHM.svg";
 import { ReactComponent as ohmTokenImg } from "../../../../assets/tokens/token_OHM.svg";
 import { ReactComponent as t33TokenImg } from "../../../../assets/tokens/token_33T.svg";
 import { ReactComponent as wsOhmTokenImg } from "src/assets/tokens/token_wsOHM.svg";
+import { ReactComponent as wethTokenImg } from "src/assets/tokens/wETH.svg";
+import { ReactComponent as abracadabraTokenImg } from "src/assets/tokens/MIM.svg";
+import rariTokenImg from "src/assets/tokens/RARI.png";
+
 import OhmImg from "src/assets/tokens/token_OHM.svg";
 import SOhmImg from "src/assets/tokens/token_sOHM.svg";
 import WsOhmImg from "src/assets/tokens/token_wsOHM.svg";
@@ -31,7 +35,136 @@ import {
   AccordionSummary,
   AccordionDetails,
   Divider,
+  Link,
 } from "@material-ui/core";
+
+import { dai, frax } from "src/helpers/AllBonds";
+const useStyles = makeStyles(theme => ({
+  menuContainer: {
+    padding: "16px",
+    width: "400px",
+  },
+  closeMenuButton: {
+    marginLeft: "auto",
+  },
+  menuItem: {
+    padding: "6px 16px",
+  },
+  viewAllBonds: {
+    marginTop: "6px",
+  },
+  menuSection: {
+    display: "flex",
+    flexDirection: "column",
+    gap: theme.spacing(1),
+    padding: theme.spacing(1, 0),
+  },
+}));
+
+const ExternalLinkIcon = ({ size = 20 }) => (
+  <SvgIcon
+    component={ArrowUpIcon}
+    style={{ height: `${size} px`, width: `${size} px`, verticalAlign: "sub" }}
+    htmlColor="#A3A3A3"
+  />
+);
+const ExternalLink = props => <Link target="_blank" rel="noreferrer" {...props} />;
+
+const MenuItemBond = ({ Icon1, Icon2, lpName }) => {
+  return (
+    <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        <Icon1 style={{ height: "25px", width: "25px" }} />
+        <Icon2 style={{ height: "25px", width: "25px" }} />
+        <Box sx={{ display: "flex", flexDirection: "column", ml: "8px" }}>
+          <Typography>{lpName}</Typography>
+          <ExternalLink href="#">
+            <Typography variant="body2" color="textSecondary">
+              Get LP <ExternalLinkIcon size={16} />
+            </Typography>
+          </ExternalLink>
+        </Box>
+      </Box>
+      <Box sx={{ display: "flex", flexDirection: "column", textAlign: "right" }}>
+        <Button size="small" color="secondary" variant="outlined">
+          <Typography>Bond to Save 12%</Typography>
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+const MenuItemBorrow = ({ Icon1, Icon2, borrowOn, totalAvailable }) => (
+  <Button size="large" variant="contained" color="secondary" fullWidth>
+    <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", width: "100%" }}>
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        <Icon1 style={{ height: "25px", width: "25px" }} />
+        <Icon2 style={{ height: "25px", width: "25px" }} />
+      </Box>
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        <Box sx={{ display: "flex", flexDirection: "column", textAlign: "right" }}>
+          <Typography>Borrow on {borrowOn}</Typography>
+          {totalAvailable && (
+            <Typography variant="body2" color="textSecondary">
+              {totalAvailable} Available
+            </Typography>
+          )}
+        </Box>
+        <Box component={ExternalLinkIcon} sx={{ ml: "6px" }} />
+      </Box>
+    </Box>
+  </Button>
+);
+
+const MenuItemUserToken = ({ name, icon, userBalance, userBalanceUSD, onExpandedChange, expanded, toggleDrawer }) => {
+  return (
+    <Accordion expanded={expanded} onChange={onExpandedChange}>
+      <AccordionSummary
+        expandIcon={<SvgIcon component={ArrowUpIcon} viewBox="0 0 32 32" style={{ height: "25px", width: "25px" }} />}
+      >
+        <Button variant="contained" style={{ width: "100%", flexDirection: "row" }} color="secondary">
+          <Typography align="left" style={{ width: "100%", flexDirection: "row" }}>
+            <SvgIcon component={icon} viewBox="0 0 32 32" style={{ height: "25px", width: "25px" }} />
+            {name}
+          </Typography>
+          <Box>
+            <Typography align="left">{userBalance}</Typography>
+            <Typography align="left">${userBalanceUSD}</Typography>
+          </Box>
+        </Button>
+      </AccordionSummary>
+      <AccordionDetails margin="auto" style={{ margin: "auto", padding: 0 }}>
+        <Box className="ohm-pairs" style={{ width: "100%" }}>
+          <Button
+            variant="contained"
+            style={{ backgroundColor: "#272D36", color: "#386794", width: "33%", minHeight: "50px" }}
+            onClick={toggleDrawer("sOHMtx")}
+            color="secondary"
+          >
+            <Typography align="left"> Transaction History</Typography>
+          </Button>
+          <Button
+            variant="contained"
+            style={{ backgroundColor: "#272D36", color: "#386794", width: "33%", minHeight: "50px" }}
+            min-height="60px"
+            onClick={toggleDrawer("sOHMLHIW")}
+            color="secondary"
+          >
+            <Typography align="left"> Learn how it works</Typography>
+          </Button>
+          <Button
+            variant="contained"
+            style={{ backgroundColor: "#272D36", color: "#386794", width: "33%", minHeight: "50px" }}
+            color="secondary"
+            onClick={toggleDrawer("sOHMZaps")}
+          >
+            <Typography align="left"> Zap</Typography>
+          </Button>
+        </Box>
+      </AccordionDetails>
+    </Accordion>
+  );
+};
 
 const addTokenToWallet = (tokenSymbol, tokenAddress, address) => async () => {
   if (window.ethereum) {
@@ -80,6 +213,7 @@ const addTokenToWallet = (tokenSymbol, tokenAddress, address) => async () => {
 };
 function InitialWalletView() {
   const theme = useTheme();
+  const styles = useStyles();
   const { chainID, address } = useWeb3Context();
   const networkID = chainID;
   const isEthereumAPIAvailable = window.ethereum;
@@ -119,40 +253,8 @@ function InitialWalletView() {
       setExpanded(isExpanded ? panel : false);
     }
   };
-  // apollo(rebasesDataQuery).then(r => {
-  //   let apy = r.data.rebases.map(entry => ({
-  //     apy: Math.pow(parseFloat(entry.percentage) + 1, 365 * 3) * 100,
-  //     timestamp: entry.timestamp,
-  //   }));
-  //   [
-  //     {
-  //       "apy": 7857.424722561997,
-  //       "timestamp": "1636797374"
-  //     }
-  //   ]
-  //   apy = apy.filter(pm => pm.apy < 300000);
-  //   setApy(apy);
-  //   √
-  // });
   return (
     <Paper>
-      {/* <Chart
-        type="line"
-        scale="log"
-        data={apy}
-        dataKey={["apy"]}
-        color={theme.palette.text.primary}
-        stroke={[theme.palette.text.primary]}
-        headerText="APY over time"
-        dataFormat="percent"
-        headerSubText={`${apy && trim(apy[0].apy, 2)}%`}
-        bulletpointColors={bulletpoints.apy}
-        itemNames={tooltipItems.apy}
-        itemType={itemType.percentage}
-        infoTooltipMessage={tooltipInfoMessages.apy}
-        expandedGraphStrokeColor={theme.palette.graphStrokeColor}
-      /> */}
-
       <Accordion expanded={expanded === "OHM"} onChange={handleChange("OHM")}>
         <AccordionSummary
           expandIcon={<SvgIcon component={ArrowUpIcon} viewBox="0 0 32 32" style={{ height: "25px", width: "25px" }} />}
