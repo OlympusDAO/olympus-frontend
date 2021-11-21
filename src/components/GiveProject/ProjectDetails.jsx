f35e118f16396fcad8b0dcb0546ba1336d4453d3
Joao Tavares <joao.tavares@twistag.com>
2021-11-21 16:45:32 -0100

    refactored project detail rendering code into separate component. Created mock project json file and iterating over it to render project related data.

---
+++ src/components/GiveProject/ProjectDetails.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Button, Typography, Grid } from "@material-ui/core";
+
+export default function ProjectDetails({ logo, title, details, remainingTime, completion }) {
+  return (
+    <>
+      <Grid item xs={12} className="cause-card">
+        {console.log("rendergin")}
+        <div className="cause-image">{logo}</div>
+        <div className="cause-content">
+          <div className="cause-title">
+            <Typography variant="h6">{title}</Typography>
+          </div>
+          <div className="cause-body">
+            <Typography variant="body2">{details}</Typography>
+          </div>
+          <div className="cause-misc-info">
+            <Typography variant="body2">Time remaining: {remainingTime}</Typography>
+            <Typography variant="body2">{completion}% complete</Typography>
+            <Button variant="outlined" color="secondary">
+              Give
+            </Button>
+          </div>
+        </div>
+      </Grid>
+    </>
+  );
+}
--- src/views/Give/CausesDashboard.jsx
+++ src/views/Give/CausesDashboard.jsx
@@ -1,17 +1,40 @@
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import "./give.scss";
 import { Button, Paper, Typography, Zoom, Grid } from "@material-ui/core";
 import { useWeb3Context } from "src/hooks/web3Context";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
+import ProjectDetails from "src/components/GiveProject/ProjectDetails";
+import data from "./mock_projects.json";
 
 export default function CausesDashboard() {
   const { provider, address, connected, connect, chainID } = useWeb3Context();
   const [zoomed, setZoomed] = useState(false);
   const isSmallScreen = useMediaQuery("(max-width: 705px)");
+  const [test, setTest] = useState(0);
+
+  const { projects } = data;
+
+  const renderProjects = useMemo(() => {
+    return projects.map(project => {
+      const { id, logo, title, details, remainingTime, completion } = project;
+      return (
+        <ProjectDetails
+          key={id}
+          logo={logo}
+          title={title}
+          details={details}
+          remainingTime={remainingTime}
+          completion={completion}
+        />
+      );
+    });
+  }, [projects]);
 
   return (
     <>
       <div className="give-view">
+        <div>{test}</div>
+        <button onClick={() => setTest(test + 1)}>setTest</button>
         <Zoom in={true}>
           <Paper className={`ohm-card secondary ${isSmallScreen && "mobile"}`}>
             <div className="card-header">
@@ -21,60 +44,7 @@ export default function CausesDashboard() {
             </div>
             <div className="causes-body">
               <Grid container spacing={2} className="data-grid">
-                <Grid item xs={12} className="cause-card">
-                  <div className="cause-image">IM Logo</div>
-                  <div className="cause-content">
-                    <div className="cause-title">
-                      <Typography variant="h6">ImpactMarket</Typography>
-                    </div>
-                    <div className="cause-body">
-                      <Typography variant="body2">Lorem ipsum dolor sit amet</Typography>
-                    </div>
-                    <div className="cause-misc-info">
-                      <Typography variant="body2">Time remaining</Typography>
-                      <Typography variant="body2">% complete</Typography>
-                      <Button variant="outlined" color="secondary">
-                        Give
-                      </Button>
-                    </div>
-                  </div>
-                </Grid>
-                <Grid item xs={12} className="cause-card">
-                  <div className="cause-image">Gitcoin Logo</div>
-                  <div className="cause-content">
-                    <div className="cause-title">
-                      <Typography variant="h6">Gitcoin</Typography>
-                    </div>
-                    <div className="cause-body">
-                      <Typography variant="body2">Lorem ipsum dolor sit amet</Typography>
-                    </div>
-                    <div className="cause-misc-info">
-                      <Typography variant="body2">Time remaining</Typography>
-                      <Typography variant="body2">% complete</Typography>
-                      <Button variant="outlined" color="secondary">
-                        Give
-                      </Button>
-                    </div>
-                  </div>
-                </Grid>
-                <Grid item xs={12} className="cause-card">
-                  <div className="cause-image">Angel Logo</div>
-                  <div className="cause-content">
-                    <div className="cause-title">
-                      <Typography variant="h6">Angel Protocol</Typography>
-                    </div>
-                    <div className="cause-body">
-                      <Typography variant="body2">Lorem ipsum dolor sit amet</Typography>
-                    </div>
-                    <div className="cause-misc-info">
-                      <Typography variant="body2">Time remaining</Typography>
-                      <Typography variant="body2">% complete</Typography>
-                      <Button variant="outlined" color="secondary">
-                        Give
-                      </Button>
-                    </div>
-                  </div>
-                </Grid>
+                {renderProjects}
               </Grid>
             </div>
             <div className="custom-recipient">
