52fd6f5c0b67d6dd184c58dc4ad6f21f286af016
mrunbanked <mrunbanked@protonmail.com>
2021-11-21 18:14:03 -0800

    revert bond history

--- src/views/Bond/BondHeader.jsx
+++ src/views/Bond/BondHeader.jsx
@@ -22,7 +22,7 @@ function BondHeader({ bond, slippage, recipientAddress, onRecipientAddressChange
 
   useEscape(() => {
     if (open) handleClose;
-    else history.push(`/${returnPath}`);
+    else history.push(`/bonds`);
   });
 
   return (
