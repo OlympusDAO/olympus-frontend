14c402afe3a6fc3714462b8e7c2c48bc6880bcb8
mrunbanked <mrunbanked@protonmail.com>
2021-11-23 16:18:05 -0800

    Add default

--- src/views/Zap/HeaderLogo.jsx
+++ src/views/Zap/HeaderLogo.jsx
@@ -4,6 +4,11 @@ const avatarStyle = { height: "35px", width: "35px", marginInline: "-4px" };
 const iconStyle = { height: "35px", width: "35px", marginInline: "-4px", zIndex: 1 };
 const viewBox = "0 0 32 32";
 export default function HeaderLogo({ images, icons, avatarStyleOverride }) {
+  if (images && !images.length) {
+    images = [
+      "https://storage.googleapis.com/zapper-fi-assets/tokens/ethereum/0x04f2694c8fcee23e8fd0dfea1d4f5bb8c352111f.png",
+    ];
+  }
   return (
     <Box display="flex" p={3} flexDirection="row">
       {images?.map(image => (
