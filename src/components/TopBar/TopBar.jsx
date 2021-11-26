ba13b8fe4fd2aecb33b842527750926ac9b508ae
casuwu <taken404@gmail.com>
2021-11-26 17:13:00 -0500

    Revert "Merge branch 'walletManagementV1' of https://github.com/OlympusDAO/olympus-frontend into walletManagementV1"
    This reverts commit 4623fd5ce4ba6fb8cc3f048a3784ed76a68c7fbf, reversing
    changes made to 7369bcbe9bad4827ebf60cbe80c90359abbaf87d.

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -2,7 +2,7 @@ import { AppBar, Toolbar, Box, Button, SvgIcon } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { ReactComponent as MenuIcon } from "../../assets/icons/hamburger.svg";
-// import OhmMenu from "./OhmMenu.jsx";
+import OhmMenu from "./OhmMenu.jsx";
 import ThemeSwitcher from "./ThemeSwitch.jsx";
 import LocaleSwitcher from "./LocaleSwitch.tsx";
 import ConnectMenu from "./ConnectMenu.jsx";
