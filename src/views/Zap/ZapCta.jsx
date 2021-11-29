6799d0314521b9b4e4887b2cb8884418633596d5
mrunbanked <mrunbanked@protonmail.com>
2021-11-29 12:20:20 -0800

    Add avax pool link

--- src/views/Zap/ZapCta.jsx
+++ src/views/Zap/ZapCta.jsx
@@ -1,7 +1,7 @@
 import { Box, Button, Paper, Typography, Grid, SvgIcon, Link } from "@material-ui/core";
 import "./zap.scss";
 import { ReactComponent as ArrowUp } from "../../assets/icons/arrow-up.svg";
-import HeaderLogo from "./HeaderLogo";
+import MultiLogo from "../../components/MultiLogo";
 import { NavLink } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import { Trans } from "@lingui/macro";
@@ -32,7 +32,7 @@ function ZapCta() {
       <Paper className="ohm-card" id="olyzaps-cta">
         <Grid container display="flex" className="cta-box" wrap>
           <Grid item xs={5} sm={3} className="icons-box">
-            <HeaderLogo images={[]} />
+            <MultiLogo images={[]} />
           </Grid>
           <Grid item xs={7} sm={5}>
             <Box alignItems="center" display="flex" flexDirection="column">
