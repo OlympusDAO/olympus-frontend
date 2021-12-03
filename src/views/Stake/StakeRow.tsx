c0bbe72c2de55e6dd589dee627e7b388e0a462ab
brightiron <brightiron@protonmail.com>
2021-12-02 18:51:33 -0600

    Breaking out Staking Balances into their own reusable component

---
+++ src/views/Stake/StakeRow.tsx
@@ -0,0 +1,30 @@
+import { Typography } from "@material-ui/core";
+import { Skeleton } from "@material-ui/lab";
+import { Trans } from "@lingui/macro";
+
+interface StakeRowProps {
+  title: string;
+  indented?: boolean;
+  id?: string;
+  balance: string;
+  isAppLoading?: boolean;
+}
+
+const StakeRow = (props: StakeRowProps) => {
+  return (
+    <div className="data-row" style={props.indented ? { paddingLeft: "10px" } : {}}>
+      <Typography variant={props.indented ? `body2` : `body1`} color={props.indented ? "textSecondary" : "primary"}>
+        <Trans>{props.title}</Trans>
+      </Typography>
+      <Typography
+        variant={props.indented ? `body2` : `body1`}
+        id={props.id}
+        color={props.indented ? "textSecondary" : "primary"}
+      >
+        {props.isAppLoading ? <Skeleton width="80px" /> : <>{props.balance}</>}
+      </Typography>
+    </div>
+  );
+};
+
+export default StakeRow;
