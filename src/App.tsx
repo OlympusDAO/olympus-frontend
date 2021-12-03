d4c0304b612b1ed436956e1bdad87469cd85c83c
appleseed-iii <80423742+appleseed-iii@users.noreply.github.com>
2021-12-03 16:04:36 -0600

    force loadAccount callback update on address change, too (#805)
    * force loadAccount callback update on address change, too
    
    * also move migrationAllowances into loadAccount since it should only fire for connectedAccounts

--- src/App.tsx
+++ src/App.tsx
@@ -133,7 +133,6 @@ function App() {
       bonds.map(bond => {
         dispatch(calcBondDetails({ bond, value: "", provider: loadProvider, networkID: networkId }));
       });
-      dispatch(getMigrationAllowances({ address, provider, networkID: networkId }));
     },
     [networkId],
   );
@@ -141,6 +140,7 @@ function App() {
   const loadAccount = useCallback(
     loadProvider => {
       dispatch(loadAccountDetails({ networkID: networkId, address, provider: loadProvider }));
+      dispatch(getMigrationAllowances({ address, provider, networkID: networkId }));
       bonds.map(bond => {
         if (bond.getAvailability(networkId)) {
           dispatch(calculateUserBondDetails({ address, bond, provider, networkID: networkId }));
@@ -153,7 +153,7 @@ function App() {
         }
       });
     },
-    [networkId],
+    [networkId, address],
   );
 
   // The next 3 useEffects handle initializing API Loads AFTER wallet is checked
