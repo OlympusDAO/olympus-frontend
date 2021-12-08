0b8d84f8c31c8e7b14a6bcb78f4f9a2cc0537cab
brightiron <95196612+brightiron@users.noreply.github.com>
2021-12-08 16:09:30 -0600

    Moving Treasury Top Level Metrics to Shared Metric Component (#823)
    * adding support for shared Metric Component to Treasury Dashboard
    
    * Swapping false/true so it logically reads correctly.

--- src/components/Metric/Metric.tsx
+++ src/components/Metric/Metric.tsx
@@ -1,20 +1,27 @@
-import { Typography } from "@material-ui/core";
+import { Typography, TypographyTypeMap } from "@material-ui/core";
 import { Skeleton } from "@material-ui/lab";
+import InfoTooltip from "src/components/InfoTooltip/InfoTooltip";
 
 interface MetricProps {
   className?: string;
   label?: string;
   metric?: string;
   isLoading?: boolean;
+  labelVariant?: TypographyTypeMap["props"]["variant"];
+  metricVariant?: TypographyTypeMap["props"]["variant"];
+  tooltip?: string;
 }
 
 const Metric = (props: MetricProps) => {
   return (
     <div className={props.className}>
-      <Typography variant="h5" color="textSecondary">
+      <Typography variant={props.labelVariant || `h5`} color="textSecondary">
         {props.label}
+        {props.tooltip && <InfoTooltip message={props.tooltip} children={undefined} />}
       </Typography>
-      <Typography variant="h4">{props.isLoading ? <span>{props.metric}</span> : <Skeleton width="150px" />}</Typography>
+      <Typography variant={props.metricVariant || `h4`} style={{ width: "100%" }}>
+        {props.isLoading ? <Skeleton width="100%" /> : <span>{props.metric}</span>}
+      </Typography>
     </div>
   );
 };
