113f4fd15224a7bfb86796bdef4b56025d7c7b2d
Lienid <0xLienid@protonmail.com>
2021-12-09 18:22:48 -0500

    More design tweaks and one typing change in AccountSlice

--- src/views/Give/GiveInfo.jsx
+++ src/views/Give/GiveInfo.jsx
@@ -6,20 +6,13 @@ export function GiveInfo() {
   return (
     <>
       <Paper className={"ohm-card secondary"}>
-        <div className="card-header">
-          <div className="give-yield-title">
-            <Typography variant="h5">How It Works</Typography>
-          </div>
-        </div>
         <div className="give-info">
           <Box className="give-info-deposit-box">
             <DepositSohm message="Deposit sOHM from wallet" />
           </Box>
-          <ArrowGraphic />
           <Box className="give-info-vault-box">
             <LockInVault message="Lock sOHM in vault" />
           </Box>
-          <ArrowGraphic />
           <Box className="give-info-yield-box">
             <ReceivesYield message="Recipient earns sOHM rebases" />
           </Box>
