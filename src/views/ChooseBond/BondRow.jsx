14f94c64d87c69f822f1e492bb873778cdddeabe
appleseed-iii <appleseed-iii@protonmail.com>
2021-12-11 22:54:30 -0600

    full width

--- src/views/ChooseBond/BondRow.jsx
+++ src/views/ChooseBond/BondRow.jsx
@@ -126,7 +126,7 @@ export function BondTableData({ bond }) {
       </TableCell>
       <TableCell>
         <Link component={NavLink} to={`/bonds/${bond.name}`}>
-          <Button variant="outlined" color="primary" disabled={!bond.isBondable[networkId]}>
+          <Button variant="outlined" color="primary" disabled={!bond.isBondable[networkId]} style={{ width: "100%" }}>
             {/* NOTE (appleseed): temporary for ONHOLD MIGRATION */}
             {/* <Typography variant="h6">{!bond.isBondable[networkId] ? t`Sold Out` : t`do_bond`}</Typography> */}
             <Typography variant="h6">{bond.isLOLable[networkId] ? bond.LOLmessage : t`do_bond`}</Typography>
