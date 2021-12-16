08e45a6255ee3f49c35088e98a8504a15e7fc632
0xJem <0x0xJem@gmail.com>
2021-12-16 22:45:48 +0400

    Recipient title consistency

--- src/views/Give/WithdrawDepositModal.tsx
+++ src/views/Give/WithdrawDepositModal.tsx
@@ -11,6 +11,7 @@ import { BigNumber } from "bignumber.js";
 import { Project } from "src/components/GiveProject/project.type";
 import { hasPendingGiveTxn, PENDING_TXN_WITHDRAW } from "src/slices/GiveThunk";
 import { t, Trans } from "@lingui/macro";
+import { shorten } from "src/helpers";
 
 export interface WithdrawSubmitCallback {
   (walletAddress: string, depositAmount: BigNumber): void;
@@ -63,7 +64,9 @@ export function WithdrawDepositModal({
   };
 
   const getRecipientTitle = () => {
-    if (!project) return walletAddress;
+    if (!project) return shorten(walletAddress);
+
+    if (!project.owner) return project.title;
 
     return project.owner + " - " + project.title;
   };
