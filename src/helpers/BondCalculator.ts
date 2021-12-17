10ac26c88bc7afebe5456c80368d805618957650
appleseed-iii <80423742+appleseed-iii@users.noreply.github.com>
2021-12-17 11:59:30 -0600

    bond calculator revert (#945)

--- src/helpers/BondCalculator.ts
+++ src/helpers/BondCalculator.ts
@@ -6,17 +6,17 @@ import { addresses } from "src/constants";
 import { BondCalcContract } from "../typechain";
 
 export const getBondCalculator = (networkID: NetworkID, provider: StaticJsonRpcProvider, v2Bond: boolean) => {
-  if (v2Bond) {
-    return new ethers.Contract(
-      addresses[networkID].BONDINGCALC_V2 as string,
-      BondCalcContractABI,
-      provider,
-    ) as BondCalcContract;
-  } else {
-    return new ethers.Contract(
-      addresses[networkID].BONDINGCALC_ADDRESS as string,
-      BondCalcContractABI,
-      provider,
-    ) as BondCalcContract;
-  }
+  // if (v2Bond) {
+  //   return new ethers.Contract(
+  //     addresses[networkID].BONDINGCALC_V2 as string,
+  //     BondCalcContractABI,
+  //     provider,
+  //   ) as BondCalcContract;
+  // } else {
+  return new ethers.Contract(
+    addresses[networkID].BONDINGCALC_ADDRESS as string,
+    BondCalcContractABI,
+    provider,
+  ) as BondCalcContract;
+  // }
 };
