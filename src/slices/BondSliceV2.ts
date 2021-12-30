da9be76540971224a9d53d1c8f017901e4eee5b8
mrunbanked <mrunbanked@protonmail.com>
2021-12-30 14:31:09 +0200

    Calculate bond discount

--- src/slices/BondSliceV2.ts
+++ src/slices/BondSliceV2.ts
@@ -17,6 +17,7 @@ import { addresses } from "src/constants";
 import { fetchAccountSuccess } from "./AccountSlice";
 import { NetworkID } from "src/lib/Bond";
 import { getTokenIdByContract, getTokenPrice } from "src/helpers";
+import { findOrLoadMarketPrice } from "./AppSlice";
 
 export const changeApproval = createAsyncThunk(
   "bonding/changeApproval",
@@ -34,6 +35,14 @@ export interface IBondV2 {
   index: number;
   displayName: string;
   price: number;
+  discount: number;
+  lastTune: number;
+  lastDecay: number;
+  length: number;
+  depositInterval: number;
+  tuneInterval: number;
+  baseDecimals: number;
+  quoteDecimals: number;
 }
 
 export interface IUserNote {
@@ -50,24 +59,43 @@ function checkNetwork(networkID: NetworkID) {
   }
 }
 
-export const getAllBonds = createAsyncThunk("bondsV2/getAll", async ({ provider, networkID }: IBaseAsyncThunk, {}) => {
-  checkNetwork(networkID);
-  const depositoryContract = BondDepository__factory.connect(addresses[networkID].BOND_DEPOSITORY, provider);
-  const liveBondIndexes = await depositoryContract.liveMarkets();
-  const liveBondPromises = liveBondIndexes.map(async index => await depositoryContract.markets(index));
-  let liveBonds: IBondV2[] = [];
+export const getAllBonds = createAsyncThunk(
+  "bondsV2/getAll",
+  async ({ provider, networkID }: IBaseAsyncThunk, { dispatch }) => {
+    checkNetwork(networkID);
+    const depositoryContract = BondDepository__factory.connect(addresses[networkID].BOND_DEPOSITORY, provider);
+    const liveBondIndexes = await depositoryContract.liveMarkets();
+    const liveBondPromises = liveBondIndexes.map(async index => await depositoryContract.markets(index));
+    const liveBondMetadataPromises = liveBondIndexes.map(async index => await depositoryContract.metadata(index));
+    let liveBonds: IBondV2[] = [];
 
-  for (let i = 0; i < liveBondPromises.length; i++) {
-    const bond = await liveBondPromises[i];
-    liveBonds.push({
-      ...bond,
-      index: +liveBondIndexes[i],
-      displayName: `${+liveBondIndexes[i]}`,
-      price: Number(await getTokenPrice(await getTokenIdByContract(bond.quoteToken))),
-    });
-  }
-  return liveBonds;
-});
+    const ohmPrice = (await dispatch(findOrLoadMarketPrice({ provider, networkID })).unwrap())?.marketPrice;
+
+    for (let i = 0; i < liveBondPromises.length && i < liveBondMetadataPromises.length; i++) {
+      const bondIndex = +liveBondIndexes[i];
+      const bond = await liveBondPromises[i];
+      const bondMetadata = await liveBondMetadataPromises[i];
+      const quoteTokenPrice = Number(await getTokenPrice((await getTokenIdByContract(bond.quoteToken)) ?? "dai"));
+      const quoteTokenDecimals = bondMetadata.quoteDecimals;
+      const payoutAmount = await depositoryContract.payoutFor(
+        ethers.utils.parseUnits("1", quoteTokenDecimals),
+        bondIndex,
+      );
+      const baseTokenDecimals = bondMetadata.baseDecimals;
+      const bondPrice = quoteTokenPrice / (payoutAmount.toNumber() / Math.pow(10, baseTokenDecimals));
+      const bondDiscount = (100 * (ohmPrice - bondPrice)) / ohmPrice;
+      liveBonds.push({
+        ...bond,
+        ...bondMetadata,
+        index: bondIndex,
+        displayName: `${bondIndex}`,
+        price: bondPrice,
+        discount: bondDiscount,
+      });
+    }
+    return liveBonds;
+  },
+);
 
 export const getUserNotes = createAsyncThunk(
   "bondsV2/getUser",
