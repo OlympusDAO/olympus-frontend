3b89862d73142b1487465c6266850df53bf246c5
mrunbanked <mrunbanked@protonmail.com>
2021-12-31 14:32:45 +0200

    Fix duration

--- src/views/BondV2/BondV2.tsx
+++ src/views/BondV2/BondV2.tsx
@@ -131,7 +131,6 @@ export const DisplayBondPrice = ({ bond }: { bond: IBondV2 }): ReactElement => {
   if (typeof bond.priceUSD === undefined) {
     return <Fragment>--</Fragment>;
   }
-  console.log(bond.price);
 
   return (
     <Fragment>
