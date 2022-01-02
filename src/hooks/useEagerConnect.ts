6f82ee82fda811600feb018e87d2445c6fe1f03c
Sam Potter <sam.potter@hotmail.com>
2022-01-02 20:31:12 +1100

    Temporarily remove provider connection analytics

--- src/hooks/useEagerConnect.ts
+++ src/hooks/useEagerConnect.ts
@@ -4,22 +4,25 @@ import { segmentUA } from "src/helpers/userAnalyticHelpers";
 import { useWeb3Context } from "./useWeb3Context";
 
 export const useEagerConnect = () => {
-  const location = useLocation();
+  // const location = useLocation();
   const [walletChecked, setWalletChecked] = useState(false);
-  const { isConnected, connect, provider, hasCachedProvider } = useWeb3Context();
+  const { isConnected, connect, hasCachedProvider } = useWeb3Context();
 
   useEffect(() => {
     if (!walletChecked && hasCachedProvider) {
-      connect().then(() => {
+      connect().catch(() => {
         setWalletChecked(true);
 
-        const currentPath = location.pathname + location.hash + location.search;
-        segmentUA({ type: "connect", provider, context: currentPath });
+        // (sam-potter)
+        // @TODO Move these analytics to a place where the `useLocation` hook
+        // is more accessible (under the necessary <Provider />)
+        // const currentPath = location.pathname + location.hash + location.search;
+        // segmentUA({ type: "connect", provider, context: currentPath });
       });
     } else {
       setWalletChecked(true);
     }
-  }, [connect, provider, location.pathname, location.hash, location.search, walletChecked, hasCachedProvider]);
+  }, [connect, walletChecked, hasCachedProvider]);
 
   useEffect(() => {
     if (!walletChecked && isConnected) {
