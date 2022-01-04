c92f59432a6d2e6e98b0e6a70cece3d152bbe101
0xJem <0x0xJem@gmail.com>
2022-01-04 12:04:41 +0400

    Remove redundant line

--- tests/setupTests.js
+++ tests/setupTests.js
@@ -3,7 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
-import { launchDApp, launchNode } from "./e2e/puppeteer/testHelpers";
 
 // TODO add jest-puppeteer preset
 
