f6272444d7ba2dbb02e9ffb0c96c0581275afa35
Alex Leventer <alex.leventer@datastax.com>
2022-01-05 13:37:37 -0800

    Upgrade getParameterByName

--- src/helpers/QueryParameterHelper.ts
+++ src/helpers/QueryParameterHelper.ts
@@ -1,12 +1,4 @@
 export function getParameterByName(name: string, url: string) {
-  const cleanName = escape(name.replace(/[\[\]]/g, "\\$&"));
-  const regex = new RegExp(`[?&]${cleanName}(=([^&#]*)|&|#|$)`),
-    results = regex.exec(url);
-  if (!results) {
-    return null;
-  }
-  if (!results[2]) {
-    return "";
-  }
-  return decodeURIComponent(results[2].replace(/\+/g, " "));
+  const urlParams = new URLSearchParams(url);
+  return urlParams.get(name);
 }
