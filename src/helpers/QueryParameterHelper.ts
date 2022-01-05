3f0e3065b519cc239fafd37671337c4db540af72
Alex Leventer <alex.leventer@datastax.com>
2022-01-05 13:02:18 -0800

    Fix codeql error

--- src/helpers/QueryParameterHelper.ts
+++ src/helpers/QueryParameterHelper.ts
@@ -1,8 +1,12 @@
 export function getParameterByName(name: string, url: string) {
-  name = name.replace(/[\[\]]/g, "\\$&");
-  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+  const cleanName = escape(name.replace(/[\[\]]/g, "\\$&"));
+  const regex = new RegExp(`[?&]${cleanName}(=([^&#]*)|&|#|$)`),
     results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return "";
+  if (!results) {
+    return null;
+  }
+  if (!results[2]) {
+    return "";
+  }
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
