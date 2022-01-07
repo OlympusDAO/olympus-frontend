fbd5d2745c1f8786cbcfb05b5e218e0174c1265f
Alex Leventer <alex.leventer@datastax.com>
2022-01-06 16:49:20 -0800

    Convert CardHeader to tsx

--- src/components/CardHeader/CardHeader.tsx
+++ src/components/CardHeader/CardHeader.tsx
@@ -1,8 +1,13 @@
 import { Box, Typography } from "@material-ui/core";
-// import "./cardheader.scss";
+import * as React from "react";
+
+interface CardHeaderProps {
+  title: string;
+  children: React.ReactNode;
+}
 
 // (aphex) removed children arg in favor of spread to silence compiler while refactoring parents
-const CardHeader = ({ title, ...props }) => {
+const CardHeader = ({ title, ...props }: CardHeaderProps) => {
   return (
     <Box className={`card-header`}>
       <Typography variant="h5">{title}</Typography>
