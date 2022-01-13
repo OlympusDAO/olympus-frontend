585b82bd00e109337a4135e732ee64eae3f3c115
mrunbanked <92614968+mrunbanked@users.noreply.github.com>
2022-01-12 20:35:48 -0800

    Mrunbanked/migration UI changes (#1158)
    * Add sohm option to migration modal
    
    * Rename asset
    
    * Add margin
    
    * Add space
    
    * Add dust handling to migration
    
    * Fix ohm conversion rate
    
    * Fix dust check and alignments
    
    * Change text
    
    * translations fix
    
    * other translations fixes
    
    * mobile styling fixes
    
    * mobile styling for migrationmodalSingle
    
    * change default to sOHM
    
    Co-authored-by: appleseed-iii <appleseed-iii@protonmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -55,6 +55,7 @@ import projectData from "src/views/Give/projects.json";
 import Announcement from "./components/Announcement/Announcement";
 import { getAllBonds, getUserNotes } from "./slices/BondSliceV2";
 import { NetworkId } from "./constants";
+import MigrationModalSingle from "./components/Migration/MigrationModalSingle";
 
 // 😬 Sorry for all the console logging
 const DEBUG = false;
@@ -218,6 +219,25 @@ function App() {
     return state.app.marketPrice * allAssetsBalance >= 10;
   });
 
+  const hasDust = useAppSelector(state => {
+    if (!state.app.currentIndex || !state.app.marketPrice) {
+      return true;
+    }
+    const wrappedBalance = Number(state.account.balances.wsohm) * Number(state.app.currentIndex!);
+    const ohmBalance = Number(state.account.balances.ohmV1);
+    const sOhmbalance = Number(state.account.balances.sohmV1);
+    if (ohmBalance > 0 && ohmBalance * state.app.marketPrice < 10) {
+      return true;
+    }
+    if (sOhmbalance > 0 && sOhmbalance * state.app.marketPrice < 10) {
+      return true;
+    }
+    if (wrappedBalance > 0 && wrappedBalance * state.app.marketPrice < 10) {
+      return true;
+    }
+    return false;
+  });
+
   const newAssetsDetected = useAppSelector(state => {
     return (
       state.account.balances &&
@@ -322,6 +342,7 @@ function App() {
               !hasActiveV1Bonds &&
               trimmedPath.indexOf("dashboard") === -1 &&
               oldAssetsEnoughToMigrate && <CallToAction setMigrationModalOpen={setMigrationModalOpen} />}
+
             {trimmedPath.indexOf("dashboard") === -1 && <Announcement />}
 
             <Switch>
@@ -426,8 +447,11 @@ function App() {
               <Route component={NotFound} />
             </Switch>
           </div>
-
-          <MigrationModal open={migrationModalOpen} handleClose={migModalClose} />
+          {hasDust ? (
+            <MigrationModalSingle open={migrationModalOpen} handleClose={migModalClose} />
+          ) : (
+            <MigrationModal open={migrationModalOpen} handleClose={migModalClose} />
+          )}
         </div>
       </ThemeProvider>
     </QueryClientProvider>
