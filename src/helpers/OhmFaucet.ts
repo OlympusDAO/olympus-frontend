ae7cf5132e0234cf0f16311167c2eb9f0fab0ae1
0xJem <0x0xjem@gmail.com>
2022-01-13 09:04:23 +0000

    WIP OHM faucet for local testnet. OhmFaucet.json copied from the olympus-contracts repo.

---
+++ src/helpers/OhmFaucet.ts
@@ -0,0 +1,33 @@
+import { addresses, NetworkId } from "src/constants";
+import { IBaseAddressAsyncThunk } from "src/slices/interfaces";
+import { error, info } from "../slices/MessagesSlice";
+import { OhmFaucet__factory } from "src/typechain";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const getOhm = createAsyncThunk(
+  "ohmFaucet",
+  async ({ address, networkID, provider }: IBaseAddressAsyncThunk, { dispatch }) => {
+    if (!provider) {
+      dispatch(error("Please connect your wallet in order to use the OHM faucet!"));
+      return;
+    }
+
+    // If not on a testnet, abort
+    if (networkID !== NetworkId.Localhost) {
+      dispatch(error("Cannot use the OHM faucet on this network."));
+      return;
+    }
+
+    // If the faucet contract doesn't exist, abort
+    if (!addresses[networkID].OHM_FAUCET || !addresses[networkID].OHM_FAUCET.trim().length)
+      throw new Error("OHM_FAUCET contract is not defined for chain ID " + networkID + ". Aborting.");
+
+    const signer = provider.getSigner();
+    const faucetContract = OhmFaucet__factory.connect(addresses[networkID].OHM_FAUCET, signer);
+
+    await faucetContract.dispense();
+    dispatch(info("OHM deposited into your wallet!"));
+
+    return;
+  },
+);
