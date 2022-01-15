34eb74700c98ec9abb6211461d4a24621d24f63d
Alex Leventer <alex.leventer@datastax.com>
2022-01-15 13:19:08 -0800

    Update tests

--- src/components/Announcement/__tests__/Announcement.unit.test.tsx
+++ src/components/Announcement/__tests__/Announcement.unit.test.tsx
@@ -5,5 +5,8 @@ describe("<Announcement/>", () => {
   it("should render component", () => {
     const { container } = render(<Announcement />);
     expect(container).toMatchSnapshot();
+    expect(container).toHaveTextContent(
+      "Treasury stats may be inaccurate during the migration. Please check discord if you have any questions.",
+    );
   });
 });
