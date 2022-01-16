54418df91eee2bbeeafbb1cf3fe74b60bdc4ff54
Alex Leventer <alex.leventer@datastax.com>
2022-01-16 08:11:28 -0800

    Bump eslint, fix prettier upgrade issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
       jsx: true,
     },
   },
-  extends: ["plugin:prettier/recommended", "prettier/react", "prettier/@typescript-eslint"],
+  extends: ["plugin:prettier/recommended"],
   plugins: ["prettier"],
   rules: {
     "prettier/prettier": ["error"],
