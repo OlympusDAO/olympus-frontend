13a9b0b32e6ce9dfcf1dae214fe1b00e8860ee99
0xJem <0x0xJem@gmail.com>
2022-01-17 15:51:01 +0400

    Consistent naming of category

--- src/components/GiveProject/ProjectCard.tsx
+++ src/components/GiveProject/ProjectCard.tsx
@@ -432,7 +432,7 @@ export default function ProjectCard({ project, mode }: ProjectDetailsProps) {
    */
   const handleProjectDetailsButtonClick = (source: string) => {
     ReactGA.event({
-      category: "Give",
+      category: "Olympus Give",
       action: "View Project",
       label: title,
       dimension1: address ?? "unknown",
