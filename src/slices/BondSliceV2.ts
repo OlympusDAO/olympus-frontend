331fcfe2e24e32fb005a01054757dd7a0dd96e12
appleseed-iii <80423742+appleseed-iii@users.noreply.github.com>
2022-01-18 15:06:17 -0600

    catch errors for bonding (#1257)
    * catch errors for bonding
    1. in BondSliceV2 when a given bond contract call fails (on the backend), catch & return the other bonds
    2. exclude bondsV1 from AppSlice contract calls unless isBondable || isLOLable
    
    * put bond in try (and remove from catch)

--- src/slices/BondSliceV2.ts
+++ src/slices/BondSliceV2.ts
@@ -313,14 +313,19 @@ export const getAllBonds = createAsyncThunk(
 
     for (let i = 0; i < liveBondIndexes.length; i++) {
       const bondIndex = +liveBondIndexes[i];
-      const bond: IBondV2Core = await liveBondPromises[i];
-      const bondMetadata: IBondV2Meta = await liveBondMetadataPromises[i];
-      const bondTerms: IBondV2Terms = await liveBondTermsPromises[i];
-      const finalBond = await processBond(bond, bondMetadata, bondTerms, bondIndex, provider, networkID, dispatch);
-      liveBonds.push(finalBond);
+      try {
+        const bond: IBondV2Core = await liveBondPromises[i];
+        const bondMetadata: IBondV2Meta = await liveBondMetadataPromises[i];
+        const bondTerms: IBondV2Terms = await liveBondTermsPromises[i];
+        const finalBond = await processBond(bond, bondMetadata, bondTerms, bondIndex, provider, networkID, dispatch);
+        liveBonds.push(finalBond);
 
-      if (address) {
-        dispatch(getTokenBalance({ provider, networkID, address, value: finalBond.quoteToken }));
+        if (address) {
+          dispatch(getTokenBalance({ provider, networkID, address, value: finalBond.quoteToken }));
+        }
+      } catch (e) {
+        console.log("getAllBonds Error for Bond Index: ", bondIndex);
+        console.log(e);
       }
     }
     return liveBonds;
