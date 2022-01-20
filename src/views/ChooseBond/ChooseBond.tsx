66438770acdacd32e859917d06adb684b7d90b87
brightiron <brightiron@protonmail.com>
2022-01-19 23:08:32 -0500

    moving to paper component library. Removing unreachable code

--- src/views/ChooseBond/ChooseBond.tsx
+++ src/views/ChooseBond/ChooseBond.tsx
@@ -98,7 +98,7 @@ function ChooseBond() {
   return (
     <div id="choose-bond-view">
       {(!isEmpty(accountNotes) || !isEmpty(accountBonds)) && <ClaimBonds activeNotes={accountNotes} />}
-
+      <ClaimBonds activeNotes={accountNotes} />
       <Zoom in={true}>
         <Paper headerText={`${t`Bond`} (1,1)`} topRight={topRightCTA}>
           <MetricCollection>
