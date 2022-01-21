c8482911b9ca0f8281aabf30b91c09d5e332d76d
Alex Leventer <alex.leventer@datastax.com>
2022-01-20 18:28:22 -0800

    Convert externalUrls component to TSX

--- src/components/Sidebar/externalUrls.tsx
+++ src/components/Sidebar/externalUrls.tsx
@@ -1,6 +1,13 @@
 import { Trans } from "@lingui/macro";
+import { ReactElement } from "react";
 
-const externalUrls = [
+export interface ExternalUrl {
+  title: ReactElement;
+  url: string;
+  icon: string;
+}
+
+const externalUrls: ExternalUrl[] = [
   {
     title: <Trans>Forum</Trans>,
     url: "https://forum.olympusdao.finance/",
@@ -21,11 +28,6 @@ const externalUrls = [
     url: "https://immunefi.com/bounty/olympus/",
     icon: "bug-report",
   },
-  // {
-  //   title: "Feedback",
-  //   url: "https://olympusdao.canny.io/",
-  //   icon: <SvgIcon color="primary" component={FeedbackIcon} />,
-  // },
 ];
 
 export default externalUrls;
