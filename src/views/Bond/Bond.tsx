ca2c8a6ec88e6252e97289e1ad706e18e8661063
mrunbanked <92614968+mrunbanked@users.noreply.github.com>
2022-01-27 10:01:22 -0800

    Mrunbanked/bond tsx (#1378)
    * Convert bonds to ts
    
    * Improve the look of the component
    
    * Add tests
    
    * Fix error
    
    * Remove unused file
    
    Co-authored-by: Alex Leventer <alex.leventer@datastax.com>

--- src/views/Bond/Bond.tsx
+++ src/views/Bond/Bond.tsx
@@ -32,7 +32,7 @@ const Bond = ({ bond }: { bond: IAllBondData }) => {
   const { provider, address, networkId } = useWeb3Context();
   usePathForNetwork({ pathName: "bonds", networkID: networkId, history });
 
-  const [slippage, setSlippage] = useState<string>("0.5");
+  const [slippage, setSlippage] = useState<number>(0.5);
   const [recipientAddress, setRecipientAddress] = useState<string>(address);
 
   const [view, setView] = useState<number>(0);
@@ -45,7 +45,7 @@ const Bond = ({ bond }: { bond: IAllBondData }) => {
   };
 
   const onSlippageChange = (e: InputEvent): void => {
-    return setSlippage(e.target.value);
+    return setSlippage(Number(e.target.value));
   };
 
   const onClickAway = (): void => {
