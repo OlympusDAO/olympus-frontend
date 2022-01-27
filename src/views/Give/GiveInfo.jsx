afa0aa2d59bd3f0073d63084942ba9ce68712bd0
0xJem <0x0xJem@gmail.com>
2022-01-27 17:15:09 +0400

    Better handling of breakpoints

--- src/views/Give/GiveInfo.jsx
+++ src/views/Give/GiveInfo.jsx
@@ -1,24 +1,21 @@
 import { t, Trans } from "@lingui/macro";
-import { Box, Button, Grid, Paper, SvgIcon, Typography, useMediaQuery } from "@material-ui/core";
+import { Box, Button, Grid, Paper, SvgIcon, Typography } from "@material-ui/core";
 
 import { ReactComponent as ArrowUp } from "../../assets/icons/arrow-up.svg";
 import { DepositSohm, LockInVault, ReceivesYield } from "../../components/EducationCard";
 
 export function GiveInfo() {
-  // TODO shift to using breakpoints
-  const isSmallScreen = useMediaQuery("(max-width: 600px)");
-
   return (
     <>
       <Paper className={"ohm-card secondary"}>
-        <Grid container className={`give-info ${isSmallScreen ? "smaller" : ""}`}>
-          <Grid item sm={8} md={3} className="give-info-deposit-box">
+        <Grid container className={"give-info"}>
+          <Grid item className="give-info-deposit-box">
             <DepositSohm message={t`Deposit sOHM from wallet`} />
           </Grid>
-          <Grid item sm={8} md={3} className="give-info-vault-box">
+          <Grid item className="give-info-vault-box">
             <LockInVault message={t`Lock sOHM in vault`} />
           </Grid>
-          <Grid item sm={8} md={3} className="give-info-yield-box">
+          <Grid item className="give-info-yield-box">
             <ReceivesYield message={t`Recipient earns sOHM rebases`} />
           </Grid>
         </Grid>
