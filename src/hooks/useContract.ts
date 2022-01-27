fa14953cf4e2c290f1352a3119e4f9754de8bcdf
Sam Potter <15123145+sam-potter@users.noreply.github.com>
2022-01-27 19:23:42 +1100

    Added static  argument

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -1,4 +1,5 @@
-import { Contract } from "ethers";
+import { Contract, ContractInterface } from "@ethersproject/contracts";
+import { StaticJsonRpcProvider } from "@ethersproject/providers";
 import { useMemo } from "react";
 import { abi as IERC20_ABI } from "src/abi/IERC20.json";
 import STAKING_ABI from "src/abi/OlympusStakingv2.json";
@@ -7,26 +8,45 @@ import { NetworkId } from "src/constants";
 import { AddressMap, STAKING_ADDRESSES } from "src/constants/addresses";
 import { assert } from "src/helpers";
 import { ohm_dai } from "src/helpers/AllBonds";
+import { NodeHelper } from "src/helpers/NodeHelper";
 import { IERC20, OlympusStakingv2, PairContract } from "src/typechain";
 
 import { useWeb3Context } from ".";
 
-export function useContract<TContract extends Contract>(addressOrMap: string, ABI: any): TContract;
-export function useContract<TContract extends Contract>(addressOrMap: AddressMap, ABI: any): TContract | null;
-export function useContract<TContract extends Contract>(addressOrMap: string | AddressMap, ABI: any): TContract | null {
-  const { provider, networkId } = useWeb3Context();
+/**
+ * Hook for fetching a contract.
+ *
+ * @param addressOrMap A contract address, or a map with a contract address for each network
+ * @param ABI The contract interface
+ * @param provider An optional static provider to be used by the contract
+ */
+export function useContract<TContract extends Contract = Contract>(
+  addressOrMap: AddressMap,
+  ABI: ContractInterface,
+): TContract | null;
+export function useContract<TContract extends Contract = Contract>(
+  addressOrMap: string,
+  ABI: ContractInterface,
+  provider?: StaticJsonRpcProvider,
+): TContract;
+export function useContract<TContract extends Contract = Contract>(
+  addressOrMap: string | AddressMap,
+  ABI: ContractInterface,
+  provider?: StaticJsonRpcProvider,
+): TContract | null {
+  const { provider: currentProvider, networkId } = useWeb3Context();
 
   return useMemo(() => {
     const address = typeof addressOrMap === "string" ? addressOrMap : addressOrMap[networkId as NetworkId];
     if (!address) return null;
 
     try {
-      return new Contract(address, ABI, provider) as TContract;
+      return new Contract(address, ABI, provider || currentProvider) as TContract;
     } catch (error) {
       console.error("Unable to get contract", error);
       return null;
     }
-  }, [addressOrMap, ABI, provider, networkId]);
+  }, [addressOrMap, ABI, provider, networkId, currentProvider]);
 }
 
 const usePairContract = (address: string) => {
@@ -35,8 +55,9 @@ const usePairContract = (address: string) => {
 
 export const useStakingContract = () => {
   const address = STAKING_ADDRESSES[NetworkId.MAINNET];
+  const provider = NodeHelper.getMainnetStaticProvider();
 
-  return useContract<OlympusStakingv2>(address, STAKING_ABI);
+  return useContract<OlympusStakingv2>(address, STAKING_ABI, provider);
 };
 
 export const useOhmDaiReserveContract = () => {
