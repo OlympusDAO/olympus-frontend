4be8e86cdbb3df756f3b529995d703988a9acc33
mrunbanked <mrunbanked@protonmail.com>
2022-01-27 15:39:00 +0000

    Enable menu item

--- src/components/Sidebar/NavContent.jsx
+++ src/components/Sidebar/NavContent.jsx
@@ -121,7 +121,7 @@ function NavContent({ handleDrawerToggle }) {
                   <NavItem to="/stake" icon="stake" label={t`Stake`} onClick={handleDrawerToggle} />
 
                   {/* NOTE (appleseed-olyzaps): OlyZaps disabled until v2 contracts */}
-                  {/*<NavItem to="/zap" icon="zap" label={t`Zap`} /> */}
+                  <NavItem to="/zap" icon="zap" label={t`Zap`} />
 
                   {EnvHelper.isGiveEnabled(location.search) && (
                     <NavItem to="/give" icon="give" label={t`Give`} chip={t`New`} onClick={handleDrawerToggle} />
