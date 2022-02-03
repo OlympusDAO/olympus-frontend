364f85010a7387d3b44f1b9fc73edf789a7a6c09
brightiron <brightiron@protonmail.com>
2022-02-02 18:02:26 -0600

    more stubs. Adding state vars. Calling gOHM Price.

--- src/views/Tender/Tender.tsx
+++ src/views/Tender/Tender.tsx
@@ -1,9 +1,19 @@
 import { t } from "@lingui/macro";
-import { Box, Grid, LinearProgress, makeStyles, Switch, Theme, Typography } from "@material-ui/core";
-import { DataRow, InfoNotification, InputWrapper, Paper, Tab, Tabs, TextButton } from "@olympusdao/component-library";
+import { Box, Divider, Grid, LinearProgress, makeStyles, Switch, Theme, Typography } from "@material-ui/core";
+import {
+  DataRow,
+  InfoNotification,
+  InputWrapper,
+  Paper,
+  PrimaryButton,
+  Tab,
+  Tabs,
+  TextButton,
+} from "@olympusdao/component-library";
 import { ChangeEvent, useEffect, useState } from "react";
 import { NetworkId } from "src/constants";
 import { trim } from "src/helpers";
+import { useGohmPrice } from "src/hooks/usePrices";
 import { balancesOf } from "src/lib/fetchBalances";
 
 const Tender = (props: { walletAddress: string }) => {
@@ -13,6 +23,10 @@ const Tender = (props: { walletAddress: string }) => {
   const [quantity, setQuantity] = useState("");
   const [daiValue, setDaiValue] = useState(0);
   const [gOhmValue, setgOHMValue] = useState(0);
+  const [depositedBalance, setDepositedBalance] = useState("0.00");
+  const [claimableBalance, setRedeemableBalance] = useState("0.00");
+  const [contractBalance, setContractBalance] = useState(0);
+  const { data: gOhmPrice } = useGohmPrice();
 
   const useStyles = makeStyles<Theme>(() => ({
     progress: {
@@ -24,15 +38,7 @@ const Tender = (props: { walletAddress: string }) => {
         backgroundColor: "#F8CC82",
       },
     },
-    switch: {
-      marginLeft: "8px",
-      marginRight: "8px",
-    },
   }));
-
-  const changeView: any = (_event: ChangeEvent<any>, newView: number) => {
-    setView(newView);
-  };
 
   useEffect(() => {
     balancesOf(props.walletAddress, NetworkId.FANTOM).then(res => {
@@ -41,32 +47,53 @@ const Tender = (props: { walletAddress: string }) => {
         setTokenBalance(token.balance);
       }
     });
+
+    //TODO: Contract call for Querying Deposited Balance
+    setDepositedBalance("10.00");
+
+    //TODO: Contract call for Querying Redeemable Balance
+    setRedeemableBalance("00.00");
+
+    //TODO: Contract Call for Contract Balance
+    setContractBalance(500000);
   }, [props.walletAddress]);
-
-  //TEMP Hardcoded
-
-  const buttonDisabled = false;
-  const currentgOHMPrice = 5000;
-  const stakeOnClick = () => console.log("Stake onClick");
 
   const setDeposit = (amount: string) => {
     setQuantity(amount);
     setDaiValue(parseInt(amount) * 50);
-    //ToDo: Need to display gOHM value. (Token Qty * 55 / currentgOHMUSDPrice)
-    //Where does current gOHMUSDPrice come from?
-    setgOHMValue((parseInt(amount) * 55) / currentgOHMPrice);
+    //Sets gOHM USD Equivalent value.
+    gOhmPrice && setgOHMValue((parseInt(amount) * 55) / gOhmPrice);
   };
 
-  const classes = useStyles();
+  //TODO: Contract call for Deposit Tokens
+  const deposit = () => console.log("Make the Contract Call for Deposit");
+
+  //TODO: Contract call for Redeem Tokens
+  const redeem = () => console.log("Make the Contract Call for Redeem");
+
+  const changeView: any = (_event: ChangeEvent<any>, newView: number) => {
+    setView(newView);
+  };
+
+  //disabled button if no token balance
+  const depositButtonDisabled = parseInt(tokenBalance) > 0 ? false : true;
+  const redeemButtonDisabled = parseInt(claimableBalance) > 0 ? false : true;
 
   //Currency formatters for the token balances
   const usdValue = quantity ? new Intl.NumberFormat("en-US").format(parseInt(quantity) * 55) : 0;
   const gOhm = new Intl.NumberFormat("en-US").format(gOhmValue);
   const dai = new Intl.NumberFormat("en-US").format(daiValue);
+  //if false gOHM, if true DAI
+  const redemptionToken = redeemToken ? "DAI" : "gOHM";
+  const contractBalanceFormatted = new Intl.NumberFormat("en-US").format(contractBalance);
+  //TODO: Is contract cap retrieved from the contract?
+  const progressValue = (contractBalance / 970000) * 100;
+
+  const classes = useStyles();
   return (
-    <Box display="flex" alignItems="center" justifyContent="center" alignContent="center">
-      <Box display="flex" flexDirection="column">
-        <InfoNotification style={{ width: "97%" }}>
+    <div id="stake-view">
+      <Box width="97%" maxWidth="833px">
+        <InfoNotification>
           This is a very important message about redeeming your Chicken Tender Offer.
           <TextButton
             href="#"
@@ -82,84 +109,80 @@ const Tender = (props: { walletAddress: string }) => {
             Learn More
           </TextButton>
         </InfoNotification>
-        <Paper headerText={t`Chicken Tender Offer`}>
-          <Box display="flex" justifyContent={"center"} mb={"10px"}>
-            <Typography>485,000/970,000 Chickens Deposited</Typography>
-          </Box>
-
-          <Box style={{ width: "50%", margin: "0 25%" }}>
-            <LinearProgress className={classes.progress} variant="determinate" value={50} />
-          </Box>
-
-          <Box
-            display="flex"
-            flexDirection="column"
-            justifyContent="space-between"
-            alignContent="center"
-            width="50%"
-          ></Box>
-          <Box className="stake-action-area">
-            <Tabs
-              centered
-              value={view}
-              textColor="primary"
-              indicatorColor="primary"
-              className="stake-tab-buttons"
-              onChange={changeView}
-              aria-label="stake tabs"
-              //hides the tab underline sliding animation in while <Zoom> is loading
-              TabIndicatorProps={{ style: { display: "none" } }}
-            >
-              <Tab label={t`Deposit`} />
-              <Tab label={t`Redeem`} />
-            </Tabs>
-
-            {props.walletAddress && (
-              <>
-                <Grid container className="stake-action-row">
-                  <InputWrapper
-                    id="amount-input"
-                    type="number"
-                    label={t`Enter an amount`}
-                    value={quantity}
-                    onChange={e => setDeposit(e.target.value)}
-                    labelWidth={0}
-                    endString={t`Max`}
-                    endStringOnClick={() => setDeposit(tokenBalance)}
-                    buttonText={t`Deposit`}
-                    buttonOnClick={stakeOnClick}
-                    disabled={buttonDisabled}
-                  />
-                  <Box
-                    display="flex"
-                    flexDirection="row"
-                    alignItems="center"
-                    justifyContent="space-between"
-                    marginTop={"15px"}
-                  >
-                    <Typography>
-                      Deposit Chicken for: {gOhm} gOHM (~${usdValue})
-                    </Typography>
-                    <Switch
-                      checked={redeemToken}
-                      onChange={() => setRedeemToken(!redeemToken)}
-                      color="default"
-                      className={classes.switch}
-                      inputProps={{ "aria-label": "stake to gohm" }}
-                    />
-                    <Typography> Deposit Chicken for: ${dai} DAI</Typography>
-                  </Box>
-                </Grid>
-                <Grid item>
-                  <DataRow title={t`Current Chicken Balance`} balance={`${trim(Number(tokenBalance), 4)} Chicken`} />
-                  <DataRow title={t`Deposited Balance`} balance={`0.00`} />
-                </Grid>
-              </>
-            )}
-          </Box>
-        </Paper>
       </Box>
-    </Box>
+
+      <Paper headerText={t`Chicken Tender Offer`}>
+        <Box display="flex" justifyContent={"center"} mb={"10px"}>
+          <Typography>{contractBalanceFormatted}/970,000 Chickens Deposited</Typography>
+        </Box>
+
+        <Box style={{ width: "50%", margin: "0 25%" }}>
+          <LinearProgress className={classes.progress} variant="determinate" value={progressValue} />
+        </Box>
+
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="space-between"
+          alignContent="center"
+          width="50%"
+        ></Box>
+        <Box className="stake-action-area">
+          <Tabs centered value={view} onChange={changeView} aria-label="stake tabs">
+            <Tab label={t`Deposit`} />
+            <Tab label={t`Redeem`} />
+          </Tabs>
+
+          {props.walletAddress && view === 0 ? (
+            <>
+              <InputWrapper
+                id="amount-input"
+                type="number"
+                label={t`Enter an amount`}
+                value={quantity}
+                onChange={e => setDeposit(e.target.value)}
+                labelWidth={0}
+                endString={t`Max`}
+                endStringOnClick={() => setDeposit(tokenBalance)}
+                buttonText={t`Deposit for ${redemptionToken}`}
+                buttonOnClick={() => deposit()}
+                disabled={depositButtonDisabled}
+              />
+              <Box
+                display="flex"
+                flexDirection="row"
+                alignItems="center"
+                justifyContent="space-between"
+                marginTop="15px"
+                marginBottom="25px"
+              >
+                <Typography>
+                  Deposit {quantity} Chicken for {gOhm} gOHM (~${usdValue})
+                </Typography>
+                <Switch checked={redeemToken} onChange={() => setRedeemToken(!redeemToken)} color="default" />
+                <Typography>
+                  Deposit {quantity} Chicken for ${dai} DAI
+                </Typography>
+              </Box>
+
+              <Grid item>
+                <Divider color="secondary" />
+                <DataRow title={t`Current Chicken Balance`} balance={`${trim(Number(tokenBalance), 4)} Chicken`} />
+                <DataRow title={t`Deposited Balance`} balance={`${depositedBalance} Chicken`} />
+              </Grid>
+            </>
+          ) : (
+            view === 1 && (
+              <Box display="flex" justifyContent={"center"} mt={"10px"}>
+                <PrimaryButton disabled={redeemButtonDisabled} onClick={() => redeem()}>
+                  Redeem
+                </PrimaryButton>
+              </Box>
+            )
+          )}
+        </Box>
+      </Paper>
+    </div>
   );
 };
 
