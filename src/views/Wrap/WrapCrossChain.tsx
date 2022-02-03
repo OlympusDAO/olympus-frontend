2e5965f327241b0adf118a9fe9614d0c2663b304
brightiron <95196612+brightiron@users.noreply.github.com>
2022-02-03 10:50:35 -0600

    WrapCrossChain . Paper to Component Library. Metric Component Refactor (#1386)
    * moving to paper and metric component library
    
    * wrap crosschain

--- src/views/Wrap/WrapCrossChain.tsx
+++ src/views/Wrap/WrapCrossChain.tsx
@@ -11,13 +11,11 @@ import {
   InputLabel,
   Link,
   OutlinedInput,
-  Paper,
-  SvgIcon,
   Typography,
   Zoom,
 } from "@material-ui/core";
 import { Skeleton } from "@material-ui/lab";
-import { InfoTooltip } from "@olympusdao/component-library";
+import { Icon, Metric, MetricCollection, Paper } from "@olympusdao/component-library";
 import { DataRow } from "@olympusdao/component-library";
 import { useCallback, useState } from "react";
 import { useDispatch } from "react-redux";
@@ -25,7 +23,6 @@ import { useAppSelector } from "src/hooks/index";
 import { useWeb3Context } from "src/hooks/web3Context";
 import { isPendingTxn, txnButtonTextMultiType } from "src/slices/PendingTxnsSlice";
 
-import { ReactComponent as ArrowUp } from "../../assets/icons/arrow-up.svg";
 import { NETWORKS } from "../../constants";
 import { formatCurrency, trim } from "../../helpers";
 import { switchNetwork } from "../../helpers/NetworkHelper";
@@ -184,63 +181,40 @@ function WrapCrossChain() {
   return (
     <div id="stake-view" className="wrapper">
       <Zoom in={true}>
-        <Paper className={`ohm-card`}>
+        <Paper
+          headerText={t`Wrap / Unwrap`}
+          topRight={
+            <Link
+              className="migrate-sohm-button"
+              style={{ textDecoration: "none" }}
+              href={"https://docs.olympusdao.finance/main/contracts/tokens#gohm"}
+              aria-label="wsohm-wut"
+              target="_blank"
+            >
+              <Typography>gOHM</Typography> <Icon name="arrow-up" style={{ marginLeft: "5px" }} />
+            </Link>
+          }
+        >
           <Grid container direction="column" spacing={2}>
             <Grid item>
-              <div className="card-header">
-                <Typography variant="h5">Wrap / Unwrap</Typography>
-                <Link
-                  className="migrate-sohm-button"
-                  style={{ textDecoration: "none" }}
-                  href={"https://docs.olympusdao.finance/main/contracts/tokens#gohm"}
-                  aria-label="wsohm-wut"
-                  target="_blank"
-                >
-                  <Typography>gOHM</Typography>{" "}
-                  <SvgIcon component={ArrowUp} color="primary" style={{ marginLeft: "5px", width: ".8em" }} />
-                </Link>
-              </div>
-            </Grid>
-
-            <Grid item>
-              <div className="stake-top-metrics">
-                <Grid container spacing={2} alignItems="flex-end">
-                  <Grid item xs={12} sm={4} md={4} lg={4}>
-                    <div className="wrap-sOHM">
-                      <Typography variant="h5" color="textSecondary">
-                        sOHM Price
-                      </Typography>
-                      <Typography variant="h4">
-                        {sOhmPrice ? formatCurrency(sOhmPrice, 2) : <Skeleton width="150px" />}
-                      </Typography>
-                    </div>
-                  </Grid>
-                  <Grid item xs={12} sm={4} md={4} lg={4}>
-                    <div className="wrap-index">
-                      <Typography variant="h5" color="textSecondary">
-                        Current Index
-                      </Typography>
-                      <Typography variant="h4">
-                        {currentIndex ? <>{trim(currentIndex, 1)} OHM</> : <Skeleton width="150px" />}
-                      </Typography>
-                    </div>
-                  </Grid>
-                  <Grid item xs={12} sm={4} md={4} lg={4}>
-                    <div className="wrap-wsOHM">
-                      <Typography variant="h5" color="textSecondary">
-                        {`${assetTo} Price`}
-                        <InfoTooltip
-                          message={`${assetTo} = sOHM * index\n\nThe price of ${assetTo} is equal to the price of OHM multiplied by the current index`}
-                          children={undefined}
-                        />
-                      </Typography>
-                      <Typography variant="h4">
-                        {gOhmPrice ? formatCurrency(gOhmPrice, 2) : <Skeleton width="150px" />}
-                      </Typography>
-                    </div>
-                  </Grid>
-                </Grid>
-              </div>
+              <MetricCollection>
+                <Metric
+                  label={`sOHM ${t`Price`}`}
+                  metric={formatCurrency(sOhmPrice, 2)}
+                  isLoading={sOhmPrice ? false : true}
+                />
+                <Metric
+                  label={t`Current Index`}
+                  metric={`${trim(currentIndex, 1)} OHM`}
+                  isLoading={currentIndex ? false : true}
+                />
+                <Metric
+                  label={`${assetTo} Price`}
+                  metric={formatCurrency(gOhmPrice, 2)}
+                  isLoading={gOhmPrice ? false : true}
+                  tooltip={`${assetTo} = sOHM * index\n\nThe price of ${assetTo} is equal to the price of OHM multiplied by the current index`}
+                />
+              </MetricCollection>
             </Grid>
 
             <div className="staking-area">
