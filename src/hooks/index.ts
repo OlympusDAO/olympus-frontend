cf9b0911b8da078d8a80c23253cf0ab747db426e
KΞITH <92357475+keith33m@users.noreply.github.com>
2022-02-04 22:20:06 +0000

    Cleanup/rename hooks and migrate to ts (#1318)
    * fix: add consistent hook file names, migrate useSegmentAnalytics to ts
    
    * chore: cleanup and rename hook file names for consistentcy
    
    * chore: cleanup and rename hook file names for consistentcy
    
    * fix: failing build
    
    * interface fix
    
    Co-authored-by: appleseed-iii <appleseed-iii@protonmail.com>

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,3 @@
-// export { default as useDebounce } from "./Debounce";
-
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "src/store";
 
@@ -7,4 +5,4 @@ export { useWeb3Context, useAddress } from "./web3Context";
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-export { default as useBonds } from "./Bonds";
+export { default as useBonds } from "./useBonds";
