c8e94cab4a8353558fff3a275a819796bbb3ff88
brightiron <brightiron@protonmail.com>
2022-02-03 20:13:38 -0600

    more cleanup

--- src/views/Tender/Tender.tsx
+++ src/views/Tender/Tender.tsx
@@ -54,6 +54,38 @@ const Tender = () => {
       },
     },
   }));
+  const classes = useStyles();
+
+  //exchange rate of gOhm from Deposit
+  const gOhmDepositExchangeRate =
+    gOhmExchangeRate && index && ohmPrice && (gOhmExchangeRate * 1e17) / (index * ohmPrice);
+
+  //amount displayed on the redeem page
+  const claimAmount =
+    choice === 1
+      ? depositedBalance &&
+        gOhmExchangeRate &&
+        gOhmDepositExchangeRate &&
+        (depositedBalance * gOhmDepositExchangeRate) / 1e18
+      : choice === 0 && depositedBalance && daiExchangeRate && depositedBalance * daiExchangeRate;
+  const redeemableBalString = claimAmount ? `${trim(Number(claimAmount), 4)} ${choice ? "gOHM" : "DAI"}` : "0.00";
+  const progressValue = totalDeposits && maxDeposits ? (totalDeposits / maxDeposits) * 100 : 0;
+
+  //If both exchange rates are positive and havent yet deposited, allow choice.
+  const allowChoice =
+    daiExchangeRate && daiExchangeRate > 0 && gOhmExchangeRate && gOhmExchangeRate > 0 && !depositedBalance;
+
+  //disabled button if no token balance or contract failed state
+  const depositButtonDisabled = Number(tokenBalance) > 0 && escrowState != "FAILED" ? false : true;
+  //disable if no deposited balance or escrow State === PENDING
+  const redeemButtonDisabled = !Number(depositedBalance) || escrowState === "PENDING" ? true : false;
+
+  //Currency formatters for the token balances
+  const usdValue = quantity ? new Intl.NumberFormat("en-US").format(Number(quantity) * 55) : 0;
+  const gOhm = new Intl.NumberFormat("en-US").format(gOhmValue);
+  const dai = new Intl.NumberFormat("en-US").format(daiValue);
+  const totalDepositsFormatted = totalDeposits && new Intl.NumberFormat("en-US").format(totalDeposits);
+  const maxDepositsFormatted = maxDeposits && new Intl.NumberFormat("en-US").format(maxDeposits);
 
   useEffect(() => {
     //mapping this to state so choice and redeemToken are always the same
@@ -71,33 +103,20 @@ const Tender = () => {
   };
 
   //Check escrow state and assign variables.
-  let redeemButtonText = "Redeem";
+  let redeemButtonText = `Redeem for ${redeemableBalString}`;
   let redeemButtonOnClick = () => {
     return;
   };
+  let message = "";
   if (escrowState === "FAILED") {
-    redeemButtonText = "Withdraw";
+    redeemButtonText = `Withdraw for ${depositedBalance} Chicken`;
     redeemButtonOnClick = () => Withdraw();
+    message = "The offer has not been accepted by the founders. Withdraw your tokens below.";
   }
   if (escrowState === "SUCCESS") {
     redeemButtonOnClick = () => Redeem();
+    message = "The offer has been accepted by the founders. Redeem your tokens below.";
   }
-
-  //disabled button if no token balance or contract failed state
-  const depositButtonDisabled = Number(tokenBalance) > 0 && escrowState != "FAILED" ? false : true;
-  //disable button if no token balance and state != PENDING
-  const redeemButtonDisabled = Number(depositedBalance) > 0 && escrowState === "PENDING" ? true : false;
-
-  //Currency formatters for the token balances
-  const usdValue = quantity ? new Intl.NumberFormat("en-US").format(Number(quantity) * 55) : 0;
-  const gOhm = new Intl.NumberFormat("en-US").format(gOhmValue);
-  const dai = new Intl.NumberFormat("en-US").format(daiValue);
-  const totalDepositsFormatted = totalDeposits && new Intl.NumberFormat("en-US").format(totalDeposits);
-  const maxDepositsFormatted = maxDeposits && new Intl.NumberFormat("en-US").format(maxDeposits);
-
-  //If both exchange rates are positive and havent yet deposited, allow choice.
-  const allowChoice =
-    daiExchangeRate && daiExchangeRate > 0 && gOhmExchangeRate && gOhmExchangeRate > 0 && !depositedBalance;
 
   //Set Strings from 0 or 1
   const redemptionToken = () => {
@@ -110,46 +129,69 @@ const Tender = () => {
     }
   };
 
-  //exchange rate of gOhm from Deposit
-  const gOhmDepositExchangeRate =
-    gOhmExchangeRate && index && ohmPrice && (gOhmExchangeRate * 1e17) / (index * ohmPrice);
-
-  //amount displayed on the redeem page
-  const claimAmount =
-    choice === 1
-      ? depositedBalance &&
-        gOhmExchangeRate &&
-        gOhmDepositExchangeRate &&
-        (depositedBalance * gOhmDepositExchangeRate) / 1e18
-      : choice === 0 && depositedBalance && daiExchangeRate && depositedBalance * daiExchangeRate;
-
-  const progressValue = totalDeposits && maxDeposits ? (totalDeposits / maxDeposits) * 100 : 0;
   const changeView: any = (_event: ChangeEvent<any>, newView: number) => {
     setView(newView);
   };
 
-  const classes = useStyles();
+  const DepositLimitMessage = () => (
+    <Box display="flex" justifyContent="center" mt="10px" mb="10px">
+      <Typography variant="h5" color="textSecondary">
+        Deposit limit has been reached
+      </Typography>
+    </Box>
+  );
+
+  const RedemptionToggle = () => (
+    <>
+      <Box
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="space-between"
+        marginTop="15px"
+        marginBottom="25px"
+        textAlign="center"
+      >
+        <Typography>
+          Deposit {quantity} Chicken for ${dai} DAI
+        </Typography>
+        <Switch
+          checked={redeemToken ? true : false}
+          onChange={() => setRedeemToken(redeemToken ? 0 : 1)}
+          color="default"
+        />
+        <Typography>
+          Deposit {quantity} Chicken for {gOhm} gOHM (~${usdValue})
+        </Typography>
+      </Box>
+      <Divider color="secondary" />
+    </>
+  );
+
+  const NotificationMessage = () => (
+    <Box width="97%" maxWidth="833px">
+      <InfoNotification>
+        This is a very important message about redeeming your Chicken Tender Offer.
+        <TextButton
+          href="#"
+          style={{
+            margin: "0px",
+            marginLeft: "10px",
+            padding: "0px",
+            lineHeight: "20px",
+            paddingBottom: "2px",
+            height: "inherit",
+          }}
+        >
+          Learn More
+        </TextButton>
+      </InfoNotification>
+    </Box>
+  );
 
   return (
     <div id="stake-view">
-      <Box width="97%" maxWidth="833px">
-        <InfoNotification>
-          This is a very important message about redeeming your Chicken Tender Offer.
-          <TextButton
-            href="#"
-            style={{
-              margin: "0px",
-              marginLeft: "10px",
-              padding: "0px",
-              lineHeight: "20px",
-              paddingBottom: "2px",
-              height: "inherit",
-            }}
-          >
-            Learn More
-          </TextButton>
-        </InfoNotification>
-      </Box>
+      <NotificationMessage />
       <Paper headerText={t`Chicken Tender Offer`}>
         <Box display="flex" justifyContent={"center"} mb={"10px"}>
           <Typography>
@@ -166,61 +208,44 @@ const Tender = () => {
           </Tabs>
           {address && view === 0 ? (
             <>
-              <InputWrapper
-                id="amount-input"
-                type="number"
-                label={t`Enter an amount`}
-                value={quantity}
-                onChange={e => e.target.value && setDeposit(Number(e.target.value))}
-                labelWidth={0}
-                endString={t`Max`}
-                endStringOnClick={() => tokenBalance && setDeposit(tokenBalance)}
-                buttonText={t`Deposit for ${redemptionToken}`}
-                buttonOnClick={() => Deposit(quantity, redeemToken)}
-                disabled={depositButtonDisabled}
-              />
-              {allowChoice && (
+              {totalDeposits && maxDeposits && totalDeposits / maxDeposits >= 1 ? (
+                <DepositLimitMessage />
+              ) : (
                 <>
-                  <Box
-                    display="flex"
-                    flexDirection="row"
-                    alignItems="center"
-                    justifyContent="space-between"
-                    marginTop="15px"
-                    marginBottom="25px"
-                    textAlign="center"
-                  >
-                    <Typography>
-                      Deposit {quantity} Chicken for ${dai} DAI
-                    </Typography>
-                    <Switch
-                      checked={redeemToken ? true : false}
-                      onChange={() => setRedeemToken(redeemToken ? 0 : 1)}
-                      color="default"
-                    />
-                    <Typography>
-                      Deposit {quantity} Chicken for {gOhm} gOHM (~${usdValue})
-                    </Typography>
-                  </Box>
-                  <Divider color="secondary" />
+                  <InputWrapper
+                    id="amount-input"
+                    type="number"
+                    label={t`Enter an amount`}
+                    value={quantity}
+                    onChange={e => e.target.value && setDeposit(Number(e.target.value))}
+                    labelWidth={0}
+                    endString={t`Max`}
+                    endStringOnClick={() => tokenBalance && setDeposit(tokenBalance)}
+                    buttonText={t`Deposit for ${redemptionToken}`}
+                    buttonOnClick={() => Deposit(quantity, redeemToken)}
+                    disabled={depositButtonDisabled}
+                  />
+                  {allowChoice && <RedemptionToggle />}
                 </>
               )}
-
               <Grid item>
                 <DataRow title={t`Current Chicken Balance`} balance={`${trim(Number(tokenBalance), 4)} Chicken`} />
                 <DataRow title={t`Deposited Balance`} balance={`${depositedBalance} Chicken `} />
+                <DataRow title={t`Redeemable Balance if Accepted`} balance={redeemableBalString} />
               </Grid>
             </>
           ) : (
             view === 1 && (
               <Box display="flex" justifyContent={"center"} mt={"10px"}>
                 <Box display="flex" flexDirection="column">
-                  <Typography>
-                    Redeem for {claimAmount} {choice ? "gOHM" : "DAI"}
+                  <Typography variant="body1" color="textSecondary">
+                    {message}
                   </Typography>
-                  <PrimaryButton disabled={redeemButtonDisabled} onClick={redeemButtonOnClick}>
-                    {redeemButtonText}
-                  </PrimaryButton>
+                  <Box display="flex" justifyContent={"center"} mt={"10px"}>
+                    <PrimaryButton disabled={redeemButtonDisabled} onClick={redeemButtonOnClick}>
+                      {redeemButtonText}
+                    </PrimaryButton>
+                  </Box>
                 </Box>
               </Box>
             )
