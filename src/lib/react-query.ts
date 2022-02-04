40134e5b4d6d59d9ddb2dbd2be33fa95e717bfb4
Sam Potter <15123145+sam-potter@users.noreply.github.com>
2022-02-04 11:41:15 +1100

    Turned off all default refetches

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -4,6 +4,8 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnMount: false,
+      refetchInterval: false,
+      refetchOnReconnect: false,
       refetchOnWindowFocus: false,
       notifyOnChangeProps: "tracked",
     },
