3651da362ab18a02d936e2d01c63b908f6b40333
Sam Potter <15123145+sam-potter@users.noreply.github.com>
2022-02-16 13:55:53 +1100

    Improve reference stability

--- src/hooks/useStaticProvider.ts
+++ src/hooks/useStaticProvider.ts
@@ -5,12 +5,7 @@ import { NodeHelper } from "src/helpers/NodeHelper";
 const providers = {} as Record<NetworkId, StaticJsonRpcProvider>;
 
 export const useStaticProvider = (networkId: NetworkId) => {
-  const provider = providers[networkId];
+  if (!providers[networkId]) providers[networkId] = NodeHelper.getAnynetStaticProvider(networkId);
 
-  if (!provider) {
-    providers[networkId] = NodeHelper.getAnynetStaticProvider(networkId);
-    return providers[networkId];
-  }
-
-  return provider;
+  return providers[networkId];
 };
