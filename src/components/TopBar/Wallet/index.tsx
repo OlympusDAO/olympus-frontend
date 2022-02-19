b4c94bb253ac45ce2325eb4592ed4429cf94fdce
brightiron <brightiron@protonmail.com>
2022-02-19 13:10:44 -0600

    nav link

--- src/components/TopBar/Wallet/index.tsx
+++ src/components/TopBar/Wallet/index.tsx
@@ -63,7 +63,7 @@ export function Wallet(props: { open?: boolean; component?: string; currentPath?
               { label: "Wallet", to: "/wallet" },
               { label: "Get OHM", to: "/get" },
               { label: "Calculator", to: "/calculator" },
-              { label: "Info", to: "/info" },
+              { label: "Info", to: "/info/faq" },
             ]}
             mb={"18px"}
           />
