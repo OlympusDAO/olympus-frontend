38d123683ff674156e3d1b51a27950f3e793c5dd
appleseed-iii <appleseed-iii@protonmail.com>
2022-02-24 09:27:25 -0600

    add other chains into nextRewardAmount calc

--- src/views/Stake/components/StakeArea/components/StakeActionArea/components/StakeNextRebaseAmount.tsx
+++ src/views/Stake/components/StakeArea/components/StakeActionArea/components/StakeNextRebaseAmount.tsx
@@ -1,29 +1,58 @@
 import { t } from "@lingui/macro";
 import { DataRow } from "@olympusdao/component-library";
-import { convertGohmToOhm, formatNumber, parseBigNumber } from "src/helpers";
-import { useGohmBalance, useSohmBalance } from "src/hooks/useBalance";
+import { BigNumber } from "ethers";
+import { convertGohmToOhm, formatNumber, nonNullable, parseBigNumber } from "src/helpers";
+import {
+  useFuseBalance,
+  useGohmBalance,
+  useGohmTokemakBalance,
+  useSohmBalance,
+  useV1SohmBalance,
+  useWsohmBalance,
+} from "src/hooks/useBalance";
 import { useCurrentIndex } from "src/hooks/useCurrentIndex";
 import { useStakingRebaseRate } from "src/hooks/useStakingRebaseRate";
 import { useTestableNetworks } from "src/hooks/useTestableNetworks";
+import { NetworkId } from "src/networkDetails";
 
 export const StakeNextRebaseAmount = () => {
-  const networks = useTestableNetworks();
-  const { data: currentIndex } = useCurrentIndex();
   const { data: rebaseRate } = useStakingRebaseRate();
 
-  const gohmBalances = useGohmBalance();
   const sohmBalances = useSohmBalance();
-  const gohmBalance = gohmBalances[networks.MAINNET].data;
-  const sohmBalance = sohmBalances[networks.MAINNET].data;
+  const gohmBalances = useGohmBalance();
+  const wsohmBalances = useWsohmBalance();
+  const v1sohmBalances = useV1SohmBalance();
+  const gohmFuseBalances = useFuseBalance();
+  const gohmTokemakBalances = useGohmTokemakBalance();
+
+  const networks = useTestableNetworks();
+  const { data: currentIndex } = useCurrentIndex();
+
+  const sohmTokens = [sohmBalances[networks.MAINNET].data, v1sohmBalances[networks.MAINNET].data];
+  const totalSohmBalance = sohmTokens.filter(nonNullable).reduce((res, bal) => res.add(bal), BigNumber.from(0));
+
+  const gohmTokens = [
+    gohmBalances[networks.MAINNET].data,
+    gohmBalances[NetworkId.ARBITRUM].data,
+    gohmBalances[NetworkId.AVALANCHE].data,
+    gohmBalances[NetworkId.POLYGON].data,
+    gohmBalances[NetworkId.FANTOM].data,
+    wsohmBalances[NetworkId.MAINNET].data,
+    wsohmBalances[NetworkId.ARBITRUM].data,
+    wsohmBalances[NetworkId.AVALANCHE].data,
+    gohmFuseBalances[NetworkId.MAINNET].data,
+    gohmTokemakBalances[NetworkId.MAINNET].data,
+  ];
+  const totalGohmBalance = gohmTokens.filter(nonNullable).reduce((res, bal) => res.add(bal), BigNumber.from(0));
 
   const props: PropsOf<typeof DataRow> = { title: t`Next Reward Amount` };
 
-  if (rebaseRate && sohmBalance && gohmBalance && currentIndex) {
-    const gohmBalanceAsSohm = convertGohmToOhm(gohmBalance, currentIndex);
+  if (rebaseRate && sohmBalances && totalGohmBalance && currentIndex) {
+    const gohmBalanceAsSohm = convertGohmToOhm(totalGohmBalance, currentIndex);
 
-    const totalSohmBalance = parseBigNumber(gohmBalanceAsSohm, 18) + parseBigNumber(sohmBalance);
+    const totalCombinedBalance = parseBigNumber(gohmBalanceAsSohm, 18) + parseBigNumber(totalSohmBalance);
 
-    const nextRewardAmount = rebaseRate * totalSohmBalance;
+    const nextRewardAmount = rebaseRate * totalCombinedBalance;
     props.balance = `${formatNumber(nextRewardAmount, 4)} sOHM`;
   } else props.isLoading = true;
 
