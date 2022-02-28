e47879ca1d61d68744cbc592b7391e980a9e7b61
Sam Potter <15123145+sam-potter@users.noreply.github.com>
2022-02-28 18:55:03 +1100

    Add tests

--- src/helpers/DecimalBigNumber/DecimalBigNumber.unit.test.ts
+++ src/helpers/DecimalBigNumber/DecimalBigNumber.unit.test.ts
@@ -20,4 +20,27 @@ describe("DecimalBigNumber", () => {
   it("discards irrelevant precision", () => {
     expect(new DecimalBigNumber("1.12345678913139872398723", 9).toAccurateString()).toEqual("1.123456789");
   });
+
+  it("adds numbers correctly", () => {
+    expect(new DecimalBigNumber("1.1", 9).add(new DecimalBigNumber("1.2", 18)).toAccurateString()).toEqual("2.3");
+  });
+
+  it("compares numbers correctly", () => {
+    expect(new DecimalBigNumber("1.1", 9).gt(new DecimalBigNumber("1.2", 18))).toEqual(false);
+    expect(new DecimalBigNumber("1.21", 9).gt(new DecimalBigNumber("1.2", 18))).toEqual(true);
+  });
+
+  it("multiples numbers correctly", () => {
+    // gOHM to OHM
+    const gohm = new DecimalBigNumber("1", 18); // 90 OHM
+    const index = new DecimalBigNumber("90", 9); // Index of 90
+    expect(gohm.mul(index, 9).toAccurateString()).toEqual("90.0"); // OHM to gOHM
+  });
+
+  it("divides numbers correctly", () => {
+    // OHM to gOHM
+    const ohm = new DecimalBigNumber("90", 9); // 90 OHM
+    const index = new DecimalBigNumber("90", 9); // Index of 90
+    expect(ohm.div(index, 18).toAccurateString()).toEqual("1.0"); // OHM to gOHM
+  });
 });
