b38a6e5cfc5c6ef275491674c6b17eaae5d6d96d
brightiron <brightiron@protonmail.com>
2022-02-28 22:24:02 -0600

    announcement message

--- src/views/Tender/NotificationMessage.tsx
+++ src/views/Tender/NotificationMessage.tsx
@@ -4,9 +4,11 @@ import { Icon, InfoNotification } from "@olympusdao/component-library";
 export const NotificationMessage = () => (
   <Box width="97%" maxWidth="833px">
     <InfoNotification>
-      This is a very important message about redeeming your SPA Tender Offer.
-      <Link href="#" style={{ marginLeft: "10px" }}>
-        Learn More
+      <Link
+        href="https://forum.olympusdao.finance/d/1096-oip-82a-amendment-to-tender-offer-for-spartacus-finance"
+        target="_blank"
+      >
+        Learn More about the Spartacus Finance Tender Offer
         <Icon name="arrow-up" style={{ marginLeft: "2px", verticalAlign: "bottom" }} />
       </Link>
     </InfoNotification>
