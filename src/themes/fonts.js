bcf63ab4b1604954748dd461137f727872a398a6
brightiron <brightiron@protonmail.com>
2022-03-05 22:04:55 -0600

    resolves #1541

--- src/themes/fonts.js
+++ src/themes/fonts.js
@@ -16,7 +16,7 @@ const square = {
 		url(${SquareWOFF}) format('woff')
 	`,
   unicodeRange:
-    "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+0400—04FF",
+    "U+0000-024F, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+0400—04FF",
 };
 
 const squareLight = {
@@ -30,7 +30,7 @@ const squareLight = {
 		url(${SquareLightWOFF}) format('woff')
 	`,
   unicodeRange:
-    "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+0400—04FF",
+    "U+0000-024F, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+0400—04FF",
 };
 
 const squareMedium = {
