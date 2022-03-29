22580611516a53635487f15651a017163efbd714
Sam Potter <15123145+sam-potter@users.noreply.github.com>
2022-03-29 18:19:44 +1100

    Prefer `useTokenPrice` for fetching token prices

--- src/hooks/useTokenPrice.ts
+++ src/hooks/useTokenPrice.ts
@@ -1,4 +1,5 @@
 import { useQuery } from "react-query";
+import { LPToken } from "src/helpers/contracts/LPToken";
 import { Token } from "src/helpers/contracts/Token";
 import { DecimalBigNumber } from "src/helpers/DecimalBigNumber/DecimalBigNumber";
 import { nonNullable } from "src/helpers/types/nonNullable";
@@ -7,7 +8,10 @@ import { NetworkId } from "src/networkDetails";
 export const tokenPriceQueryKey = (networkId: NetworkId, address: string) =>
   ["useTokenPrice", networkId, address].filter(nonNullable);
 
-export const useTokenPrice = <TToken extends Token = Token>(networkId: keyof TToken["addresses"], token: TToken) => {
+export const useTokenPrice = <TToken extends Token | LPToken = Token>(
+  networkId: keyof TToken["addresses"],
+  token: TToken,
+) => {
   const _networkId = networkId as NetworkId;
 
   const key = tokenPriceQueryKey(_networkId, token.getAddress(_networkId));
