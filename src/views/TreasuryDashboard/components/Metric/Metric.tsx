d30d041c7c66a21a892da05586bdbebdadf6b7cf
Jem <91760036+0xJem@users.noreply.github.com>
2022-04-02 14:38:25 +0400

    Add additional convenience methods to DecimalBigNumber (#1606)

--- src/views/TreasuryDashboard/components/Metric/Metric.tsx
+++ src/views/TreasuryDashboard/components/Metric/Metric.tsx
@@ -98,7 +98,7 @@ export const CurrentIndex: React.FC<AbstractedMetricProps> = props => {
     tooltip: t`The current index tracks the amount of sOHM accumulated since the beginning of staking. Basically, how much sOHM one would have if they staked and held 1 OHM from launch.`,
   };
 
-  if (currentIndex) _props.metric = `${currentIndex.toFormattedString(2)} sOHM`;
+  if (currentIndex) _props.metric = `${currentIndex.toString({ decimals: 2, trim: false, format: true })} sOHM`;
   else _props.isLoading = true;
 
   return <Metric {..._props} />;
