628964841a18121004f61862125306c0627a5f6e
brightiron <brightiron@protonmail.com>
2022-04-02 20:48:54 -0500

    wrap input area tests

--- src/views/Wrap/__tests__/WrapInputArea.unit.test.jsx
+++ src/views/Wrap/__tests__/WrapInputArea.unit.test.jsx
@@ -1,5 +1,6 @@
 import { fireEvent } from "@testing-library/react";
 import { BigNumber } from "ethers";
+import Messages from "src/components/Messages/Messages";
 import { useContractAllowance } from "src/hooks/useContractAllowance";
 import * as useWeb3Context from "src/hooks/web3Context";
 import { mockWeb3Context } from "src/testHelpers";
@@ -9,6 +10,7 @@ import Wrap from "../Wrap";
 
 jest.mock("src/hooks/useContractAllowance");
 let container;
+
 beforeEach(async () => {
   const data = jest.spyOn(useWeb3Context, "useWeb3Context");
   useContractAllowance.mockReturnValue({ data: BigNumber.from(10000) });
@@ -18,7 +20,12 @@ beforeEach(async () => {
   });
 
   await act(async () => {
-    ({ container } = render(<Wrap />));
+    ({ container } = render(
+      <>
+        <Messages />
+        <Wrap />
+      </>,
+    ));
   });
 });
 
@@ -40,5 +47,48 @@ describe("Wrap Input Area", () => {
     fireEvent.click(listbox.getByTestId("gohm-dropdown-select"));
     expect(await screen.findByText("Unwrap from gOHM"));
     expect(container).toMatchSnapshot();
+  });
+});
+
+describe("Check Wrap to gOHM Error Messages", () => {
+  it("Error message with no amount", async () => {
+    fireEvent.click(await screen.getByText("Wrap to gOHM"));
+    expect(await screen.findByText("Please enter a number")).toBeInTheDocument();
+  });
+
+  it("Error message with amount <=0", async () => {
+    fireEvent.change(await screen.findByPlaceholderText("Enter an amount of sOHM"), { target: { value: "-1" } });
+    fireEvent.click(await screen.getByText("Wrap to gOHM"));
+    expect(await screen.findByText("Please enter a number greater than 0")).toBeInTheDocument();
+  });
+
+  it("Error message amount > 0 but no wallet balance", async () => {
+    fireEvent.change(await screen.findByPlaceholderText("Enter an amount of sOHM"), { target: { value: "10000" } });
+    fireEvent.click(await screen.getByText("Wrap to gOHM"));
+    expect(await screen.findByText("Please refresh your page and try again")).toBeInTheDocument();
+  });
+});
+
+describe("Check Unwrap from gOHM Error Messages", () => {
+  beforeEach(() => {
+    fireEvent.mouseDown(screen.getByRole("button", { name: "sOHM" }));
+    const listbox = within(screen.getByRole("listbox"));
+    fireEvent.click(listbox.getByTestId("gohm-dropdown-select"));
+  });
+  it("Error message with no amount", async () => {
+    fireEvent.click(screen.getByText("Unwrap from gOHM"));
+    expect(await screen.findByText("Please enter a number")).toBeInTheDocument();
+  });
+
+  it("Error message with amount <=0", async () => {
+    fireEvent.change(await screen.findByPlaceholderText("Enter an amount of gOHM"), { target: { value: "-1" } });
+    fireEvent.click(screen.getByText("Unwrap from gOHM"));
+    expect(await screen.findByText("Please enter a number greater than 0")).toBeInTheDocument();
+  });
+
+  it("Error message amount > 0 but no wallet balance", async () => {
+    fireEvent.change(await screen.findByPlaceholderText("Enter an amount of gOHM"), { target: { value: "10000" } });
+    fireEvent.click(screen.getByText("Unwrap from gOHM"));
+    expect(await screen.findByText("Please refresh your page and try again")).toBeInTheDocument();
   });
 });
