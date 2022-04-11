600024fa2a29eb659797c9b22b4996804872a6df
Sam Potter <15123145+sam-potter@users.noreply.github.com>
2022-04-11 16:02:19 +1000

    Add automatic timeout

--- src/helpers/react-query/getQueryData.ts
+++ src/helpers/react-query/getQueryData.ts
@@ -1,19 +1,35 @@
 import { hashQueryKey, QueryKey } from "react-query";
 import { queryCache, queryClient } from "src/lib/react-query";
 
-export const getQueryData = async <TData>(key: QueryKey): Promise<TData> => {
-  return new Promise(resolve => {
-    const data: TData | undefined = queryClient.getQueryData(key);
+export const getQueryData = async <TData>(
+  key: QueryKey,
+  options?: {
+    /**
+     * The number of milliseconds before we cancel
+     * this request for data, preventing endlessly
+     * waiting for data that may never arrive
+     */
+    timeout?: number;
+  },
+): Promise<TData> => {
+  const { timeout = 10000 } = options || {};
 
+  return await new Promise((resolve, reject) => {
+    const data: TData | undefined = queryClient.getQueryData(key);
     if (data) resolve(data);
 
-    const _key = hashQueryKey(key);
+    const id = setTimeout(() => {
+      unsubscribe();
+      return reject(new Error("Request timed out"));
+    }, timeout);
 
+    const hashedKey = hashQueryKey(key);
     const unsubscribe = queryCache.subscribe(event => {
       if (event?.type === "queryUpdated") {
-        if (event.query.queryHash === _key) {
+        if (event.query.queryHash === hashedKey) {
           unsubscribe();
-          resolve(event.query.state.data);
+          clearTimeout(id);
+          return resolve(event.query.state.data);
         }
       }
     });
