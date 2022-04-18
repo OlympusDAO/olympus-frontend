daa32a56e82ddf78d0547fe883ad9e259a46f275
0xJem <0x0xJem@gmail.com>
2022-04-18 15:33:04 +0400

    Fix issues in view details button with md breakpoint

--- src/components/GiveProject/ProjectCard.tsx
+++ src/components/GiveProject/ProjectCard.tsx
@@ -466,10 +466,10 @@ export default function ProjectCard({ project, mode }: ProjectDetailsProps) {
             </Grid>
             <Grid item xs />
             <Grid item container xs={12} alignItems="flex-end">
-              <Grid item xs={12} sm={6} md={8}>
+              <Grid item xs={12} sm={6} lg={8}>
                 {renderGoalCompletion()}
               </Grid>
-              <Grid item xs={12} sm={6} md={4}>
+              <Grid item xs={12} sm={6} lg={4}>
                 <Link
                   href={`#/give/projects/${project.slug}`}
                   onClick={() => handleProjectDetailsButtonClick("View Details Button")}
