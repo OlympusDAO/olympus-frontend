1d8394a640967361afaaddd77853d221b969c855
0xJem <0x0xJem@gmail.com>
2022-04-20 17:25:13 +0400

    Add position and dimensions to ContractNode

--- src/components/Transparency/ContractNode.ts
+++ src/components/Transparency/ContractNode.ts
@@ -8,14 +8,23 @@ export type ContractNode = {
   icon?: string;
   style?: CSSProperties;
   type?: string; // Node type: input, output, group or default
+  x?: number;
+  y?: number;
+  width?: number;
+  height?: number;
 };
 
 export const getNode = (item: ContractNode): Node => {
+  const dimensions: CSSProperties = {
+    ...(item.width && { width: item.width }),
+    ...(item.height && { height: item.height }),
+  };
+
   return {
     id: item.address,
     data: { label: item.name },
-    position: { x: 0, y: 0 },
-    ...(item.style && { style: item.style }),
+    position: { x: item.x || 0, y: item.y || 0 },
+    ...((item.style || Object.keys(dimensions).length) && { style: { ...item.style, ...dimensions } }),
     ...(item.type && { type: item.type }),
   };
 };
--- src/components/Transparency/__tests__/ContractNode.unit.test.ts
+++ src/components/Transparency/__tests__/ContractNode.unit.test.ts
@@ -15,6 +15,25 @@ describe("ContractNode", () => {
       });
     });
 
+    it("should display a basic node with dimensions", () => {
+      const contractNode: ContractNode = {
+        name: "Node 1",
+        address: "0x1",
+        width: 10,
+        height: 11,
+      };
+
+      expect(getNode(contractNode)).toEqual({
+        id: "0x1",
+        data: { label: "Node 1" },
+        position: { x: 0, y: 0 },
+        style: {
+          width: 10,
+          height: 11,
+        },
+      });
+    });
+
     it("should display a basic node with a specified type", () => {
       const contractNode: ContractNode = {
         name: "Node 1",
@@ -43,6 +62,27 @@ describe("ContractNode", () => {
         position: { x: 0, y: 0 },
         style: {
           backgroundColor: "yellow",
+        },
+      });
+    });
+
+    it("should support a specified background color style and dimensions", () => {
+      const contractNode: ContractNode = {
+        name: "Node 1",
+        address: "0x1",
+        style: { backgroundColor: "yellow" },
+        width: 10,
+        height: 11,
+      };
+
+      expect(getNode(contractNode)).toEqual({
+        id: "0x1",
+        data: { label: "Node 1" },
+        position: { x: 0, y: 0 },
+        style: {
+          backgroundColor: "yellow",
+          width: 10,
+          height: 11,
         },
       });
     });
