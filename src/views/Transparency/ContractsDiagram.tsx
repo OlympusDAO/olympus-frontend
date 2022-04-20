8e40c21ac24f93b0813c38143f7ab3a5e903a829
0xJem <0x0xJem@gmail.com>
2022-04-20 17:25:30 +0400

    Populate more dummy data for contract. Remove auto-positioning.

--- src/views/Transparency/ContractsDiagram.tsx
+++ src/views/Transparency/ContractsDiagram.tsx
@@ -1,79 +1,13 @@
-import { Container, Grid } from "@material-ui/core";
-import ELK, { ElkExtendedEdge, ElkNode } from "elkjs";
-import { useEffect, useState } from "react";
-import ReactFlow, { Edge, Node, Position } from "react-flow-renderer";
+import { Container, Grid, useTheme } from "@material-ui/core";
+import { useState } from "react";
+import ReactFlow from "react-flow-renderer";
 
 import { initialEdges, initialNodes } from "./contractNodes";
 
-// Based on: https://reactflow.dev/docs/examples/layouting/
-const getElementsWithLayout = async (nodes: Node[], edges: Edge[]) => {
-  const graph: ElkNode = {
-    id: "root",
-    layoutOptions: { "elk.algorithm": "layered" },
-    children: [],
-    edges: [],
-  };
-
-  nodes.forEach(node => {
-    graph.children?.push({
-      id: node.id,
-      width: 50,
-      height: 20,
-    });
-  });
-
-  edges.forEach(edge => {
-    const newEdge: ElkExtendedEdge = {
-      id: edge.id,
-      sources: [edge.source],
-      targets: [edge.target],
-      sections: [],
-    };
-    graph.edges?.push(newEdge);
-  });
-
-  const elkNodes = await new ELK().layout(graph);
-
-  // Extract children into a map by id
-  const children: { [id: string]: ElkNode } = {};
-  elkNodes.children?.forEach(value => {
-    children[value.id] = value;
-  });
-
-  nodes.forEach(node => {
-    const nodeWithPosition = children[node.id];
-    node.targetPosition = Position.Bottom;
-    node.sourcePosition = Position.Top;
-    node.position = { x: nodeWithPosition.x || 0, y: nodeWithPosition.y || 0 };
-    node.width = nodeWithPosition.width;
-    node.height = nodeWithPosition.height;
-
-    console.log("new node ", node);
-
-    // We are shifting the dagre node position (anchor=center center) to the top left
-    // so it matches the React Flow node anchor point (top left).
-    // node.position = {
-    //   x: nodeWithPosition.x - nodeDimensions.width / 2,
-    //   y: nodeWithPosition.y - nodeDimensions.height / 2,
-    // };
-  });
-
-  return { nodes, edges };
-};
-
 export const ContractsDiagram = (): JSX.Element => {
-  const [nodes, setNodes] = useState(initialNodes);
-  const [edges, setEdges] = useState(initialEdges);
-
-  // TODO handle auto-positioning
-  useEffect(() => {
-    console.log("updating");
-    getElementsWithLayout(nodes, edges).then(value => {
-      setNodes(value.nodes);
-      setEdges(value.edges);
-      console.log("updated");
-    });
-  }, []);
+  const theme = useTheme();
+  const [nodes] = useState(initialNodes(theme));
+  const [edges] = useState(initialEdges);
 
   // TODO fix incompatibility with Paper from component-library (but not MUI) which results in the edge paths not being positioned correctly
 
@@ -86,7 +20,7 @@ export const ContractsDiagram = (): JSX.Element => {
            * components).
            */}
           <Container style={{ height: "80vh", width: "80vw" }}>
-            <ReactFlow nodes={nodes} edges={edges} nodesDraggable={false} nodesConnectable={false} />
+            <ReactFlow fitView nodes={nodes} edges={edges} nodesDraggable={false} nodesConnectable={false} />
           </Container>
         </Grid>
       </Grid>
--- src/views/Transparency/contractNodes.ts
+++ src/views/Transparency/contractNodes.ts
@@ -1,3 +1,5 @@
+import { Theme } from "@material-ui/core";
+import { CSSProperties } from "react";
 import { Edge, Node } from "react-flow-renderer";
 import { getEdges } from "src/components/Transparency/ContractEdge";
 import { getNodes } from "src/components/Transparency/ContractNode";
@@ -11,32 +13,51 @@ import {
   STAKING_ADDRESSES,
 } from "src/constants/addresses";
 
-export const initialNodes: Node[] = getNodes([
-  {
-    name: "Staking",
-    address: STAKING_ADDRESSES[NetworkId.MAINNET],
-  },
-  {
-    name: "Bond Depository",
-    address: BOND_DEPOSITORY_ADDRESSES[NetworkId.MAINNET],
-  },
-  {
-    name: "gOHM Token",
-    address: GOHM_ADDRESSES[NetworkId.MAINNET],
-  },
-  {
-    name: "sOHM Token",
-    address: SOHM_ADDRESSES[NetworkId.MAINNET],
-  },
-  {
-    name: "OHM Token",
-    address: OHM_ADDRESSES[NetworkId.MAINNET],
-  },
-  {
-    name: "DAO Treasury",
-    address: DAO_TREASURY_ADDRESSES[NetworkId.MAINNET],
-  },
-]);
+export const initialNodes = (theme: Theme): Node[] => {
+  console.log("theme ", theme);
+  const primaryStyle: CSSProperties = {
+    backgroundColor: theme.palette.primary.main,
+  };
+
+  return getNodes([
+    {
+      name: "Staking",
+      address: STAKING_ADDRESSES[NetworkId.MAINNET],
+      x: 50,
+      y: 200,
+      style: primaryStyle,
+    },
+    {
+      name: "Bond Depository",
+      address: BOND_DEPOSITORY_ADDRESSES[NetworkId.MAINNET],
+      x: 500,
+      y: 100,
+    },
+    {
+      name: "gOHM Token",
+      address: GOHM_ADDRESSES[NetworkId.MAINNET],
+      x: 250,
+      y: 0,
+    },
+    {
+      name: "sOHM Token",
+      address: SOHM_ADDRESSES[NetworkId.MAINNET],
+      x: 50,
+      y: 300,
+    },
+    {
+      name: "OHM Token",
+      address: OHM_ADDRESSES[NetworkId.MAINNET],
+    },
+    {
+      name: "DAO Treasury",
+      address: DAO_TREASURY_ADDRESSES[NetworkId.MAINNET],
+      x: 500,
+      y: 0,
+      style: primaryStyle,
+    },
+  ]);
+};
 
 export const initialEdges: Edge[] = getEdges([
   { source: STAKING_ADDRESSES[NetworkId.MAINNET], target: SOHM_ADDRESSES[NetworkId.MAINNET], label: "Mints new sOHM" },
