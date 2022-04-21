6ff1c40e0dae7c7ea45dc22b3d12e71faa670c7d
0xJem <0x0xJem@gmail.com>
2022-04-21 11:38:39 +0400

    Fix test snapshots

--- src/components/GiveProject/GiveBox.tsx
+++ src/components/GiveProject/GiveBox.tsx
@@ -13,7 +13,7 @@ interface GiveBoxProps {
  */
 export function GiveBox(props: GiveBoxProps) {
   return (
-    <Box border={1} className={`grey-box ${props?.overrideClass}`} borderRadius="10px" padding="20px">
+    <Box border={1} className={`grey-box ${props?.overrideClass || ""}`} borderRadius="10px" padding="20px">
       {props.children}
     </Box>
   );
