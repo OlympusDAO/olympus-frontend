72de48e01e1488222afcb9417237d12de8153f2a
0xJem <0x0xJem@gmail.com>
2022-04-25 16:00:23 +0400

    Revisit the approach to token holders and display a breakdown of holders by token

--- src/views/TreasuryDashboard/components/Metric/Metric.tsx
+++ src/views/TreasuryDashboard/components/Metric/Metric.tsx
@@ -1,7 +1,17 @@
 import { t } from "@lingui/macro";
 import { Metric } from "@olympusdao/component-library";
+import { useEffect, useState } from "react";
+import { UseQueryResult } from "react-query";
+import {
+  GOHM_ADDRESSES,
+  OHM_ADDRESSES,
+  SOHM_ADDRESSES,
+  V1_OHM_ADDRESSES,
+  V1_SOHM_ADDRESSES,
+} from "src/constants/addresses";
 import { formatCurrency, formatNumber } from "src/helpers";
 import { DecimalBigNumber } from "src/helpers/DecimalBigNumber/DecimalBigNumber";
+import { useWeb3Context } from "src/hooks";
 import { useCurrentIndex } from "src/hooks/useCurrentIndex";
 import { useGohmPrice, useOhmPrice } from "src/hooks/usePrices";
 import {
@@ -13,7 +23,7 @@ import {
   useTreasuryTotalBacking,
 } from "src/hooks/useProtocolMetrics";
 import { useStakingRebaseRate } from "src/hooks/useStakingRebaseRate";
-import { useAllTokenHolders } from "src/hooks/useTokenHolders";
+import { useTokenHolders } from "src/hooks/useTokenHolders";
 
 type MetricProps = PropsOf<typeof Metric>;
 type AbstractedMetricProps = Omit<MetricProps, "metric" | "label" | "tooltip" | "isLoading">;
@@ -172,14 +182,54 @@ export const TreasuryBalance: React.FC<AbstractedMetricProps> = props => {
 };
 
 export const Holders: React.FC<AbstractedMetricProps> = props => {
-  const { data: sOhmHolders } = useAllTokenHolders();
+  const { networkId } = useWeb3Context();
+  const v1OhmHolders = useTokenHolders(V1_OHM_ADDRESSES[networkId as keyof typeof V1_OHM_ADDRESSES]);
+  const v1sOhmHolders = useTokenHolders(V1_SOHM_ADDRESSES[networkId as keyof typeof V1_SOHM_ADDRESSES]);
+  const gOhmHolders = useTokenHolders(GOHM_ADDRESSES[networkId as keyof typeof GOHM_ADDRESSES]);
+  const ohmHolders = useTokenHolders(OHM_ADDRESSES[networkId as keyof typeof OHM_ADDRESSES]);
+  const sOhmHolders = useTokenHolders(SOHM_ADDRESSES[networkId as keyof typeof SOHM_ADDRESSES]);
+
+  type TokenResult = {
+    [key: string]: UseQueryResult<number, Error>;
+  };
+
+  const tokenResults: TokenResult = {
+    "OHM v1": v1OhmHolders,
+    "sOHM V1": v1sOhmHolders,
+    "gOHM V2": gOhmHolders,
+    "OHM V2": ohmHolders,
+    "sOHM V2": sOhmHolders,
+  };
+
+  const [holderNumber, setHolderNumber] = useState(0);
+  useEffect(() => {
+    let total = 0;
+
+    Object.values(tokenResults).forEach(element => {
+      if (element.isLoading || !element.data) return;
+
+      total += element.data;
+    });
+
+    setHolderNumber(total);
+  }, [v1OhmHolders, v1sOhmHolders, gOhmHolders, ohmHolders, sOhmHolders]);
+
+  const tooltipText = (tokenName: string, result: UseQueryResult<number, Error>) => {
+    return t`${tokenName}: ${result.isLoading || result.data === undefined ? "Loading" : formatNumber(result.data)}`;
+  };
 
   const _props: MetricProps = {
     ...props,
     label: t`Number of Holders`,
+    tooltip: Object.keys(tokenResults)
+      .map(key => {
+        const value = tokenResults[key];
+        return tooltipText(key, value);
+      })
+      .join("\n"),
   };
 
-  if (sOhmHolders) _props.metric = formatNumber(sOhmHolders, 0);
+  if (sOhmHolders) _props.metric = formatNumber(holderNumber, 0);
   else _props.isLoading = true;
 
   return <Metric {..._props} />;
