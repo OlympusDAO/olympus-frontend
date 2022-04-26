9521997ea9950a10a9fcd1d93c9d62443dfca2dd
appleseed-iii <appleseed-iii@protonmail.com>
2022-04-26 08:51:30 -0500

    update coinbasewallet connection rpc params

--- src/hooks/web3Context.tsx
+++ src/hooks/web3Context.tsx
@@ -67,20 +67,6 @@ const initModal = new Web3Modal({
   // network: "mainnet", // optional
   cacheProvider: true, // optional
   providerOptions: {
-    walletlink: {
-      package: CoinbaseWalletSDK, // Required
-      options: {
-        appName: "Olympus DAO",
-        rpc: {
-          1: NETWORKS[1].uri(),
-          4: NETWORKS[4].uri(),
-          42161: NETWORKS[42161].uri(),
-          421611: NETWORKS[421611].uri(),
-          43113: NETWORKS[43113].uri(),
-          43114: NETWORKS[43114].uri(),
-        },
-      },
-    },
     walletconnect: {
       package: WalletConnectProvider,
       options: {
@@ -92,6 +78,14 @@ const initModal = new Web3Modal({
           43113: NETWORKS[43113].uri(),
           43114: NETWORKS[43114].uri(),
         },
+      },
+    },
+    coinbasewallet: {
+      package: CoinbaseWalletSDK, // Required
+      options: {
+        appName: "Olympus DAO",
+        chainId: 1,
+        rpc: NETWORKS[1].uri(),
       },
     },
   },
