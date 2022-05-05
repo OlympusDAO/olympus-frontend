468a8ac2d6359d9d0e1f51e63a67d2031bc1a4b0
Sam Potter <15123145+sam-potter@users.noreply.github.com>
2022-05-05 13:47:34 +1000

    Make uni/sushi tokens dynamic

--- src/helpers/contracts/getLPTokenByAddress.ts
+++ src/helpers/contracts/getLPTokenByAddress.ts
@@ -3,13 +3,20 @@ import { NetworkId } from "src/networkDetails";
 import { getBalancerLPToken } from "./getBalancerLPToken";
 import { getCurveLPToken } from "./getCurveLPToken";
 import { getGelatoLPToken } from "./getGelatoLPToken";
+import { getUniOrSushiLPToken } from "./getUniOrSushiLPToken";
 
 export const getLPTokenByAddress = async ({ address, networkId }: { address: string; networkId: NetworkId }) => {
+  // Note that the order between gelato and uniOrSushi is important here. Since all contract
+  // calls to uniOrSushi are a subset of the calls to gelato, checking if a token is a gelato LP
+  // must always preceed checking if it's a uniOrSushi LP.
+  const gelato = await getGelatoLPToken({ address, networkId });
+  if (gelato) return gelato;
+
+  const uniOrSushi = await getUniOrSushiLPToken({ address, networkId });
+  if (uniOrSushi) return uniOrSushi;
+
   const balancer = await getBalancerLPToken({ address, networkId });
   if (balancer) return balancer;
 
-  const curve = await getCurveLPToken({ address, networkId });
-  if (curve) return curve;
-
-  return getGelatoLPToken({ address, networkId });
+  return getCurveLPToken({ address, networkId });
 };
