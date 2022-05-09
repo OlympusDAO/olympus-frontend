9bd674b720bcc4c568f282d75e6af401fdd57296
Sam Potter <sam.potter@hotmail.com>
2022-05-09 22:34:56 +1000

    Re-add support for bonding Curve + Balancer + GUniV3 LP tokens (#1753)
    * Add balancer vault contract
    
    * Add support for balancer LP
    
    * Remove pointless LPToken abstraction
    
    * Move file
    
    * Update ABI
    
    * Add Curve LP valuation function
    
    * Add gUniV3 LP valuation function
    
    * Remove `v2BondDetails`
    
    * Remove "gas" from ABI
    
    * Add tokens
    
    * Update ABIs
    
    * Add Curve factory contract
    
    * Get tokens dynamically
    
    * Update pricing functions
    
    * Improve type
    
    * Remove hardcoded tokens
    
    * Remove token addresses
    
    * Add early return
    
    * Add Rinkeby support
    
    * Cs
    
    * Add early return
    
    * Add Curve pool ABI
    
    * Get factory address dynamically
    
    * Remove static contract definition
    
    * Rename file
    
    * Make uni/sushi tokens dynamic
    
    * Remove static contract definitions
    
    * Add comments
    
    * Cs
    
    * Update test

--- src/views/Bond/__tests__/InverseBond.unit.test.jsx
+++ src/views/Bond/__tests__/InverseBond.unit.test.jsx
@@ -19,6 +19,7 @@ import {
 } from "../__mocks__/mockLiveMarkets";
 import { Bond } from "../Bond";
 import { BondModalContainer } from "../components/BondModal/BondModal";
+
 beforeEach(() => {
   const data = jest.spyOn(useWeb3Context, "useWeb3Context");
   data.mockReturnValue(mockWeb3Context);
@@ -26,9 +27,10 @@ beforeEach(() => {
   Token.OHM_TOKEN.getPrice = jest.fn().mockResolvedValue(new DecimalBigNumber("20"));
   Token.DAI_TOKEN.getPrice = jest.fn().mockResolvedValue(new DecimalBigNumber("1"));
   Token.OHM_DAI_LP_TOKEN.getPrice = jest.fn().mockResolvedValue(new DecimalBigNumber("200000"));
-  Token.OHM_WETH_LP_TOKEN.getPrice = jest.fn().mockResolvedValue(new DecimalBigNumber("1"));
-  Token.OHM_LUSD_LP_TOKEN.getPrice = jest.fn().mockResolvedValue(new DecimalBigNumber("1"));
+  Token.LUSD_TOKEN.getPrice = jest.fn().mockResolvedValue(new DecimalBigNumber("1"));
+  Token.FRAX_TOKEN.getPrice = jest.fn().mockResolvedValue(new DecimalBigNumber("1"));
 });
+
 jest.mock("react-router", () => ({
   ...jest.requireActual("react-router"),
   useParams: jest.fn(),
