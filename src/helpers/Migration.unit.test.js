ad42c6db0170c043fd729dc7cbd6a0e2122b181b
appleseed-iii <appleseed-iii@protonmail.com>
2022-05-26 08:42:19 -0500

    fast check float fixes

--- src/helpers/Migration.unit.test.js
+++ src/helpers/Migration.unit.test.js
@@ -3,9 +3,13 @@ import * as fc from "fast-check";
 import { formatCurrency } from "./Migration";
 
 test("formatCurrency always returns value starting with $", async () => {
+  // force 0.1 to a 32-bit float
+  const min32 = new Float32Array([0.1])[0];
   fc.assert(
-    fc.property(fc.float({ min: 0.1 }), value => {
+    fc.property(fc.float({ min: min32 }), value => {
       const formatted = formatCurrency(value);
+
+      console.log("formatted", value, formatted);
       expect(formatted).toStartWith("$");
     }),
   );
@@ -13,7 +17,7 @@ test("formatCurrency always returns value starting with $", async () => {
 
 test("formatCurrency splits thousands with comma", async () => {
   fc.assert(
-    fc.property(fc.float({ min: 1000, max: 999999 }), value => {
+    fc.property(fc.integer({ min: 1000, max: 999999 }), value => {
       const formatted = formatCurrency(value);
       expect(formatted).toContain(",");
       const chunks = formatted.split(",");
@@ -23,8 +27,11 @@ test("formatCurrency splits thousands with comma", async () => {
 });
 
 test("formatCurrency trims cents", async () => {
+  // force 1.01 to a 32-bit float
+  const float32 = new Float32Array([1.01])[0];
+  const max32 = new Float32Array([1.999])[0];
   fc.assert(
-    fc.property(fc.float({ min: 1.01, max: 1.999 }), value => {
+    fc.property(fc.float({ min: float32, max: max32 }), value => {
       const formatted = formatCurrency(value);
       expect(formatted).not.toContain(".");
     }),
