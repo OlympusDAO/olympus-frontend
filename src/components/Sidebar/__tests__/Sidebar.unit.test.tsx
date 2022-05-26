500f7c5c7627985361d98712b2beb6c820f3eaa5
brightiron <brightiron@protonmail.com>
2022-05-26 15:03:49 -0500

    sidebar snapshot

--- src/components/Sidebar/__tests__/Sidebar.unit.test.tsx
+++ src/components/Sidebar/__tests__/Sidebar.unit.test.tsx
@@ -1,9 +1,9 @@
-import { render } from "../../../testUtils";
+import { render, screen } from "../../../testUtils";
 import Sidebar from "../Sidebar";
 
 describe("<Sidebar/>", () => {
-  it("should render component", () => {
-    const { container } = render(<Sidebar />);
-    expect(container).toMatchSnapshot();
+  it("should Display Sidebar with Nav Items", () => {
+    render(<Sidebar />);
+    expect(screen.getByText("Dashboard"));
   });
 });
