4a1fa713362f6f1eb21226d2ccbec7143a9043c5
brightiron <brightiron@protonmail.com>
2022-05-31 12:08:43 -0500

    revert cta snapshot

--- src/components/CallToAction/__tests__/CallToAction.unit.test.tsx
+++ src/components/CallToAction/__tests__/CallToAction.unit.test.tsx
@@ -1,9 +1,9 @@
-import { render, screen } from "../../../testUtils";
+import { render } from "../../../testUtils";
 import CallToAction from "../CallToAction";
 
 describe("<CallToAction/>", () => {
-  it("should display CTA", () => {
-    render(<CallToAction setMigrationModalOpen={() => console.log("setMigrationModalOpen")} />);
-    expect(screen.getByText("You have assets ready to migrate to v2"));
+  it("should render component", () => {
+    const { container } = render(<CallToAction setMigrationModalOpen={() => console.log("setMigrationModalOpen")} />);
+    expect(container).toMatchSnapshot();
   });
 });
