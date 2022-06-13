1398f6cdcf53a7ea3bc26545f242e7136707e138
Lienid <0xLienid@protonmail.com>
2022-06-13 11:32:31 -0400

    Setting up re-deployed contracts. All works except bonds

--- src/views/Bond/hooks/useLiveBonds.ts
+++ src/views/Bond/hooks/useLiveBonds.ts
@@ -33,6 +33,8 @@ export const fetchLiveBonds = async ({ networkId, isInverseBond }: UseLiveBondsO
     }),
   );
 
+  console.log(promises);
+
   return promises
     .filter(({ status }) => status === "fulfilled")
     .map(promise => (promise as PromiseFulfilledResult<Bond>).value);
