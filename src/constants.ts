a72328f353b933e2ccb3d578a0a186553422a0cb
0xJem <0x0xJem@gmail.com>
2022-06-17 12:25:43 +0400

    Temporarily shift to using in-development subgraph for the dashboard

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const THE_GRAPH_URL = "https://api.thegraph.com/subgraphs/name/drondin/olympus-protocol-metrics";
+export const THE_GRAPH_URL = "https://api.studio.thegraph.com/query/28103/olympus-protocol-metrics/0.0.190";
 export const EPOCH_INTERVAL = 2200;
 
 export * from "./networkDetails";
