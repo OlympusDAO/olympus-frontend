6fbab49eedb5b52fdcde216c9331ea60ffe89313
0xJem <0x0xJem@gmail.com>
2022-06-23 15:13:29 +0400

    Revert to production URL

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const THE_GRAPH_URL = "https://api.studio.thegraph.com/query/28103/olympus-protocol-metrics/2.0.8";
+export const THE_GRAPH_URL = "https://api.thegraph.com/subgraphs/name/olympusdao/olympus-protocol-metrics";
 export const EPOCH_INTERVAL = 2200;
 
 export * from "./networkDetails";
