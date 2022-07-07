810153ea0de341ad2528565cd89e617ecff9091c
Jem <0x0xjem@gmail.com>
2022-07-07 06:16:38 +0000

    Document subgraph delays

--- src/hooks/useProtocolMetrics.ts
+++ src/hooks/useProtocolMetrics.ts
@@ -174,12 +174,30 @@ export const useOhmFloatingSupply = () =>
     {},
     { select: data => data.protocolMetrics[0].ohmFloatingSupply },
   );
+/**
+ * Returns the latest OHM price (in USD).
+ *
+ * This data is fetched from the subgraph, and will not reflect market rates.
+ * Do NOT use this if you need real-time data. Instead, use `useOhmPrice` from
+ * `src/hooks/usePrices.ts`.
+ *
+ * @returns
+ */
 export const useOhmPrice = () =>
   useMetricsBarLatestOnlyQuery(
     { endpoint: getSubgraphUrl() },
     {},
     { select: data => data.protocolMetrics[0].ohmPrice },
   );
+/**
+ * Returns the latest gOHM price (in USD).
+ *
+ * This data is fetched from the subgraph, and will not reflect market rates.
+ * Do NOT use this if you need real-time data. Instead, use `useGohmPrice` from
+ * `src/hooks/usePrices.ts`.
+ *
+ * @returns
+ */
 export const useGOhmPrice = () =>
   useMetricsBarLatestOnlyQuery(
     { endpoint: getSubgraphUrl() },
