c7176be2e866a0474f273722a9ab07dc647b0e76
Jem <0x0xjem@gmail.com>
2022-07-15 08:08:16 +0000

    Remove Multifarm tabs and redundant CSS/code.

--- src/views/TreasuryDashboard/TreasuryDashboard.tsx
+++ src/views/TreasuryDashboard/TreasuryDashboard.tsx
@@ -1,5 +1,3 @@
-import "./TreasuryDashboard.scss";
-
 import { t } from "@lingui/macro";
 import {
   Box,
@@ -11,11 +9,9 @@ import {
   useMediaQuery,
 } from "@mui/material";
 import { styled, useTheme } from "@mui/material/styles";
-import { DashboardPro, Proteus, TotalIncome, TreasuryAllocation } from "@multifarm/widget";
 import { Metric, MetricCollection, Paper, Tab, Tabs } from "@olympusdao/component-library";
 import { memo, useEffect, useState } from "react";
 import { NavLink, Outlet, Route, Routes, useSearchParams } from "react-router-dom";
-import { Environment } from "src/helpers/environment/Environment/Environment";
 
 import {
   AssetsTable,
@@ -79,8 +75,8 @@ const MetricsDashboard = () => {
 
   return (
     <>
-      <Box className="hero-metrics">
-        <Paper className="ohm-card">
+      <Box>
+        <Paper fullWidth>
           <MetricCollection>
             <MarketCap {...sharedMetricProps} />
             <OHMPrice {...sharedMetricProps} />
@@ -145,58 +141,32 @@ const MetricsDashboard = () => {
 };
 
 const PageWrapper = () => {
-  //const [view, setView] = useState(activeView);
   const isSmallScreen = useMediaQuery("(max-width: 650px)");
   const isVerySmallScreen = useMediaQuery("(max-width: 379px)");
 
   return (
     <div id="treasury-dashboard-view" className={`${isSmallScreen && "smaller"} ${isVerySmallScreen && "very-small"}`}>
-      {!Environment.isMultifarmDashboardEnabled() ? (
-        <Container
-          style={{
-            paddingLeft: isSmallScreen || isVerySmallScreen ? "0" : "3.3rem",
-            paddingRight: isSmallScreen || isVerySmallScreen ? "0" : "3.3rem",
-          }}
-        >
-          <Outlet />
-        </Container>
-      ) : (
-        <>
-          <Tabs
-            value={false}
-            variant={!(isSmallScreen || isVerySmallScreen) ? "standard" : "scrollable"}
-            centered={!(isSmallScreen || isVerySmallScreen)}
-            scrollButtons="auto"
-            textColor="primary"
-            indicatorColor="primary"
-            aria-label="dashboard-tabs"
-          >
-            <Link to="/dashboard" end component={NavLink}>
-              <Tab label={t`Dashboard`} />
-            </Link>
-            {/* <Link to="/dashboard/treasury" component={NavLink}>
-              <Tab label={t`Treasury`} />
-            </Link>
-            <Link to="/dashboard/revenue" component={NavLink}>
-              <Tab label={t`Revenue`} />
-            </Link> */}
-            <Link to="/dashboard/olympuspro" component={NavLink}>
-              <Tab label={t`Olympus Pro`} style={{ whiteSpace: "nowrap" }} />
-            </Link>
-            <Link to="/dashboard/proteus" component={NavLink}>
-              <Tab label={t`Proteus`} />
-            </Link>
-          </Tabs>
-          <Container
-            style={{
-              paddingLeft: isSmallScreen || isVerySmallScreen ? "0" : "3.3rem",
-              paddingRight: isSmallScreen || isVerySmallScreen ? "0" : "3.3rem",
-            }}
-          >
-            <Outlet />
-          </Container>
-        </>
-      )}
+      <Tabs
+        value={false}
+        variant={!(isSmallScreen || isVerySmallScreen) ? "standard" : "scrollable"}
+        centered={!(isSmallScreen || isVerySmallScreen)}
+        scrollButtons="auto"
+        textColor="primary"
+        indicatorColor="primary"
+        aria-label="dashboard-tabs"
+      >
+        <Link to="/dashboard" end component={NavLink}>
+          <Tab label={t`Dashboard`} />
+        </Link>
+      </Tabs>
+      <Container
+        style={{
+          paddingLeft: isSmallScreen || isVerySmallScreen ? "0" : "3.3rem",
+          paddingRight: isSmallScreen || isVerySmallScreen ? "0" : "3.3rem",
+        }}
+      >
+        <Outlet />
+      </Container>
     </div>
   );
 };
@@ -210,38 +180,6 @@ const TreasuryDashboard = () => {
             element={
               <Box sx={{ mt: "15px" }}>
                 <MetricsDashboard />
-              </Box>
-            }
-          />
-          <Route
-            path="treasury"
-            element={
-              <Box sx={{ mt: "15px" }}>
-                <TreasuryAllocation />
-              </Box>
-            }
-          />
-          <Route
-            path="revenue"
-            element={
-              <Box className="treasury">
-                <TotalIncome />
-              </Box>
-            }
-          />
-          <Route
-            path="olympuspro"
-            element={
-              <Box className="dashboard-pro">
-                <DashboardPro />
-              </Box>
-            }
-          />
-          <Route
-            path="proteus"
-            element={
-              <Box className="proteus">
-                <Proteus />
               </Box>
             }
           />
