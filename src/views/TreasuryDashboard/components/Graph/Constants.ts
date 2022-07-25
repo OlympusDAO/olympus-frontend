1c2a79e9e66cfc1dcf86b640cd13abfcb7603481
Jem <0x0xjem@gmail.com>
2022-07-25 11:54:23 +0000

    Remove redundant declaration

--- src/views/TreasuryDashboard/components/Graph/Constants.ts
+++ src/views/TreasuryDashboard/components/Graph/Constants.ts
@@ -1,7 +1,3 @@
-export interface ToggleCallback {
-  (event: React.MouseEvent<HTMLElement>, newValue: string | null): void;
-}
-
 export const QUERY_RECORD_COUNT = "recordCount";
 export const QUERY_TOKEN = "token";
 export const QUERY_TOKEN_OHM = "OHM";
