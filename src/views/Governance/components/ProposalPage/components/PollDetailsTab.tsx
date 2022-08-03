3776bb5b7184ddc4209fb85e51b6a467967de3eb
appleseed-iii <appleseed-iii@protonmail.com>
2022-08-03 17:21:24 -0500

    markdown parsing

--- src/views/Governance/components/ProposalPage/components/PollDetailsTab.tsx
+++ src/views/Governance/components/ProposalPage/components/PollDetailsTab.tsx
@@ -1,12 +1,21 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid } from "@mui/material";
 import { TextButton } from "@olympusdao/component-library";
+import MarkdownIt from "markdown-it";
 import { ProposalTabProps } from "src/views/Governance/interfaces";
 
 export const PollDetailsTab = ({ proposal }: ProposalTabProps) => {
+  const getRenderedDetails = () => {
+    console.log(proposal?.content);
+    return {
+      __html: MarkdownIt({ linkify: true, typographer: true }).render(proposal ? proposal.content : ""),
+    };
+  };
+
   return (
     <Grid container direction="column">
       <Grid item>
-        <Typography variant="body1">{proposal.content}</Typography>
+        {/* TODO(appleseed): ... verify this is ok, XSS security wise */}
+        <div dangerouslySetInnerHTML={getRenderedDetails()} />
       </Grid>
       <Grid className="discussion-button" item>
         <TextButton href={proposal.uri} endIconName="arrow-up">
