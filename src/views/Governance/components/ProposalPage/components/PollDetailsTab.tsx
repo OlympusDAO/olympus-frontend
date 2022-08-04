a4180b249ddf136e661c0763eaec2972f29fcac3
brightiron <brightiron@protonmail.com>
2022-08-04 08:43:17 -0500

    fix image overflow issue

--- src/views/Governance/components/ProposalPage/components/PollDetailsTab.tsx
+++ src/views/Governance/components/ProposalPage/components/PollDetailsTab.tsx
@@ -1,22 +1,26 @@
-import { Grid } from "@mui/material";
+import { Grid, styled } from "@mui/material";
 import { TextButton } from "@olympusdao/component-library";
-import MarkdownIt from "markdown-it";
+import ReactMarkdown from "react-markdown";
 import { ProposalTabProps } from "src/views/Governance/interfaces";
 
 export const PollDetailsTab = ({ proposal }: ProposalTabProps) => {
-  const getRenderedDetails = () => {
-    console.log(proposal?.content);
-    return {
-      __html: MarkdownIt({ linkify: true, typographer: true }).render(proposal ? proposal.content : ""),
-    };
-  };
+  const StyledGridItem = styled(Grid)(() => ({
+    "p > img": {
+      maxWidth: "100%",
+    },
+  }));
+  // const getRenderedDetails = () => {
+  //   console.log(proposal?.content);
+  //   return {
+  //     __html: MarkdownIt({ linkify: true, typographer: true }).render(proposal ? proposal.content : ""),
+  //   };
+  // };
 
   return (
     <Grid container direction="column">
-      <Grid item>
-        {/* TODO(appleseed): ... verify this is ok, XSS security wise */}
-        <div dangerouslySetInnerHTML={getRenderedDetails()} />
-      </Grid>
+      <StyledGridItem item>
+        <ReactMarkdown children={proposal.content} />
+      </StyledGridItem>
       <Grid className="discussion-button" item>
         <TextButton href={proposal.uri} endIconName="arrow-up">
           Discussion
