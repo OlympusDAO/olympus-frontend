8e06deb237e16f0c6201a1f0f557c7e5e5d2c0d0
appleseed-iii <appleseed-iii@protonmail.com>
2022-08-05 21:25:42 -0500

    ugly, but functioning contract creation

--- src/helpers/Web3Storage.ts
+++ src/helpers/Web3Storage.ts
@@ -1,19 +1,31 @@
 import { IProposalContent } from "src/hooks/useProposals";
 import { Filelike, Web3Storage } from "web3.storage";
+import { CIDString } from "web3.storage/dist/src/lib/interface";
 
 import { Environment } from "./environment/Environment/Environment";
+
+export interface IPFSFileData {
+  path: string;
+  cid: CIDString;
+  fileName: string;
+}
 
 /**
  * uploads a given file to Web3.Storage with the API Key
  */
-export const uploadToIPFS = async (files: Iterable<Filelike>) => {
+export const uploadToIPFS = async (file: Filelike) => {
   const accessToken = Environment.getWeb3StorageKey();
   if (accessToken) {
     // NOTE(appleseed): the below endpoint is required in typescript, it is simply the default from: https://github.com/web3-storage/web3.storage/blob/main/packages/client/src/lib.js#L92
     const client = new Web3Storage({ token: accessToken, endpoint: new URL("https://api.web3.storage") });
-    const cid = await client.put(files);
+    // NOTE(appleseed): web3Storage client expects an array of files.
+    const cid = await client.put([file]);
     console.log("stored files with cid:", cid);
-    return cid;
+    return {
+      path: `${cid}/${file.name}`,
+      cid,
+      fileName: file.name,
+    };
   }
 };
 
@@ -30,21 +42,21 @@ export const uploadToIPFS = async (files: Iterable<Filelike>) => {
  * }
  * ```
  */
-interface IProposalJson extends IProposalContent {
+export interface IProposalJson extends IProposalContent {
   content: string;
 }
 
 /**
  * expects a js object, turns it into a json file
- * @returns an Iterable<Filelike>
+ * @returns an Filelike
  */
-export const makeJsonFile = (object: IProposalJson): Iterable<Filelike> => {
+export const makeJsonFile = (object: IProposalJson, fileName: string): Filelike => {
   // You can create File objects from a Blob of binary data
   // see: https://developer.mozilla.org/en-US/docs/Web/API/Blob
   // Here we're just storing a JSON object, but you can store images,
   // audio, or whatever you want!
   const blob = new Blob([JSON.stringify(object)], { type: "application/json" });
 
-  const files = [new File([blob], "proposal.json")];
-  return files;
+  const file = new File([blob], fileName);
+  return file;
 };
