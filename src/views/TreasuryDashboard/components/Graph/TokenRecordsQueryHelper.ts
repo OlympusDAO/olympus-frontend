b02ea391b889bd2ddde9b7c957020d8ad5203ec5
Jem <0x0xjem@gmail.com>
2022-08-22 06:48:16 +0000

    Pagination works with this approach.

--- src/views/TreasuryDashboard/components/Graph/TokenRecordsQueryHelper.ts
+++ src/views/TreasuryDashboard/components/Graph/TokenRecordsQueryHelper.ts
@@ -26,26 +26,38 @@ export const getNextPageParamFactory = (
     `${queryName}-${earliestDate}: create getNextPageParam with earliestDate ${earliestDate} and currentStartDate ${currentStartDate.current}`,
   );
   return (lastPage: TokenRecordsQuery): TokenRecordsQueryVariables | undefined => {
-    console.debug(
-      `${queryName}-${earliestDate}: Received ${lastPage.tokenRecords.length} records with currentStartDate ${currentStartDate.current}`,
-    );
+    // console.debug(
+    //   `${queryName}-${earliestDate}: Received ${lastPage.tokenRecords.length} records with currentStartDate ${currentStartDate.current}`,
+    // );
+    console.debug(`${queryName}-${earliestDate}: Received ${lastPage.tokenRecords.length} records`);
 
-    /**
-     * If we are at the earliestDate, then there is no need to fetch the next page.
-     *
-     * Returning undefined tells react-query not to fetch the next page.
-     */
-    if (new Date(currentStartDate.current).getTime() <= new Date(earliestDate).getTime()) {
-      console.debug(`${queryName}-${earliestDate}: Data loading done`);
+    if (lastPage.tokenRecords.length === 0) {
+      console.debug(`${queryName}-${earliestDate}: No records. Exiting.`);
       return;
     }
+
+    const existingStartDate = lastPage.tokenRecords[lastPage.tokenRecords.length - 1].date;
+    if (new Date(existingStartDate).getTime() <= new Date(earliestDate).getTime()) {
+      console.debug(`${queryName}-${earliestDate}: Hit earliestDate.`);
+      return;
+    }
+
+    // /**
+    //  * If we are at the earliestDate, then there is no need to fetch the next page.
+    //  *
+    //  * Returning undefined tells react-query not to fetch the next page.
+    //  */
+    // if (new Date(currentStartDate.current).getTime() <= new Date(earliestDate).getTime()) {
+    //   console.debug(`${queryName}-${earliestDate}: Data loading done`);
+    //   return;
+    // }
 
     /**
      * We adjust the date range and trigger the next query.
      */
-    const newFinishDate = currentStartDate.current;
+    const newFinishDate = existingStartDate;
     const newStartDate = getNextPageStartDate(newFinishDate, earliestDate, dateOffset);
-    currentStartDate.current = newStartDate;
+    // currentStartDate.current = newStartDate;
 
     console.debug(
       `${queryName}-${earliestDate}: Loading next page with start date ${newStartDate} and finish date ${newFinishDate}`,
