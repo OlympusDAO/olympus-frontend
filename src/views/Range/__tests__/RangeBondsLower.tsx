db15194edee04c166eda4583f4892e2e406eff94
brightiron <brightiron@protonmail.com>
2022-09-06 10:50:38 -0500

    updating range test

--- src/views/Range/__tests__/RangeBondsLower.tsx
+++ src/views/Range/__tests__/RangeBondsLower.tsx
@@ -4,7 +4,7 @@ import { DecimalBigNumber } from "src/helpers/DecimalBigNumber/DecimalBigNumber"
 import * as Balance from "src/hooks/useBalance";
 import { useContractAllowance } from "src/hooks/useContractAllowance";
 import { connectWallet } from "src/testHelpers";
-import { render, screen } from "src/testUtils";
+import { fireEvent, render, screen } from "src/testUtils";
 import * as BondTellerContract from "src/typechain/factories/BondTeller__factory";
 import * as IERC20Factory from "src/typechain/factories/IERC20__factory";
 import * as RANGEPriceContract from "src/typechain/factories/RangePrice__factory";
@@ -61,25 +61,31 @@ describe("Lower Wall Active Bond Market", () => {
   });
 
   it("Should Buy at Upper wall price of $24.18", async () => {
+    //@ts-expect-error
+    RangeHooks.DetermineRangePrice = jest.fn().mockReturnValue({ data: { price: "24.18" } });
     render(<Range />);
     expect(await screen.findByTestId("swap-price")).toHaveTextContent("24.18");
   });
 
-  // it("Should Sell at Bond market price of $10.12", async () => {
-  //   render(<Range />);
-  //   fireEvent.click(screen.getByTestId("sell-tab"));
-  //   expect(await screen.findByTestId("swap-price")).toHaveTextContent("10.12");
-  // });
+  it("Should Sell at Bond market price of $10.12", async () => {
+    //@ts-expect-error
+    RangeHooks.DetermineRangePrice = jest.fn().mockReturnValue({ data: { price: "10.12" } });
+    render(<Range />);
+    fireEvent.click(screen.getByTestId("sell-tab"));
+    expect(await screen.findByTestId("swap-price")).toHaveTextContent("10.12");
+  });
 
-  // it("Should have a disclaimer notifying a sell below current market price ($13.20)", async () => {
-  //   render(<Range />);
-  //   fireEvent.click(screen.getByTestId("sell-tab"));
-  //   fireEvent.input(await screen.findByTestId("reserve-amount"), { target: { value: "6" } });
-  //   fireEvent.click(screen.getByTestId("range-submit"));
-  //   expect(
-  //     await screen.findByText("I understand that I am selling at a discount to current market price"),
-  //   ).toBeInTheDocument();
-  // });
+  it("Should have a disclaimer notifying a sell below current market price ($13.20)", async () => {
+    //@ts-expect-error
+    RangeHooks.DetermineRangePrice = jest.fn().mockReturnValue({ data: { price: "10.12" } });
+    render(<Range />);
+    fireEvent.click(screen.getByTestId("sell-tab"));
+    fireEvent.input(await screen.findByTestId("reserve-amount"), { target: { value: "6" } });
+    fireEvent.click(screen.getByTestId("range-submit"));
+    expect(
+      await screen.findByText("I understand that I am selling at a discount to current market price"),
+    ).toBeInTheDocument();
+  });
 });
 
 // describe("Bond Swap Transaction", () => {
