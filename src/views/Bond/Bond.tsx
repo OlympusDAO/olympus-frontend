7db14dfc10902f148d5b7d342e0996dcf8feb748
brightiron <brightiron@protonmail.com>
2022-09-20 21:17:23 -0500

    remove console log

--- src/views/Bond/Bond.tsx
+++ src/views/Bond/Bond.tsx
@@ -25,8 +25,6 @@ export const Bond = () => {
   const bonds = liveBondsV2.concat(liveBondsV3);
   const inverse = inverseV2.concat(inverseV3);
 
-  console.log(bonds, "bonds", inverse);
-
   const showTabs = !!inverse && inverse.length > 0 && !!bonds && bonds.length > 0;
 
   /**
