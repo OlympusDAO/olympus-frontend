f9e6034265b0ae6bbb458d381ff416df923c825b
brightiron <brightiron@protonmail.com>
2022-09-27 15:35:35 -0500

    revert a couple changes

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
 import { FC, ReactElement } from "react";
 
 export interface OHMPageTitleProps {
@@ -9,15 +9,19 @@ export interface OHMPageTitleProps {
  * Component for Displaying PageTitle
  */
 const PageTitle: FC<OHMPageTitleProps> = ({ name }) => {
+  const theme = useTheme();
+  const mobile = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
     <Box
       width="100%"
+      paddingTop="3px"
       paddingBottom="22.5px"
       display="flex"
       flexDirection="row"
       justifyContent="flex-start"
       alignContent="center"
-      marginLeft="33px"
+      marginLeft={mobile ? "9px" : "33px"}
     >
       <Typography variant="h1">{name}</Typography>
     </Box>
