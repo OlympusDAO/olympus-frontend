2522cea30b7d08054320ba98876f11900a811b13
brightiron <brightiron@protonmail.com>
2022-10-11 18:42:05 -0500

    fix test

--- src/views/Bond/components/BondDuration.tsx
+++ src/views/Bond/components/BondDuration.tsx
@@ -1,5 +1,5 @@
 import { prettifySecondsInDays } from "src/helpers/timeUtil";
 
 export const BondDuration: React.VFC<{ duration: number }> = props => {
-  return <>{props.duration === 0 ? "Instant" : prettifySecondsInDays(props.duration)}</>;
+  return <>{props.duration === 0 ? "Instantly" : prettifySecondsInDays(props.duration)}</>;
 };
