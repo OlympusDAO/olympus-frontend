2cb4d2e4525ff01557f8da1f9f425208f994fa59
brightiron <brightiron@protonmail.com>
2022-12-18 17:06:33 -0600

    wrap in test so snapshots execute

--- src/components/Sidebar/__tests__/Sidebar.unit.test.tsx
+++ src/components/Sidebar/__tests__/Sidebar.unit.test.tsx
@@ -1,9 +1,13 @@
 import Sidebar from "src/components/Sidebar/Sidebar";
 import { render } from "src/testUtils";
+import { test } from "vitest";
+import { describe, expect, it } from "vitest";
 
 describe("<Sidebar/>", () => {
-  it("should render component", () => {
-    const { container } = render(<Sidebar />);
-    expect(container).toMatchSnapshot();
+  test("should render component", () => {
+    it("should render component", () => {
+      const { container } = render(<Sidebar />);
+      expect(container).toMatchSnapshot();
+    });
   });
 });
--- src/views/Stake/__tests__/Stake.unit.test.tsx
+++ src/views/Stake/__tests__/Stake.unit.test.tsx
@@ -1,20 +1,24 @@
 import { render, screen } from "src/testUtils";
 import Stake from "src/views/Stake/Stake";
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, test } from "vitest";
 
 describe("<Stake/>", () => {
-  it("should render component", async () => {
-    const { container } = render(<Stake />);
-    expect(container).toMatchSnapshot();
+  test("should render component", () => {
+    it("should render component", async () => {
+      const { container } = render(<Stake />);
+      expect(container).toMatchSnapshot();
+    });
   });
 
-  it("should render correct staking headers", () => {
-    const { container } = render(<Stake />);
-    // there should be a header inviting user to Stake
-    expect(screen.getAllByText("Stake")[0]);
-    //  there should be a Farm Pool table
+  test("should render correct staking headers", () => {
+    it("should render correct staking headers", () => {
+      const { container } = render(<Stake />);
+      // there should be a header inviting user to Stake
+      expect(screen.getAllByText("Stake")[0]);
+      //  there should be a Farm Pool table
 
-    expect(screen.getByText("Farm Pool"));
-    expect(container).toMatchSnapshot();
+      expect(screen.getByText("Farm Pool"));
+      expect(container).toMatchSnapshot();
+    });
   });
 });
