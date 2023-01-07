86196ae23e9694c871b4c2f6e461839ed0211c71
brightiron <brightiron@protonmail.com>
2023-01-07 08:48:08 -0600

    add ohm-weth to farm pools

--- src/helpers/AllExternalPools.ts
+++ src/helpers/AllExternalPools.ts
@@ -1,9 +1,9 @@
 import { NetworkId } from "src/constants";
 import { ExternalPool } from "src/lib/ExternalPool";
 
-export const balancer_ohm_dai = new ExternalPool({
-  poolName: "OHM-DAI",
-  icons: ["OHM", "DAI"],
+export const balancer_ohm_eth = new ExternalPool({
+  poolName: "OHM-wETH",
+  icons: ["OHM", "wETH"],
   stakeOn: "Balancer",
   pairGecko: "ethereum",
   rewardGecko: "gOHM",
@@ -13,6 +13,20 @@ export const balancer_ohm_dai = new ExternalPool({
   rewarder: "", //not currenly incentivized
   vault: "0xba12222222228d8ba445958a75a0704d566bf2c8",
   poolId: "0xd1ec5e215e8148d76f4460e4097fd3d5ae0a35580002000000000000000003d3",
+  networkID: NetworkId.MAINNET,
+});
+export const balancer_ohm_dai = new ExternalPool({
+  poolName: "OHM-DAI",
+  icons: ["OHM", "DAI"],
+  stakeOn: "Balancer",
+  pairGecko: "ethereum",
+  rewardGecko: "gOHM",
+  href: "https://app.balancer.fi/#/ethereum/pool/0x76fcf0e8c7ff37a47a799fa2cd4c13cde0d981c90002000000000000000003d2",
+  address: "0x76fcf0e8c7ff37a47a799fa2cd4c13cde0d981c9",
+  masterchef: "0xc45D42f801105e861e86658648e3678aD7aa70f9", //not currenly incentivized
+  rewarder: "", //not currenly incentivized
+  vault: "0xba12222222228d8ba445958a75a0704d566bf2c8",
+  poolId: "0x76fcf0e8c7ff37a47a799fa2cd4c13cde0d981c90002000000000000000003d2",
   networkID: NetworkId.MAINNET,
 });
 
@@ -59,7 +73,7 @@ export const frax_ohm_frax = new ExternalPool({
   mintAndSync: true,
 });
 
-export const balancerPools = [balancer_ohm_dai];
+export const balancerPools = [balancer_ohm_dai, balancer_ohm_eth];
 export const curvePools = [curve_ohm_eth];
 export const convexPools = [convex_ohm_eth];
 export const fraxPools = [frax_ohm_frax];
