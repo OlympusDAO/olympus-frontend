705c2a236a51a5bf25942be5a37d5d3635d516ea
brightiron <brightiron@protonmail.com>
2023-04-23 19:48:30 -0500

    copy updates

--- src/views/Liquidity/YourAMODeposits.tsx
+++ src/views/Liquidity/YourAMODeposits.tsx
@@ -107,9 +107,6 @@ export const YourAmoDeposits = ({ vaults }: { vaults: VaultInfo[] }) => {
             <Typography fontSize="12px" fontWeight="500" lineHeight="15px">
               You don't have any Boosted Liquidity Vault deposits yet.
             </Typography>
-            <Typography fontSize="12px" color={theme.colors.gray[40]} mb="16.5px">
-              By doing so, you are strengthening the protocol and making your voice heard.
-            </Typography>
             <TertiaryButton href="https://docs.olympusdao.finance">Learn about OHM Boosted Vaults</TertiaryButton>
           </Box>
         </>
