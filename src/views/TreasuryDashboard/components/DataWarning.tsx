3b119c3a4c908931adc8914eee982b65fcd71047
Jem <0x0xjem@gmail.com>
2023-05-04 09:00:32 +0000

    Clean up data warning page. Highlight lagging blockchain.

--- src/views/TreasuryDashboard/components/DataWarning.tsx
+++ src/views/TreasuryDashboard/components/DataWarning.tsx
@@ -1,16 +1,11 @@
-import {
-  Grid,
-  Skeleton,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Typography,
-} from "@mui/material";
+import { Grid, Skeleton, Table, TableBody, TableCell, TableHead, TableRow, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
+import { getISO8601String } from "src/helpers/DateHelper";
 import { PaginatedTokenRecord, useTokenRecordsLatestQuery } from "src/hooks/useFederatedSubgraphQuery";
+
+const getDateFromTimestamp = (timestamp: string): Date => {
+  return new Date(+timestamp * 1000);
+};
 
 /**
  * React Component that renders the contents of a Markdown file
@@ -24,6 +19,8 @@ const DataWarning = (): JSX.Element => {
   const [isWarningEnabled, setIsWarningEnabled] = useState(false);
   const [latestCompleteDate, setLatestCompleteDate] = useState<string>();
   const [latestRecords, setLatestRecords] = useState<PaginatedTokenRecord[]>([]);
+
+  const laggingColorStyle = { background: "orange" };
 
   useEffect(() => {
     // Still loading
@@ -75,15 +72,15 @@ const DataWarning = (): JSX.Element => {
           margin: "0 auto", // Centers
         }}
       >
-        <p>
-          The data on this dashboard is sourced from subgraphs on each blockchain that are periodically updated. One or
-          more of the subgraphs is currently out of date, which is affecting the accuracy of the data shown here.
-        </p>
-        <p>The latest date for which data is complete is: {latestCompleteDate || <Skeleton width="20px" />}</p>
+        The data on this dashboard is sourced from subgraphs on each blockchain that are periodically updated. One or
+        more of the subgraphs is currently out of date, which is affecting the accuracy of the data shown here.
+        <br />
+        <br />
+        The latest date for which data is complete is: {latestCompleteDate || <Skeleton width="20px" />}
       </Grid>
-      <Grid item xs={8} alignItems="center">
-        {/* Center-align */}
-        <TableContainer>
+      <Grid item xs={12} container wrap={"nowrap"}>
+        <Grid item xs={1} m={2} />
+        <Grid item xs={10} m={8} style={{ margin: "0px" }}>
           <Table>
             <TableHead>
               <TableRow>
@@ -95,9 +92,16 @@ const DataWarning = (): JSX.Element => {
             <TableBody>
               {latestRecords.length ? (
                 latestRecords.map(record => (
-                  <TableRow>
+                  <TableRow
+                    style={{
+                      // If the current timestamp is equal to the value of latestCompleteDate, then it is the lagging blockchain and should be highlighted
+                      ...(getISO8601String(getDateFromTimestamp(record.timestamp)) == latestCompleteDate
+                        ? laggingColorStyle
+                        : {}),
+                    }}
+                  >
                     <TableCell>{record.blockchain}</TableCell>
-                    <TableCell>{record.date}</TableCell>
+                    <TableCell>{getDateFromTimestamp(record.timestamp).toISOString()}</TableCell>
                     <TableCell>{record.block}</TableCell>
                   </TableRow>
                 ))
@@ -106,7 +110,8 @@ const DataWarning = (): JSX.Element => {
               )}
             </TableBody>
           </Table>
-        </TableContainer>
+        </Grid>
+        <Grid item xs={1} m={2} />
       </Grid>
     </Grid>
   );
