e540c9806393c4cbc5d4aa370dcbdc28618a6988
Jem <0x0xJem@gmail.com>
2023-09-19 16:51:50 +0400

    Shift positions over to use API

--- src/views/Lending/Cooler/Positions.tsx
+++ src/views/Lending/Cooler/Positions.tsx
@@ -11,8 +11,8 @@ import {
   Typography,
 } from "@mui/material";
 import { PrimaryButton, SecondaryButton, Token } from "@olympusdao/component-library";
-import { ethers } from "ethers";
 import { useState } from "react";
+import { formatCurrency, formatNumber } from "src/helpers";
 import { CreateLoan } from "src/views/Lending/Cooler/CreateLoan";
 import { ExtendLoan } from "src/views/Lending/Cooler/ExtendLoan";
 import { useGetClearingHouse } from "src/views/Lending/Cooler/hooks/useGetClearingHouse";
@@ -27,7 +27,7 @@ export const CoolerPositions = () => {
   const { data: clearingHouse } = useGetClearingHouse();
   const { latestSnapshot } = useSnapshotLatest();
   const [createLoanModalOpen, setCreateLoanModalOpen] = useState(false);
-  const { data: loans, isFetched: isFetchedLoans } = useGetCoolerLoans({
+  const { loans } = useGetCoolerLoans({
     walletAddress: address,
     factoryAddress: clearingHouse?.factory,
     collateralAddress: clearingHouse?.collateralAddress,
@@ -44,19 +44,22 @@ export const CoolerPositions = () => {
   const [extendLoan, setExtendLoan] = useState<any>(null);
   const [repayLoan, setRepayLoan] = useState<any>(null);
 
+  // TODO handle wallet not connected
+
   return (
     <div id="cooler-positions">
       <Box mb="21px" mt="66px">
         <Typography variant="h1">Your Positions</Typography>
         <div>Borrow DAI from the Olympus Treasury against your gOHM</div>
       </Box>
-      {!isFetchedLoans && (
+
+      {loans && (
         <Box display="flex" justifyContent="center">
           <Skeleton variant="rectangular" width="100%" height={100} />
         </Box>
       )}
 
-      {isFetchedLoans && (
+      {loans && loans.length == 0 && (
         <Box display="flex" justifyContent="center">
           <Box textAlign="center">
             <Box fontWeight={700}>You currently have no Cooler loans</Box>
@@ -68,68 +71,62 @@ export const CoolerPositions = () => {
         </Box>
       )}
 
-      {coolerAddress && (
+      {loans && loans.length > 0 && (
         <>
-          {loans && loans.length > 0 && (
-            <>
-              <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell sx={{ fontSize: "15px", padding: "9px" }}>Collateral</TableCell>
-                      <TableCell sx={{ fontSize: "15px", padding: "9px" }} align="right">
-                        Interest Rate
-                      </TableCell>
-                      <TableCell sx={{ fontSize: "15px", padding: "9px" }} align="right">
-                        Repayment
-                      </TableCell>
-                      <TableCell sx={{ fontSize: "15px", padding: "9px" }} align="right">
-                        Maturity Date
-                      </TableCell>
-                      <TableCell sx={{ fontSize: "15px", padding: "9px" }} align="right"></TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {loans?.map((loan, index) => (
-                      <TableRow key={index} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-                        <TableCell component="th" scope="row" sx={{ padding: "9px" }}>
-                          <Box display="flex" alignItems="center" gap="3px">
-                            {loan.collateral && Number(ethers.utils.formatUnits(loan.collateral.toString())).toFixed(4)}{" "}
-                            gOHM <Token name="gOHM" style={{ fontSize: "21px" }} />
-                          </Box>
-                        </TableCell>
-                        <TableCell align="right" sx={{ padding: "9px" }}>
-                          {loan.request?.interest && (
-                            <Box>{Number(ethers.utils.formatUnits(loan.request.interest.toString())) * 100}%</Box>
-                          )}
-                        </TableCell>
-                        <TableCell align="right" sx={{ padding: "9px" }}>
-                          {loan.amount && (
-                            <Box display="flex" justifyContent="end" alignItems={"center"} gap="3px">
-                              {Number(ethers.utils.formatUnits(loan.amount.toString())).toFixed(2)} DAI{" "}
-                              <Token name="DAI" style={{ fontSize: "21px" }} />
-                            </Box>
-                          )}
-                        </TableCell>
-                        <TableCell align="right" sx={{ padding: "9px" }}>
-                          {loan.expiry && <Box>{new Date(Number(loan.expiry.toString()) * 1000).toLocaleString()}</Box>}
-                        </TableCell>
-                        <TableCell align="right" sx={{ padding: "9px" }}>
-                          <Box display="flex">
-                            <SecondaryButton onClick={() => setRepayLoan(loan)}>Repay</SecondaryButton>
-                            <PrimaryButton onClick={() => setExtendLoan(loan)}>Extend</PrimaryButton>
-                          </Box>
-                        </TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-              <Box display="flex" justifyContent={"center"}>
-                <PrimaryButton onClick={() => setCreateLoanModalOpen(true)}>Borrow DAI & Open Position</PrimaryButton>
-              </Box>
-            </>
-          )}
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell sx={{ fontSize: "15px", padding: "9px" }}>Collateral</TableCell>
+                  <TableCell sx={{ fontSize: "15px", padding: "9px" }} align="right">
+                    Interest Rate
+                  </TableCell>
+                  <TableCell sx={{ fontSize: "15px", padding: "9px" }} align="right">
+                    Repayment
+                  </TableCell>
+                  <TableCell sx={{ fontSize: "15px", padding: "9px" }} align="right">
+                    Maturity Date
+                  </TableCell>
+                  <TableCell sx={{ fontSize: "15px", padding: "9px" }} align="right"></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {loans.map((loan, index) => (
+                  <TableRow key={index} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                    <TableCell component="th" scope="row" sx={{ padding: "9px" }}>
+                      <Box display="flex" alignItems="center" gap="3px">
+                        {loan.collateralDeposited && formatNumber(loan.collateralDeposited, 4)} gOHM{" "}
+                        <Token name="gOHM" style={{ fontSize: "21px" }} />
+                      </Box>
+                    </TableCell>
+                    <TableCell align="right" sx={{ padding: "9px" }}>
+                      {loan.interest && <Box>{loan.interest * 100}%</Box>}
+                    </TableCell>
+                    <TableCell align="right" sx={{ padding: "9px" }}>
+                      {loan.principal && (
+                        <Box display="flex" justifyContent="end" alignItems={"center"} gap="3px">
+                          {formatCurrency(loan.principal, 2, "DAI")}
+                          <Token name="DAI" style={{ fontSize: "21px" }} />
+                        </Box>
+                      )}
+                    </TableCell>
+                    <TableCell align="right" sx={{ padding: "9px" }}>
+                      {loan.expiryTimestamp && <Box>{new Date(loan.expiryTimestamp * 1000).toLocaleString()}</Box>}
+                    </TableCell>
+                    <TableCell align="right" sx={{ padding: "9px" }}>
+                      <Box display="flex">
+                        <SecondaryButton onClick={() => setRepayLoan(loan)}>Repay</SecondaryButton>
+                        <PrimaryButton onClick={() => setExtendLoan(loan)}>Extend</PrimaryButton>
+                      </Box>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <Box display="flex" justifyContent={"center"}>
+            <PrimaryButton onClick={() => setCreateLoanModalOpen(true)}>Borrow DAI & Open Position</PrimaryButton>
+          </Box>
         </>
       )}
 
