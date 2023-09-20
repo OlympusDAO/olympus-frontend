0e9e51b966694408d7526966e5a84ca5817a2d34
Jem <0x0xJem@gmail.com>
2023-09-20 18:06:51 +0400

    Cleanup

--- src/views/Lending/Cooler/index.tsx
+++ src/views/Lending/Cooler/index.tsx
@@ -1,28 +1,13 @@
 import { ArrowBack } from "@mui/icons-material";
 import { Box, Link, Tab, Tabs, Typography, useMediaQuery, useTheme } from "@mui/material";
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
 import PageTitle from "src/components/PageTitle";
 import { CoolerDashboard } from "src/views/Lending/Cooler/dashboard/Dashboard";
-import { useCoolerSnapshotLatest } from "src/views/Lending/Cooler/hooks/useSnapshot";
 import { CoolerPositions } from "src/views/Lending/Cooler/Positions";
 import { LiquidityCTA } from "src/views/Liquidity/LiquidityCTA";
 
 export const Cooler = () => {
-  const { latestSnapshot } = useCoolerSnapshotLatest();
-
-  const [interestRate, setInterestRate] = useState<number>();
-  useMemo(() => {
-    if (!latestSnapshot || !latestSnapshot.terms?.interestRate) {
-      setInterestRate(undefined);
-      return;
-    }
-
-    // Stored as 0.005 (0.5%)
-    // Multiply by 100 to get 0.5
-    setInterestRate(latestSnapshot.terms.interestRate * 100);
-  }, [latestSnapshot]);
-
   const [tabIndex, setTabIndex] = useState(0);
   const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
     setTabIndex(newValue);
