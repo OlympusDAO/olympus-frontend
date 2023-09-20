ef211c6abad19ab06d18fead327bd7fe6a64403f
Jem <0x0xJem@gmail.com>
2023-09-20 12:32:17 +0400

    Shift utilisation and income charts to use the subgraph

--- src/views/Lending/Cooler/dashboard/IncomeGraph.tsx
+++ src/views/Lending/Cooler/dashboard/IncomeGraph.tsx
@@ -1,20 +1,37 @@
 import { useTheme } from "@mui/material";
+import { useMemo, useState } from "react";
 import Chart from "src/components/Chart/Chart";
 import { ChartType, DataFormat } from "src/components/Chart/Constants";
+import { Snapshot } from "src/generated/coolerLoans";
 import {
   getBulletpointStylesMap,
   getCategoriesMap,
   getDataKeyColorsMap,
 } from "src/helpers/subgraph/ProtocolMetricsHelper";
-import { useCoolerSnapshots } from "src/views/Lending/Cooler/dashboard/DataHelper";
+import { useCoolerSnapshot } from "src/views/Lending/Cooler/hooks/useSnapshot";
 import { DEFAULT_BULLETPOINT_COLOURS, DEFAULT_COLORS } from "src/views/TreasuryDashboard/components/Graph/Constants";
 import { getTickStyle } from "src/views/TreasuryDashboard/components/Graph/helpers/ChartHelper";
 
 export const IncomeGraph = () => {
   const theme = useTheme();
 
-  // Get loan data
-  const byDateSnapshots = useCoolerSnapshots();
+  // TODO replace with prop
+  const startDate = new Date();
+  startDate.setDate(startDate.getDate() - 30);
+
+  const { data } = useCoolerSnapshot(startDate);
+
+  const [coolerSnapshots, setCoolerSnapshots] = useState<Snapshot[] | undefined>();
+  useMemo(() => {
+    if (!data) {
+      setCoolerSnapshots(undefined);
+      return;
+    }
+
+    // Sort in descending order
+    data.sort((a, b) => b.timestamp - a.timestamp);
+    setCoolerSnapshots(data);
+  }, [data]);
 
   /**
    * Chart inputs
@@ -29,7 +46,7 @@ export const IncomeGraph = () => {
   return (
     <Chart
       type={ChartType.Bar}
-      data={byDateSnapshots || []}
+      data={coolerSnapshots || []}
       dataFormat={DataFormat.Currency}
       headerText="Protocol Income"
       headerSubText={""}
@@ -38,7 +55,7 @@ export const IncomeGraph = () => {
       dataKeyBulletpointStyles={bulletpointStyles}
       dataKeyLabels={dataKeyLabels}
       infoTooltipMessage={""}
-      isLoading={byDateSnapshots == null}
+      isLoading={!coolerSnapshots}
       tickStyle={getTickStyle(theme)}
       itemDecimals={0}
       margin={{ left: 30 }}
--- src/views/Lending/Cooler/dashboard/UtilisationGraph.tsx
+++ src/views/Lending/Cooler/dashboard/UtilisationGraph.tsx
@@ -1,20 +1,55 @@
 import { useTheme } from "@mui/material";
+import { useMemo, useState } from "react";
 import Chart from "src/components/Chart/Chart";
 import { ChartType, DataFormat } from "src/components/Chart/Constants";
+import { Snapshot } from "src/generated/coolerLoans";
 import {
   getBulletpointStylesMap,
   getCategoriesMap,
   getDataKeyColorsMap,
 } from "src/helpers/subgraph/ProtocolMetricsHelper";
-import { useCoolerSnapshots } from "src/views/Lending/Cooler/dashboard/DataHelper";
+import { getCapacity, getReceivables, useCoolerSnapshot } from "src/views/Lending/Cooler/hooks/useSnapshot";
 import { DEFAULT_BULLETPOINT_COLOURS, DEFAULT_COLORS } from "src/views/TreasuryDashboard/components/Graph/Constants";
 import { getTickStyle } from "src/views/TreasuryDashboard/components/Graph/helpers/ChartHelper";
+
+type CoolerSnapshotWithTotals = Snapshot & {
+  capacity: number;
+  receivables: number;
+};
 
 export const UtilisationGraph = () => {
   const theme = useTheme();
 
-  // Get loan data
-  const byDateSnapshots = useCoolerSnapshots(new Date("2023-08-01"));
+  // TODO replace with prop
+  const startDate = new Date();
+  startDate.setDate(startDate.getDate() - 30);
+
+  const { data } = useCoolerSnapshot(startDate);
+
+  // Add calculated data
+  const [coolerSnapshots, setCoolerSnapshots] = useState<CoolerSnapshotWithTotals[] | undefined>(undefined);
+  useMemo(() => {
+    if (!data) {
+      setCoolerSnapshots(undefined);
+      return;
+    }
+
+    const _coolerSnapshotsWithTotals = data.map(snapshot => {
+      const capacity = getCapacity(snapshot);
+      const receivables = getReceivables(snapshot);
+
+      return {
+        ...snapshot,
+        capacity,
+        receivables,
+      };
+    });
+
+    // Sort in descending order
+    _coolerSnapshotsWithTotals.sort((a, b) => b.timestamp - a.timestamp);
+
+    setCoolerSnapshots(_coolerSnapshotsWithTotals);
+  }, [data]);
 
   /**
    * Chart inputs
@@ -29,7 +64,7 @@ export const UtilisationGraph = () => {
   return (
     <Chart
       type={ChartType.MultiLine}
-      data={byDateSnapshots || []}
+      data={coolerSnapshots || []}
       dataFormat={DataFormat.Currency}
       headerText="Utilisation"
       headerSubText={""}
@@ -38,7 +73,7 @@ export const UtilisationGraph = () => {
       dataKeyBulletpointStyles={bulletpointStyles}
       dataKeyLabels={dataKeyLabels}
       infoTooltipMessage={""}
-      isLoading={byDateSnapshots == null}
+      isLoading={!coolerSnapshots}
       tickStyle={getTickStyle(theme)}
       itemDecimals={0}
       margin={{ left: 30 }}
