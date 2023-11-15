ed5cfa4df4f74b62b815558925d83b276cd8cd57
brightiron <brightiron@protonmail.com>
2023-11-15 09:40:12 -0600

    copy

--- src/views/MyBalances/LearnAboutGohm.tsx
+++ src/views/MyBalances/LearnAboutGohm.tsx
@@ -11,9 +11,8 @@ export const LearnAboutGohm = () => {
       <Box my="18px">
         <Typography fontWeight="500">What is gOHM?</Typography>
         <Typography color={theme.colors.gray[40]} mt="9px">
-          gOHM is the governance token of the Olympus protocol. gOHM can be obtained by wrapping OHM. gOHM can be
-          unwrapped to OHM at any time. gOHM is used to vote in governance and can be used as collateral to take a
-          Cooler Loan.
+          gOHM is Olympus protocol's governance token, acquired by wrapping OHM for voting and collateral. It can be
+          unwrapped to OHM.
         </Typography>
       </Box>
       <Box display="flex" justifyContent="center">
